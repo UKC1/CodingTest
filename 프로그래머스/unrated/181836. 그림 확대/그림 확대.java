67bb7289a776ed17949252e82024efb9480ea1c5
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-14 01:14:48 +0900

    [level unrated] Title: 그림 확대, Time: 0.10 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.java
@@ -1,17 +1,15 @@
 class Solution {
     public String[] solution(String[] picture, int k) {
         String[] answer = new String[picture.length * k];
-        // StringBuilder sb = new StringBuilder();
+        
         for (int i = 0; i < picture.length; i++) {
-            String str = "";
+            StringBuilder sb = new StringBuilder();
             for (int j = 0; j < picture[i].length(); j++) {
-                for (int m = 0; m < k; m++) {
-                      str += picture[i].charAt(j);
-                }
+                sb.append(picture[i].substring(j, j + 1).repeat(k));
             }
             
             for (int j = 0; j < k; j++) {
-                answer[i * k + j] = str;
+                answer[i * k + j] = sb.toString();
             }
         }
         
