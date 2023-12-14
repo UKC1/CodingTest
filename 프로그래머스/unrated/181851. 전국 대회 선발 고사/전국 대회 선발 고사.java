2441928869b4c684b9dde88955f55c23d7bde743
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-15 01:59:55 +0900

    [level unrated] Title: 전국 대회 선발 고사, Time: 0.19 ms, Memory: 76.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -0,0 +1,24 @@
+import java.util.HashMap;
+import java.util.ArrayList;
+import java.util.Collections;
+class Solution {
+    public int solution(int[] rank, boolean[] attendance) {
+        int answer = 0;
+        HashMap<Integer, Integer> map = new HashMap();
+        ArrayList<Integer> list = new ArrayList();
+        for (int i = 0; i < rank.length; i++) {
+            if (attendance[i]) {
+                map.put(rank[i], i);
+            }
+        }
+        
+        int[] check = new int[3];
+        for (Integer i : map.keySet()) {
+            list.add(i); 
+        }
+        Collections.sort(list);
+        
+        answer = 10000 * map.get(list.get(0)) + 100 * map.get(list.get(1)) + map.get(list.get(2));
+        return answer;
+    }
+}
