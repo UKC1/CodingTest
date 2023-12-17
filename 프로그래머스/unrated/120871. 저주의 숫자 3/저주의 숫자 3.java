67d723578fc80d9aae0888637104e7a735d3b801
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-18 01:40:36 +0900

    [level unrated] Title: 저주의 숫자 3, Time: 0.02 ms, Memory: 75.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/120871. 저주의 숫자 3/저주의 숫자 3.java
@@ -0,0 +1,11 @@
+class Solution {
+    public int solution(int n) {
+        for (int i = 1; i <= n; i++) {
+            if (i % 3 == 0 || i % 10 == 3 || i / 10 == 3 || (i / 10) % 10 == 3) {
+                n++;
+            } 
+        }
+        return n;
+
+    }
+}
