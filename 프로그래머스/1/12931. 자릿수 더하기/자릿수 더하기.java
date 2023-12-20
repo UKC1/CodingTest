f1b629361adcd89bce195050658a73787934d919
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-20 10:45:22 +0900

    [level 1] Title: 자릿수 더하기, Time: 0.05 ms, Memory: 80.6 MB -BaekjoonHub

--- 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
+++ 프로그래머스/1/12931. 자릿수 더하기/자릿수 더하기.java
@@ -2,13 +2,13 @@ import java.util.*;
 
 public class Solution {
     public int solution(int n) {
+        String str = Integer.toString(n);
+        char[] arr = str.toCharArray();
+        
         int answer = 0;
-        while(n > 0) {
-            answer += n % 10;
-            n /= 10;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i] - '0';
         }
-   
-
         return answer;
     }
 }
