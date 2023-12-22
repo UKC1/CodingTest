971ab36118969f15d140926dcfd57bdb5969a368
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-22 13:41:08 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.91 ms, Memory: 72.9 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.java
@@ -1,10 +1,7 @@
+import java.util.Arrays;
+
 class Solution {
     public int solution(int[] numbers) {
-        int answer = 45;
-        for (int i = 0; i < numbers.length; i++) {
-            answer -= numbers[i];
-            
-        }
-        return answer;
+        return 45 - Arrays.stream(numbers).sum();
     }
 }
