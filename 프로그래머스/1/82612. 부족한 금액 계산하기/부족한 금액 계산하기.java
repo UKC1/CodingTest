e98ae9a62e72b7ffe87ff825cde10fca41f70b16
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-25 00:15:27 +0900

    [level 1] Title: 부족한 금액 계산하기, Time: 0.11 ms, Memory: 78.5 MB -BaekjoonHub

---
+++ 프로그래머스/1/82612. 부족한 금액 계산하기/부족한 금액 계산하기.java
@@ -0,0 +1,9 @@
+class Solution {
+    public long solution(int price, int money, int count) {
+        long answer = 0;
+        for (int i = 0; i < count; i++) {
+            answer += price * (i + 1);
+        }
+        return answer - money < 0 ? 0 : answer - money;
+    }
+}
