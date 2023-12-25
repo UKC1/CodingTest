29163e3d76f24d133a0461cdfc83c8f65733eba6
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-25 23:15:56 +0900

    [level 1] Title: 문자열 다루기 기본, Time: 0.04 ms, Memory: 75.3 MB -BaekjoonHub

--- 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
+++ 프로그래머스/1/12918. 문자열 다루기 기본/문자열 다루기 기본.java
@@ -8,12 +8,11 @@ class Solution {
         }
         
         for (int i = 0; i < arr.length; i++) {
-            if ((arr[i] < '0' || arr[i] > '9')) {
+            if (!Character.isDigit(arr[i])) {
                 answer = false;
                 break;
-            } 
+            }
         }
-        
         return answer;
     }
 }
