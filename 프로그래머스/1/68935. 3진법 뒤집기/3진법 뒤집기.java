09858c1f72ef40d08b72824e6ca80229e417be8b
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-26 00:42:27 +0900

    [level 1] Title: 3진법 뒤집기, Time: 0.05 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
+++ 프로그래머스/1/68935. 3진법 뒤집기/3진법 뒤집기.java
@@ -1,18 +1,11 @@
 class Solution {
-    public long solution(int n) {
-        long answer = 0;
+    public int solution(int n) {
         StringBuilder sb = new StringBuilder();
-        while (n > 0) {
+        while(n > 0) {
             sb.append(n % 3);
             n /= 3;
         }
         
-        long num = Long.parseLong(sb.toString());
-        int i = 0;
-        while (num > 0) {
-            answer += (num % 10) * (int) Math.pow(3, i++);
-            num /= 10;
-        }
-        return answer;
+        return Integer.parseInt(sb.toString(), 3);
     }
 }
