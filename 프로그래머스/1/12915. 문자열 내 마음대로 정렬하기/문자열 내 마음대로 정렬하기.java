586499e4ab526eed2f39aa5ad9a4f73698f1e759
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-27 11:54:04 +0900

    [level 1] Title: 문자열 내 마음대로 정렬하기, Time: 1.23 ms, Memory: 74.7 MB -BaekjoonHub

--- 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
+++ 프로그래머스/1/12915. 문자열 내 마음대로 정렬하기/문자열 내 마음대로 정렬하기.java
@@ -1,14 +1,38 @@
-import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.Collections;
+
+class Pair {
+    String originalString;
+    char nthChar;
+
+    public Pair(String originalString, int n) {
+        this.originalString = originalString;
+        this.nthChar = originalString.charAt(n);
+    }
+}
+
 class Solution {
     public String[] solution(String[] strings, int n) {
-        for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].charAt(n) + strings[i];
+        ArrayList<Pair> pairs = new ArrayList<>();
+
+        // Pair 객체를 생성하여 ArrayList에 추가
+        for (String s : strings) {
+            pairs.add(new Pair(s, n));
         }
-        
-        Arrays.sort(strings);
-         for (int i = 0; i < strings.length; i++) {
-            strings[i] = strings[i].substring(1);
+
+        // 사용자 정의 정렬 로직 적용
+        Collections.sort(pairs, (p1, p2) -> {
+            if (p1.nthChar == p2.nthChar) {
+                return p1.originalString.compareTo(p2.originalString);
+            }
+            return p1.nthChar - p2.nthChar;
+        });
+
+        // 정렬된 문자열을 결과 배열에 저장
+        String[] answer = new String[strings.length];
+        for (int i = 0; i < pairs.size(); i++) {
+            answer[i] = pairs.get(i).originalString;
         }
-        return strings;
+        return answer;
     }
 }
