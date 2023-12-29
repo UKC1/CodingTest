4a2abdd23546f37bd21eaf8c7cfa634e77f227c5
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2023-12-29 16:55:49 +0900

    [level unrated] Title: 평행, Time: 0.02 ms, Memory: 83.8 MB -BaekjoonHub

--- 프로그래머스/unrated/120875. 평행/평행.java
+++ 프로그래머스/unrated/120875. 평행/평행.java
@@ -1,41 +1,25 @@
 class Solution {
     public int solution(int[][] dots) {
-        int answer = 0;
-        double dx12 = dots[1][0] - dots[0][0];
-        double dy12 = dots[1][1] - dots[0][1];
-        double slope12 = dy12 / dx12; 
-        
-        double dx34 = dots[3][0] - dots[2][0];
-        double dy34 = dots[3][1] - dots[2][1];
-        double slope34 = dy34 / dx34; 
-        
-        double dx13 = dots[2][0] - dots[0][0];
-        double dy13 = dots[2][1] - dots[0][1];
-        double slope13 = dy13 / dx13; 
-        
-        double dx24 = dots[3][0] - dots[1][0];
-        double dy24 = dots[3][1] - dots[1][1];
-        double slope24 = dy24 / dx24; 
-        
-        double dx14 = dots[3][0] - dots[0][0];
-        double dy14 = dots[3][1] - dots[0][1];
-        double slope14 = dy14 / dx14; 
-        
-        double dx23 = dots[2][0] - dots[1][0];
-        double dy23 = dots[2][1] - dots[1][1];
-        double slope23 = dy23 / dx23; 
-        
-        if (slope12 == slope34) {
-            answer = 1;
-            System.out.println(slope12 + " " + slope34);
-        } else if (slope13 == slope24) {
-            answer = 1;
-            System.out.println(slope13 + " " + slope24);
-        } else if (slope14 == slope23) {
-            answer = 1;
-            System.out.println(slope14 + " " + slope23);
+        int index = 0;
+        if (
+            calculateSlope(dots[0], dots[1]) == calculateSlope(dots[2], dots[3])
+            ) {
+            return 1;
+        } else if (
+            calculateSlope(dots[0], dots[2]) == calculateSlope(dots[1], dots[3])
+        ) {
+            return 1;
+        } else if (
+            calculateSlope(dots[0], dots[3]) == calculateSlope(dots[1], dots[2])
+        ) {
+            return 1;
         }
-        
-        return answer;
+        return 0;
+    }
+
+    private double calculateSlope(int[] dot1, int[] dot2) {
+        double dy = dot2[1] - dot1[1];
+        double dx = dot2[0] - dot1[0];
+        return dy / dx;
     }
 }
