8dd3a6399755eb6a2f39381e9cea5067604e3ef8
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-02 23:55:49 +0900

    [level 1] Title: 소수 만들기, Time: 0.03 ms, Memory: 78.9 MB -BaekjoonHub

---
+++ 프로그래머스/1/12977. 소수 만들기/소수 만들기.java
@@ -0,0 +1,32 @@
+class Solution {
+    public int solution(int[] nums) {
+        int answer = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                for (int k = j + 1; k < nums.length; k++) {
+                    if (prime(nums[i] + nums[j] + nums[k])) {
+                        answer++;
+                    }
+                }
+            }
+        }
+
+        return answer;
+    }
+    
+    public boolean prime(int num) {
+        int cnt = 0;
+        for (int i = 1; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) {
+                cnt++;
+            }
+        }
+        
+        if (cnt == 1) {
+            return true;
+        }
+        
+        return false;
+    }
+}
