765abb40b3beeaf701bb48e4c1e3364a196ffca3
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-04 17:03:12 +0900

    [level 1] Title: 실패율, Time: 0.82 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42889. 실패율/실패율.java
+++ 프로그래머스/1/42889. 실패율/실패율.java
@@ -1,44 +1,53 @@
 import java.util.Arrays;
-import java.util.Comparator;
+import java.util.ArrayList;
 
 class Solution {
     public int[] solution(int N, int[] stages) {
-        int[] stageReached = new int[N + 1];
-        for (int stage : stages) {
-            if (stage <= N) {
-                stageReached[stage]++;
+       Arrays.sort(stages);
+       double[] arr = new double[N];
+       int[] answer = new int[N];
+        
+        for (int i = 0; i < stages.length; i++) {
+            if (stages[i] > N) {
+                continue;
+            } else {
+                arr[stages[i] - 1] ++;
             }
         }
         
-        int totalPlayers = stages.length;
-        StageFailure[] failures = new StageFailure[N];
-        for (int i = 0; i < N; i++) {
-            double failureRate = totalPlayers > 0 ? (double)stageReached[i + 1] / totalPlayers : 0;
-            failures[i] = new StageFailure(i + 1, failureRate);
-            totalPlayers -= stageReached[i + 1];
+        ArrayList<Double> list = new ArrayList<>();
+        double n = stages.length;
+        double ck = 0;
+        for (int i = 0; i < arr.length; i++) {
+            ck = arr[i];
+            
+            arr[i] = arr[i] / n;
+            list.add(arr[i]);
+            if (n - ck > 0) {
+                n -= ck;
+            }      
         }
         
-        Arrays.sort(failures, Comparator.comparingDouble(StageFailure::getFailureRate).reversed());
+       
+        double[] arrSorted = new double[N];
+        System.arraycopy(arr, 0, arrSorted, 0, arr.length);
+        Arrays.sort(arrSorted);
+        System.out.println(Arrays.toString(arrSorted)); 
+        System.out.println(Arrays.toString(arr));
         
-        int[] answer = new int[N];
-        for (int i = 0; i < N; i++) {
-            answer[i] = failures[i].stage;
+
+        for (int i = 0; i < arr.length; i++) {
+            double max = arrSorted[arr.length - 1 - i];
+            for (int j = 0; j < list.size(); j++) {
+                if (max == list.get(j)) {
+                    answer[i] = j + 1;
+              
+                    list.set(j, -1.0);
+                    break;
+                }
+            }
         }
-        
+
         return answer;
-    }
-
-    static class StageFailure {
-        int stage;
-        double failureRate;
-
-        StageFailure(int stage, double failureRate) {
-            this.stage = stage;
-            this.failureRate = failureRate;
-        }
-
-        double getFailureRate() {
-            return failureRate;
-        }
     }
 }
