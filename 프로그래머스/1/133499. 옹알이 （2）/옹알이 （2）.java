52288298c65d166c0711141144c979346d95241b
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-04 19:33:12 +0900

    [level 1] Title: 옹알이 (2), Time: 1.03 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
+++ 프로그래머스/1/133499. 옹알이 （2）/옹알이 （2）.java
@@ -7,9 +7,16 @@ class Solution {
                        .replace("woo", "3")
                        .replace("ma", "4");
 
-            if (word.matches("^[1-4]+$") && !word.matches(".*(11|22|33|44).*")) {
+            if (word.contains("11") || word.contains("22") || word.contains("33") || word.contains("44")) {
+                continue;
+            }
+            
+            if (word.replace("1","").replace("2", "").replace("3", "").replace("4", "").isEmpty()) {
                 answer++;
             }
+            // if (word.matches("^[1-4]+$") && !word.matches(".*(11|22|33|44).*")) {
+            //     answer++;
+            // }
         }
         
         return answer;
