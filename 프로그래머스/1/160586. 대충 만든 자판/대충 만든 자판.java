ac427c0766a3e88ad8e67a7533596e11c9bf6d94
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-08 11:09:36 +0900

    [level 1] Title: 대충 만든 자판, Time: 1.86 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
+++ 프로그래머스/1/160586. 대충 만든 자판/대충 만든 자판.java
@@ -2,32 +2,39 @@ import java.util.HashMap;
 
 class Solution {
     public int[] solution(String[] keymap, String[] targets) {
-        HashMap<Character, Integer> map = new HashMap();
-        for (int i = 0; i < keymap.length; i++) {
-            for (int j = 0; j < keymap[i].length(); j++) {
-                if (map.get(keymap[i].charAt(j)) == null) {
-                    map.put(keymap[i].charAt(j), j + 1);
-                } else if (map.get(keymap[i].charAt(j)) > j + 1) {
-                    map.put(keymap[i].charAt(j), j + 1);
-                } 
-            }
-        }
-        
-        System.out.println(map);
+        HashMap<Character, Integer> map = populateKeyMap(keymap);
+
         int[] answer = new int[targets.length];
         for (int i = 0; i < targets.length; i++) {
-            for (int j = 0; j< targets[i].length(); j++) {
-                if (map.get(targets[i].charAt(j)) == null) {
-                    answer[i] = 0;
-                    break;
-                }
-                answer[i] += map.get(targets[i].charAt(j));
-            }
-            if (answer[i] == 0) {
-                answer[i] = -1;
+            answer[i] = calculateKeyPresses(map, targets[i]);
+        }
+
+        return answer;
+    }
+
+    private HashMap<Character, Integer> populateKeyMap(String[] keymap) {
+    HashMap<Character, Integer> map = new HashMap<>();
+    for (int i = 0; i < keymap.length; i++) {
+        for (int j = 0; j < keymap[i].length(); j++) {
+            char currentChar = keymap[i].charAt(j);
+            int keyPresses = j + 1; // 키 입력 횟수 계산
+            if (!map.containsKey(currentChar) || map.get(currentChar) > keyPresses) {
+                map.put(currentChar, keyPresses); // 더 적은 키 입력 횟수로 업데이트
             }
         }
-                
-        return answer;
+    }
+    return map;
+}
+
+    private int calculateKeyPresses(HashMap<Character, Integer> map, String target) {
+        int totalKeyPresses = 0;
+        for (char ch : target.toCharArray()) {
+            Integer keyPresses = map.get(ch);
+            if (keyPresses == null) {
+                return -1;
+            }
+            totalKeyPresses += keyPresses;
+        }
+        return totalKeyPresses;
     }
 }
