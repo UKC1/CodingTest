69fd0bf338f843ec9a987cbdd20e34898e826f8e
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-08 15:19:58 +0900

    [level 1] Title: 둘만의 암호, Time: 10.42 ms, Memory: 72.1 MB -BaekjoonHub

--- 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
+++ 프로그래머스/1/155652. 둘만의 암호/둘만의 암호.java
@@ -1,26 +1,26 @@
-import java.util.Arrays;
 class Solution {
     public String solution(String s, String skip, int index) {
         String answer = "";
         int[] arr = new int[26];
-        
+
         for (int i = 0; i < skip.length(); i++) {
             arr[skip.charAt(i) - 'a']++;
         }
-        
+
         for (int i = 0; i < s.length(); i++) {
             char c = s.charAt(i);
-            int idx = 0;
-            while(idx < index || arr[(c - 'a') % 26] == 1) {
-                idx++;
-                c = (char) ((c + 1 - 'a') % 26 + 'a');
-                if(arr[(c - 'a') % 26] == 1) {
-                    idx--;
+
+                int shiftedIndex = 0;
+                while (shiftedIndex < index || arr[c - 'a'] == 1) {
+                    c = (char) ((c - 'a' + 1) % 26 + 'a');
+                    shiftedIndex++;
+                    if (arr[c - 'a'] == 1) {
+                        shiftedIndex--;
+                    }
+                    
                 }
-            }
-            answer += c;
+                answer += c;
         }
-        
         return answer;
     }
 }
