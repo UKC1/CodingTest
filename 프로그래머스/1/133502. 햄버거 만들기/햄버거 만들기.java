78a5ec23ff5c23c4597c3abec886a26c3ecc23ad
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-09 14:45:47 +0900

    [level 1] Title: 햄버거 만들기, Time: 0.15 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
+++ 프로그래머스/1/133502. 햄버거 만들기/햄버거 만들기.java
@@ -1,44 +1,23 @@
 import java.util.Stack;
 class Solution {
     public int solution(int[] ingredient) {
-        int answer = 0;
         Stack<Integer> stack = new Stack();
+        int answer = 0;
         for (int i = 0; i < ingredient.length; i++) {
             stack.push(ingredient[i]);
-            while(stack.size() > 3) {
-                if(stack.peek() == 1) {
-                    int four = stack.pop();
-                    if(stack.peek() == 3) {
-                    	int three = stack.pop();
-                        if(stack.peek() == 2) {
-                            int two = stack.pop();
-                            if (stack.peek() == 1) {
-                                int one = stack.pop();
-                                answer++;
-                                break;
-                            } else {
-                                stack.push(two);
-                                stack.push(three);
-                                stack.push(four);
-                                break;
-                            }
-                        } else {
-                            stack.push(three);
-                            stack.push(four);
-                            break;
-                        }
-                    } else {
-                        stack.push(four);
-                        break;
-                    }
-                } else {
-                    break;
+            if(stack.size() > 3) {
+                if(stack.get(stack.size() - 4) == 1 
+                   && stack.get(stack.size() - 3) == 2
+                   && stack.get(stack.size() - 2) == 3 
+                   && stack.get(stack.size() - 1) == 1) {
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    stack.pop();
+                    answer++;
                 }
-            } 
-            
-               
+            }
         }
-       
         return answer;
     }
 }
