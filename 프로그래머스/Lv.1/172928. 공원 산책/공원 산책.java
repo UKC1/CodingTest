1224f50c308f42e0a8f0c81881cd75b21e2c6ae0
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-12 09:29:53 +0900

    [level Lv.1] Title: 공원 산책, Time: 0.15 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/Lv.1/172928. 공원 산책/공원 산책.java
+++ 프로그래머스/Lv.1/172928. 공원 산책/공원 산책.java
@@ -1,85 +1,73 @@
 class Solution {
     public int[] solution(String[] park, String[] routes) {
-        int[] answer = new int[2];
+        int[] position = new int[2]; // 현재 위치
         for (int i = 0; i < park.length; i++) {
             for (int j = 0; j < park[i].length(); j++) {
                 if (park[i].charAt(j) == 'S') {
-                    answer[0] = i;
-                    answer[1] = j;
+                    position[0] = i;
+                    position[1] = j;
                     break;
                 }
             }
         }
-        
-        for (int i = 0; i < routes.length; i++) {
-            String direction = routes[i].split(" ")[0];
-            int count = Integer.parseInt(routes[i].split(" ")[1]);
-            char c = park[answer[0]].charAt(answer[1]);
-            int ck = 0;
-            
-            if (direction.equals("E")) {
-                for (int j = 0; j < count; j++) {
-                    if (answer[1] + 1 < park[0].length()) {
-                        answer[1]++;
-                        ck++;
-                        c = park[answer[0]].charAt(answer[1]);
-                        if (c == 'X') {
-                            answer[1] -= ck;
-                            break;
-                        }
-                    } else {
-                        answer[1] -= ck;
-                        break;
-                    }
-                }
-            } else if (direction.equals("W")) {
-                for (int j = count - 1; j >= 0; j--) {
-                    if (answer[1] - 1 >= 0) {
-                        answer[1]--;
-                        ck++;
-                        c = park[answer[0]].charAt(answer[1]);
-                        if (c == 'X') {
-                            answer[1] += ck;
-                            break;
-                        }
-                    } else {
-                        answer[1] += ck;
-                        break;
-                    }
-                }
-            } else if (direction.equals("S")) {
-                 for (int j = 0; j < count; j++) {
-                    if (answer[0] + 1 < park.length) {
-                        answer[0]++;
-                        ck++;
-                        c = park[answer[0]].charAt(answer[1]);
-                        if (c == 'X') {
-                            answer[0] -= ck;
-                            break;
-                        }
-                    } else {
-                        answer[0] -= ck;
-                        break;
-                    }
-                }
-            } else if (direction.equals("N")) {
-                for (int j = count - 1; j >= 0; j--) {
-                    if (answer[0] - 1 >= 0) {
-                        answer[0]--;
-                        ck++;
-                        c = park[answer[0]].charAt(answer[1]);
-                        if (c == 'X') {
-                            answer[0] += ck;
-                            break;
-                        }
-                    } else {
-                        answer[0] += ck;
-                        break;
-                    }
-                }
-            } 
-            
+
+        for (String route : routes) {
+            String[] parts = route.split(" ");
+            String direction = parts[0];
+            int count = Integer.parseInt(parts[1]);
+            move(park, position, direction, count);
         }
-        return answer;
+
+        return position;
+    }
+
+    private void move(String[] park, int[] position, String direction, int count) {
+        int originalRow = position[0];
+        int originalCol = position[1];
+        int tempRow = position[0];
+        int tempCol = position[1];
+        int rowLength = park.length;
+        int colLength = park[0].length();
+
+        for (int i = 0; i < count; i++) {
+            switch (direction) {
+                case "E":
+                    if (tempCol + 1 < colLength && park[tempRow].charAt(tempCol + 1) != 'X') tempCol++;
+                    else {
+                        position[0] = originalRow;
+                        position[1] = originalCol;
+                        return;
+                    }
+                    break;
+                case "W":
+                    if (tempCol - 1 >= 0 && park[tempRow].charAt(tempCol - 1) != 'X') tempCol--;
+                    else {
+                        position[0] = originalRow;
+                        position[1] = originalCol;
+                        return;
+                    }
+                    break;
+                case "S":
+                    if (tempRow + 1 < rowLength && park[tempRow + 1].charAt(tempCol) != 'X') tempRow++;
+                    else {
+                        position[0] = originalRow;
+                        position[1] = originalCol;
+                        return;
+                    }
+                    break;
+                case "N":
+                    if (tempRow - 1 >= 0 && park[tempRow - 1].charAt(tempCol) != 'X') tempRow--;
+                    else {
+                        position[0] = originalRow;
+                        position[1] = originalCol;
+                        return;
+                    }
+                    break;
+            }
+        }
+        
+        // 이동 성공적으로 마친 경우, 위치 업데이트
+        position[0] = tempRow;
+        position[1] = tempCol;
     }
 }
