6dc48a9376c589d65894552dfc74c24806534871
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-12 17:31:58 +0900

    [level unrated] Title: 가장 많이 받은 선물, Time: 10.03 ms, Memory: 81.2 MB -BaekjoonHub

--- 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
+++ 프로그래머스/unrated/258712. 가장 많이 받은 선물/가장 많이 받은 선물.java
@@ -1,43 +1,43 @@
 import java.util.HashMap;
-import java.util.Arrays;
-
 class Solution {
     public int solution(String[] friends, String[] gifts) {
-        HashMap<String, Integer> map = new HashMap();
+        HashMap<String, Integer> map = new HashMap<>();
         int[] giftIdx = new int[friends.length];
-        int[] getGift = new int[friends.length];
-        
-        for(int i = 0; i < friends.length; i++) {
+        int maxGifts = 0; // 최대 선물 횟수를 저장하는 변수
+
+        for (int i = 0; i < friends.length; i++) {
             map.put(friends[i], i);
         }
-        
+
         int[][] arr = new int[friends.length][friends.length];
-        
-        for (int i = 0; i < gifts.length; i++) {
-            int post = map.get(gifts[i].split(" ")[0]);
-            int get = map.get(gifts[i].split(" ")[1]);
-            arr[post][get]++;
+
+        for (String gift : gifts) {
+            String[] parts = gift.split(" ");
+            int giver = map.get(parts[0]);
+            int receiver = map.get(parts[1]);
+            arr[giver][receiver]++;
         }
-    
+
         for (int i = 0; i < arr.length; i++) {
+            int giftsGiven = 0; // 현재 친구가 준 선물 수
+            int giftsReceived = 0; // 현재 친구가 받은 선물 수
             for (int j = 0; j < arr.length; j++) {
-                giftIdx[i] += arr[i][j];
-                giftIdx[i] -= arr[j][i];
+                giftsGiven += arr[i][j];
+                giftsReceived += arr[j][i];
             }
+            giftIdx[i] = giftsGiven - giftsReceived;
         }
-        
+
         for (int i = 0; i < arr.length; i++) {
+            int currentGifts = 0; // 현재 친구의 선물 횟수
             for (int j = 0; j < arr.length; j++) {
-               if(arr[i][j] > arr[j][i]) {
-                   getGift[i]++;
-               } else if(i != j && arr[i][j] == arr[j][i]) {
-                   if(giftIdx[i] > giftIdx[j]) {
-                       getGift[i]++;
-                   } 
-               }
+                if (i != j && (arr[i][j] > arr[j][i] || (arr[i][j] == arr[j][i] && giftIdx[i] > giftIdx[j]))) {
+                    currentGifts++;
+                }
             }
+            maxGifts = Math.max(maxGifts, currentGifts); // 최대 선물 횟수 갱신
         }
-        Arrays.sort(getGift);
-        return getGift[getGift.length - 1];
+
+        return maxGifts;
     }
 }
