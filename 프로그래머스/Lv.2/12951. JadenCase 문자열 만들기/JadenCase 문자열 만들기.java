52d0a0935cf2e5f63c8faf9a9c5edcad1f738713
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-13 22:38:38 +0900

    [level Lv.2] Title: JadenCase 문자열 만들기, Time: 1.18 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
+++ 프로그래머스/Lv.2/12951. JadenCase 문자열 만들기/JadenCase 문자열 만들기.java
@@ -1,24 +1,32 @@
 class Solution {
     public String solution(String s) {
-        StringBuilder sb = new StringBuilder();
-        char[] arr = s.toCharArray();
-        boolean isFirst = true;
-        boolean isMiddleBlank;
-        
-        for (int i = 0; i < arr.length; i++) {
-            if(arr[i] == ' ') {
-                isFirst = true;
-                sb.append(arr[i]);
-            } else {
-                if(isFirst) {
-                    sb.append(Character.toUpperCase(arr[i]));
-                    isFirst = false;
-                } else {
-                    sb.append(Character.toLowerCase(arr[i]));
+        String answer = s.toLowerCase();
+        String check = "";
+        // boolean ch = true;
+        String[] str = answer.split(" ");
+        for (int i = 0; i < str.length; i++) {
+            // if (str[i].isEmpty()) {
+            //      check += " ";
+            //      ch = false;} 
+            if (!str[i].isEmpty()) {
+                check += str[i].substring(0, 1).toUpperCase();
+                check += str[i].substring(1);
+                if (i != str.length - 1) {
+                    check += " ";
                 }
-            }
-         }
-        
-        return sb.toString();
+                // ch = true;
+
+             } else {
+                 check += str[i];
+                 if (i != str.length - 1) {
+                     check += " ";
+                 }
+                // ch = true;
+             }
+        }
+        if (s.length() != check.length()) {
+            check += " ";
+        }
+        return check;
     }
 }
