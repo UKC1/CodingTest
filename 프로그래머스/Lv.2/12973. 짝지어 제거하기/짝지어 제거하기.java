6c9983df59ce7191be3a590618160cbc37f3dfaf
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-15 09:40:56 +0900

    [level Lv.2] Title: 짝지어 제거하기, Time: 53.34 ms, Memory: 60.9 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12973. 짝지어 제거하기/짝지어 제거하기.java
+++ 프로그래머스/Lv.2/12973. 짝지어 제거하기/짝지어 제거하기.java
@@ -3,15 +3,12 @@ class Solution
 {
     public int solution(String s)
     {
-        boolean twice = false;
         Stack<Character> stack = new Stack();
         for (char c : s.toCharArray()) {
             if (stack.isEmpty()) {
                 stack.push(c);    
             } else {
                 if(stack.peek() == c) {
-                    stack.push(c);
-                    stack.pop();
                     stack.pop();
                 } else {
                     stack.push(c);
