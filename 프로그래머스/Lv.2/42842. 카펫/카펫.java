bd1ebf5543ff3bf7e67ccc381774a4d8367fe94c
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-16 01:54:26 +0900

    [level Lv.2] Title: 카펫, Time: 0.02 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42842. 카펫/카펫.java
+++ 프로그래머스/Lv.2/42842. 카펫/카펫.java
@@ -1,24 +1,21 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int[] answer = new int[2];
-        int width = brown + yellow;
-        int yellowRec = yellow;
-        
-        
-        for (int i = 1; i <= Math.sqrt(width); i++) {
-            if (yellow % i == 0) {
-                int wid = yellow / i;
-                int len = i;
-                while (wid * len < width) {
-                    wid += 2;
-                    len += 2;
+        int totalArea = brown + yellow; // 전체 면적 계산
+
+        for (int i = 1; i <= Math.sqrt(totalArea); i++) {
+            if (totalArea % i == 0) {
+                // 세로 길이
+                int height = i;
+                // 가로 길이
+                int width = totalArea / i;
+
+                // 갈색 타일의 수를 고려한 조건 확인
+                if ((width - 2) * (height - 2) == yellow) {
+                    return new int[] {width, height};
                 }
-                if (wid * len == width) {
-                    return new int[] {wid, len};
-                }
-                
-            } 
+            }
         }
-        return answer;
+
+        return new int[2]; // 조건에 맞는 카펫 크기를 찾지 못한 경우 빈 배열 반환
     }
 }
