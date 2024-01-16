087ab7c42c4f0942037ed642f2d957247652dfb2
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-16 15:39:57 +0900

    [level Lv.2] Title: 예상 대진표, Time: 0.03 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12985. 예상 대진표/예상 대진표.java
+++ 프로그래머스/Lv.2/12985. 예상 대진표/예상 대진표.java
@@ -1,11 +1,10 @@
-class Solution
-{
-    public int solution(int n, int a, int b)
-    {
+class Solution {
+    public int solution(int n, int a, int b) {
         int answer = 0;
-        while(Math.abs(a - b) > 0) {
-            a = a / 2 + a % 2;
-            b = b / 2 + b % 2;
+
+        while (a != b) {
+            a = (a + 1) / 2;
+            b = (b + 1) / 2;
             answer++;
         }
 
