4c4ba96bb0c628e08e68b0ef353cb44a9db9eefb
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-16 17:55:13 +0900

    [level Lv.2] Title: 멀리 뛰기, Time: 0.14 ms, Memory: 74.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/12914. 멀리 뛰기/멀리 뛰기.java
+++ 프로그래머스/Lv.2/12914. 멀리 뛰기/멀리 뛰기.java
@@ -4,10 +4,12 @@ class Solution {
         long[] dp = new long[n + 1];
         dp[0] = 1;
         dp[1] = 1;
+        int mod = 1234567;
+            
         for (int i = 2; i <= n; i++) {
-            dp[i] = dp[i - 1] % 1234567 + dp[i - 2] % 1234567;
+            dp[i] = (dp[i - 1] + dp[i - 2]) % mod;
         }
         
-        return dp[n] % 1234567;
+        return dp[n];
     }
 }
