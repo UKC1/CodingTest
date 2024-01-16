53722a3195afa149bc68953c5f483856d4b62663
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-16 17:58:05 +0900

    [level Lv.2] Title: 귤 고르기, Time: 60.70 ms, Memory: 135 MB -BaekjoonHub

--- 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.java
+++ 프로그래머스/Lv.2/138476. 귤 고르기/귤 고르기.java
@@ -2,8 +2,7 @@ import java.util.Arrays;
 class Solution {
     public int solution(int k, int[] tangerine) {
         int answer = 0;
-        Arrays.sort(tangerine);
-        int[] arr = new int[tangerine[tangerine.length - 1] + 1];
+        int[] arr = new int[10000001];
         
         for (int i = 0; i < tangerine.length; i++) {
             arr[tangerine[i]]++;
