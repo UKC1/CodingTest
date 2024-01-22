836eb3c8f16ad7807431ed86321cd67331542b99
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-01-23 00:55:35 +0900

    [level 2] Title: 행렬의 곱셈, Time: 1.46 ms, Memory: 78 MB -BaekjoonHub

--- 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
+++ 프로그래머스/2/12949. 행렬의 곱셈/행렬의 곱셈.java
@@ -1,16 +1,13 @@
 class Solution {
     public int[][] solution(int[][] arr1, int[][] arr2) {
         int[][] answer = new int[arr1.length][arr2[0].length];
-        for (int i = 0; i < arr1.length; i++){
-            int idx = 0;
-            while(idx < arr2[0].length) {
-                int sum = 0;
-                for (int j = 0; j < arr1[0].length; j++) {
-                    sum += arr1[i][j] * arr2[j][idx];
+        for (int i = 0; i < arr1.length; i++) {
+            for (int j = 0; j < arr2[0].length; j++) {
+                for (int k = 0; k < arr2.length; k++) {
+                    answer[i][j] += arr1[i][k] * arr2[k][j];
                 }
-                answer[i][idx++] = sum;
             }
-        }         
+        }
         return answer;
     }
 }
