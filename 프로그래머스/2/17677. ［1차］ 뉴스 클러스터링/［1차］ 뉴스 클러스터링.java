fc759778eacf2ad44626b816d3546e1ac3efe7c8
Kang Min Kim <99055686+UKC1@users.noreply.github.com>
2024-03-21 17:40:52 +0900

    [level 2] Title: [1차] 뉴스 클러스터링, Time: 1.74 ms, Memory: 73.7 MB -BaekjoonHub

--- 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
+++ 프로그래머스/2/17677. ［1차］ 뉴스 클러스터링/［1차］ 뉴스 클러스터링.java
@@ -1,39 +1,43 @@
 import java.util.*;
+
 class Solution {
     public int solution(String str1, String str2) {
-        String patternString = "^[a-z]+$";
+        str1 = str1.toLowerCase();
+        str2 = str2.toLowerCase();
+
         List<String> list1 = new ArrayList<>();
         List<String> list2 = new ArrayList<>();
+      
         for (int i = 0; i < str1.length() - 1; i++) {
-            String tmp1 = str1.toLowerCase().substring(i, i + 2);
-            if (tmp1.matches(patternString)) {
+            String tmp1 = str1.substring(i, i + 2);
+            if (tmp1.matches("[a-z]{2}")) {
                 list1.add(tmp1);
             }
         }
-        
+
         for (int i = 0; i < str2.length() - 1; i++) {
-            String tmp2 = str2.toLowerCase().substring(i, i + 2);
-            if (tmp2.matches(patternString)) {
+            String tmp2 = str2.substring(i, i + 2);
+            if (tmp2.matches("[a-z]{2}")) {
                 list2.add(tmp2);
             }
         }
-        
-        double answer = list2.size();
-        for (String s : list1) {
-            list2.remove(s);
+
+        int intersect = 0;
+        for (String s : new ArrayList<>(list1)) {
+            if (list2.remove(s)) {
+                intersect++;
+            }
         }
+
+        int union = list1.size() + list2.size();
         
-        double intersect = answer - list2.size();
-        double uniform = list2.size() + list1.size();
-        
-        answer = intersect / uniform;
-        // System.out.println(list1);
-        // System.out.println(list2);
-        // System.out.println(intersect);
-        // System.out.println(uniform);
-        if (uniform == 0) {
-            return 65536;
+        double answer;
+        if (union == 0) {
+            answer = 1; 
+        } else {
+            answer = (double) intersect / union;
         }
+
         return (int) (answer * 65536);
     }
 }
