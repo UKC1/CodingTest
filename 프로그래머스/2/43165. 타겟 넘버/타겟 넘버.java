19ed3642e83696dba283bca21cc6e3f8e9af2708
Kang Min Kim <lnewgatel@gmail.com>
2024-04-12 12:09:45 +0900

    [level 2] Title: 타겟 넘버, Time: 1.14 ms, Memory: 70.3 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,17 +1,18 @@
 class Solution {
-    int count = 0;
+    int cnt = 0;
     public int solution(int[] numbers, int target) {
         dfs(numbers, 0, 0, target);
-        return count;
+        return cnt;
     }
     
     public void dfs(int[] numbers, int idx, int sum, int target) {
         if (idx == numbers.length) {
             if (sum == target) {
-                count++;
+                cnt++;
             }
             return;
         }
+        
         dfs(numbers, idx + 1, sum + numbers[idx], target);
         dfs(numbers, idx + 1, sum - numbers[idx], target);
     }
