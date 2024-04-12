eaf331d0b799273c95023b98cd946b9a0ea7d562
Kang Min Kim <lnewgatel@gmail.com>
2024-04-12 11:57:15 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 5.77 ms, Memory: 55.3 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -1,40 +1,38 @@
-import java.util.*;
+import java.util.Queue;
+import java.util.LinkedList;
 
 class Solution {
     public int solution(int[][] maps) {
+        int answer = 0;
         int n = maps.length, m = maps[0].length;
+        Queue<int[] > queue = new LinkedList();
         boolean[][] visited = new boolean[n][m];
-        Queue<int[]> queue = new LinkedList<>();
         
-        // 시작 위치와 거리 초기화
         queue.offer(new int[] {0, 0, 1});
         visited[0][0] = true;
         
-        // BFS 실행
-        while (!queue.isEmpty()) {
-            int[] current = queue.poll();
-            int x = current[0], y = current[1], dist = current[2];
+        while(!queue.isEmpty()) {
+            int[] check = queue.poll();
+            int x = check[0];
+            int y = check[1];
+            int dist = check[2];
             
-            // 목표 도착 시 거리 반환
             if (x == n - 1 && y == m - 1) {
                 return dist;
             }
-            
-            int[] dx = {1, 0, -1, 0};
-            int[] dy = {0, 1, 0, -1};
+            int[] dx = {-1, 0, 1, 0};
+            int[] dy = {0, -1, 0, 1};
             
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
                 
-                if (nx >= 0 && ny >= 0 && nx < n && ny < m && maps[nx][ny] == 1 && !visited[nx][ny]) {
+                if(nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
                     visited[nx][ny] = true;
                     queue.offer(new int[] {nx, ny, dist + 1});
                 }
             }
         }
-        
-        // 목표 도착 불가 시
         return -1;
     }
 }
