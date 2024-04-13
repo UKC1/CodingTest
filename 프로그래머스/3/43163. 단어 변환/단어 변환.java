8d6ded3c7d6048a8815d4212d749a63cbf2b810a
Kang Min Kim <lnewgatel@gmail.com>
2024-04-14 00:59:21 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 79 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,44 @@
-import java.util.LinkedList;
-import java.util.Queue;
+import java.util.*;
 
 class Solution {
-    public int solution(String begin, String target, String[] words) {
-        Queue<Node> queue = new LinkedList<>();
-        boolean[] visited = new boolean[words.length];
-        queue.offer(new Node(begin, 0));
-
-        while (!queue.isEmpty()) {
-            Node current = queue.poll();
-            if (current.word.equals(target)) {
-                return current.depth;
-            }
-
-            for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canChange(current.word, words[i])) {
-                    visited[i] = true;
-                    queue.offer(new Node(words[i], current.depth + 1));
-                }
-            }
-        }
-
-        return 0;  // target을 만들 수 없는 경우
-    }
-
-    private boolean canChange(String current, String next) {
-        int count = 0;
-        for (int i = 0; i < current.length(); i++) {
-            if (current.charAt(i) != next.charAt(i)) {
-                count++;
-            }
-        }
-        return count == 1;  // 한 글자만 다른 경우 true
-    }
-
     class Node {
         String word;
         int depth;
-
-        Node(String word, int depth) {
+        public Node(String word, int depth) {
             this.word = word;
             this.depth = depth;
         }
+    }
+    public int solution(String begin, String target, String[] words) {
+        Queue<Node> queue = new LinkedList();
+        boolean[] visited = new boolean[words.length];
+        queue.offer(new Node(begin, 0));
+        
+        while(!queue.isEmpty()) {
+            Node currentNode = queue.poll();
+            if(currentNode.word.equals(target)) {
+                return currentNode.depth;
+            }
+            
+            for (int i = 0; i < words.length; i++) {
+                if (canChange(currentNode.word, words[i]) && !visited[i]) {
+                    visited[i] = true;
+                    queue.offer(new Node(words[i], currentNode.depth + 1));
+                    
+                }
+            }
+            
+        }
+        return 0;
+    }
+    
+    public boolean canChange(String begin, String word) {
+        int cnt = 0;
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) != word.charAt(i)) {
+                cnt++;
+            }
+        }
+        return cnt == 1;
     }
 }
