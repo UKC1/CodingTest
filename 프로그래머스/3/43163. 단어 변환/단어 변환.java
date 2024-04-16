0de88bfd5e58895cc25f020b3f75525736ce9d47
Kang Min Kim <lnewgatel@gmail.com>
2024-04-16 10:51:50 +0900

    [level 3] Title: 단어 변환, Time: 0.40 ms, Memory: 75.1 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -3,39 +3,39 @@ import java.util.*;
 class Solution {
     class Node {
         String word;
-        int depth;
-        public Node(String word, int depth) {
+        int cnt;
+        Node(String word, int cnt) {
             this.word = word;
-            this.depth = depth;
+            this.cnt = cnt;
         }
     }
     public int solution(String begin, String target, String[] words) {
+        int answer = 0;
         Queue<Node> queue = new LinkedList();
-        boolean[] visited = new boolean[words.length];
         queue.offer(new Node(begin, 0));
-        
-        while(!queue.isEmpty()) {
+        boolean[] visited = new boolean[words.length];
+        while (!queue.isEmpty()) {
             Node currentNode = queue.poll();
-            if(currentNode.word.equals(target)) {
-                return currentNode.depth;
+            if (target.equals(currentNode.word)) {
+                return currentNode.cnt;
             }
             
             for (int i = 0; i < words.length; i++) {
-                if (canChange(currentNode.word, words[i]) && !visited[i]) {
+                if (!visited[i] && canChange(currentNode.word, words[i])) {
                     visited[i] = true;
-                    queue.offer(new Node(words[i], currentNode.depth + 1));
-                    
-                }
+                    queue.offer(new Node(words[i], currentNode.cnt + 1)); 
+                }   
             }
-            
+        
         }
-        return 0;
+      
+        return answer;
     }
     
     public boolean canChange(String begin, String word) {
         int cnt = 0;
         for (int i = 0; i < begin.length(); i++) {
-            if (begin.charAt(i) != word.charAt(i)) {
+            if(begin.charAt(i) != word.charAt(i)) {
                 cnt++;
             }
         }
