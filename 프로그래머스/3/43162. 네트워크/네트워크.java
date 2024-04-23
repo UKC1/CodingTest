ef53660c72ec5375d4b14a987fa77171e3faf7d4
Kang Min Kim <lnewgatel@gmail.com>
2024-04-23 10:59:00 +0900

    [level 3] Title: 네트워크, Time: 0.12 ms, Memory: 74.4 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -4,17 +4,16 @@ class Solution {
         boolean[] visited = new boolean[n];
         for (int i = 0; i < n; i++) {
             if (!visited[i]) {
-                dfs(computers, visited, i);
                 answer++;
+                dfs(computers, visited, i);
             }
         }
         return answer;
     }
-    
     public void dfs(int[][] computers, boolean[] visited, int start) {
         visited[start] = true;
         for (int i = 0; i < visited.length; i++) {
-            if (computers[start][i] == 1 && !visited[i]) {
+            if(computers[start][i] == 1 && !visited[i]) {
                 dfs(computers, visited, i);
             }
         }
