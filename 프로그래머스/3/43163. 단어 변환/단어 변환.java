ecbf76d27089ea9c38384651acca6b832da4ea5a
Kang Min Kim <lnewgatel@gmail.com>
2024-04-23 11:17:06 +0900

    [level 3] Title: 단어 변환, Time: 0.28 ms, Memory: 74 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -10,11 +10,11 @@ class Solution {
         }
     }
     public int solution(String begin, String target, String[] words) {
-        int answer = 0;
+        boolean[] visited = new boolean[words.length];
         Queue<Node> queue = new LinkedList();
         queue.offer(new Node(begin, 0));
-        boolean[] visited = new boolean[words.length];
-        while (!queue.isEmpty()) {
+        
+        while(!queue.isEmpty()) {
             Node currentNode = queue.poll();
             if (target.equals(currentNode.word)) {
                 return currentNode.cnt;
@@ -23,19 +23,17 @@ class Solution {
             for (int i = 0; i < words.length; i++) {
                 if (!visited[i] && canChange(currentNode.word, words[i])) {
                     visited[i] = true;
-                    queue.offer(new Node(words[i], currentNode.cnt + 1)); 
-                }   
+                    queue.offer(new Node(words[i], currentNode.cnt + 1));
+                }
             }
-        
         }
-      
-        return answer;
-    }
     
+        return 0;
+    }
     public boolean canChange(String begin, String word) {
         int cnt = 0;
         for (int i = 0; i < begin.length(); i++) {
-            if(begin.charAt(i) != word.charAt(i)) {
+            if (begin.charAt(i) != word.charAt(i)) {
                 cnt++;
             }
         }
