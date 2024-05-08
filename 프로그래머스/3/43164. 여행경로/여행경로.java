c87a4dc1cbd0fa36102c097221e7a429975aa379
Kang Min Kim <lnewgatel@gmail.com>
2024-05-08 15:37:34 +0900

    [level 3] Title: 여행경로, Time: 0.73 ms, Memory: 76.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,49 +1,37 @@
 import java.util.*;
 
 class Solution {
-    List<String> path = new ArrayList<>();
-    List<List<String>> allPaths = new ArrayList<>();
-
     public String[] solution(String[][] tickets) {
+        // 항공권을 출발 도시 기준으로 정렬
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        
+        List<String> answer = new ArrayList<>();
         boolean[] visited = new boolean[tickets.length];
-        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1])); // Sort tickets to prioritize lexicographical order
+        answer.add("ICN");  // 초기 출발 도시 추가
 
-        // Start DFS from 'ICN'
+        dfs("ICN", tickets, visited, answer, 0);
+        return answer.toArray(new String[0]);
+    }
+
+    private boolean dfs(String current, String[][] tickets, boolean[] visited, List<String> path, int count) {
+        if (count == tickets.length) {
+            return true; // 모든 티켓을 사용했으면 true 반환
+        }
+
         for (int i = 0; i < tickets.length; i++) {
-            if (tickets[i][0].equals("ICN")) {
-                path.add("ICN"); // Start from ICN
-                dfs(tickets, visited, i);
+            if (!visited[i] && tickets[i][0].equals(current)) {
+                visited[i] = true;
+                path.add(tickets[i][1]);
+                
+                if (dfs(tickets[i][1], tickets, visited, path, count + 1)) {
+                    return true; // 경로 완성
+                }
+                
+                visited[i] = false; // 백트래킹
                 path.remove(path.size() - 1);
             }
         }
 
-        // Sort all found paths lexicographically
-        Collections.sort(allPaths, (a, b) -> {
-            for (int i = 0; i < Math.min(a.size(), b.size()); i++) {
-                int comp = a.get(i).compareTo(b.get(i));
-                if (comp != 0) return comp;
-            }
-            return 0;
-        });
-
-        return allPaths.isEmpty() ? new String[0] : allPaths.get(0).toArray(new String[0]);
-    }
-
-    public void dfs(String[][] tickets, boolean[] visited, int index) {
-        visited[index] = true;
-        path.add(tickets[index][1]);
-
-        if (path.size() == tickets.length + 1) {
-            allPaths.add(new ArrayList<>(path));
-        } else {
-            for (int i = 0; i < tickets.length; i++) {
-                if (!visited[i] && tickets[i][0].equals(tickets[index][1])) {
-                    dfs(tickets, visited, i);
-                }
-            }
-        }
-
-        visited[index] = false;
-        path.remove(path.size() - 1);
+        return false;
     }
 }
