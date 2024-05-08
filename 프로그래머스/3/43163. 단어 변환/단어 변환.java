87ced79220cede6f820a51017e8ef95a82ba856e
Kang Min Kim <lnewgatel@gmail.com>
2024-05-08 11:38:21 +0900

    [level 3] Title: 단어 변환, Time: 0.25 ms, Memory: 66.3 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -2,34 +2,38 @@ import java.util.*;
 
 class Solution {
     class Node {
-        String word;
-        int cnt;
-        Node(String word, int cnt) {
+        private String word;
+        private int changedCnt; 
+
+        Node(String word, int changedCnt) {
             this.word = word;
-            this.cnt = cnt;
+            this.changedCnt = changedCnt;
         }
-    }
+     }
+   
+    
     public int solution(String begin, String target, String[] words) {
-        boolean[] visited = new boolean[words.length];
+        int answer = 0;
         Queue<Node> queue = new LinkedList();
-        queue.offer(new Node(begin, 0));
+        queue.add(new Node(begin, 0));
+        boolean[] visited = new boolean[words.length];
         
         while(!queue.isEmpty()) {
-            Node currentNode = queue.poll();
-            if (target.equals(currentNode.word)) {
-                return currentNode.cnt;
+            Node current = queue.poll();
+            if (target.equals(current.word)) {
+                return current.changedCnt;
             }
             
             for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canChange(currentNode.word, words[i])) {
+                if(!visited[i] && canChange(current.word, words[i])) {
                     visited[i] = true;
-                    queue.offer(new Node(words[i], currentNode.cnt + 1));
+                    queue.add(new Node(words[i], current.changedCnt + 1));
                 }
             }
         }
-    
-        return 0;
+        return answer;
     }
+    
     public boolean canChange(String begin, String word) {
         int cnt = 0;
         for (int i = 0; i < begin.length(); i++) {
