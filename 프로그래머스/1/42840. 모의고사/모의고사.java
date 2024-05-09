754ce4e8d0c8c21a29dfed98cdf6656b7805f4b1
Kang Min Kim <lnewgatel@gmail.com>
2024-05-10 00:06:15 +0900

    [level 1] Title: 모의고사, Time: 0.99 ms, Memory: 73.8 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,45 +1,69 @@
+import java.util.List;
+import java.util.ArrayList;
+
 class Solution {
     public int[] solution(int[] answers) {
+        List<Integer> list = new ArrayList();
+        int[] one = {1, 2, 3, 4, 5};
+        int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
+        int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int[] arr1 = {1, 2, 3, 4, 5};
-        int[] arr2 = {2, 1, 2, 3, 2, 4, 2, 5};
-        int[] arr3 = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
-        
-        int c1 = 0;
-        int c2 = 0;
-        int c3 = 0;
+        int oneCnt = 0;
+        int twoCnt = 0;
+        int threeCnt = 0;
         
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == arr1[i % arr1.length]) {
-                c1++;
+            if (answers[i] == one[i % one.length]) {
+                oneCnt++;
             } 
             
-            if (answers[i] == arr2[i % arr2.length]) {
-                c2++;
-            } 
+            if (answers[i] == two[i % two.length]) {
+                twoCnt++;
+            }
             
-            if (answers[i] == arr3[i % arr3.length]) {
-                c3++;
+            if (answers[i] == three[i % three.length]) {
+                threeCnt++;
             }
         }
         
-        if (c1 == c2 && c2 == c3) {
+        if (oneCnt == twoCnt && twoCnt == threeCnt) {
             return new int[] {1, 2, 3};
-        } else if (c1 == c2 && c2 > c3) {
-            return new int[] {1, 2};
-        } else if (c1 == c3 && c3 > c2) {
-            return new int[] {1, 3};
-        } else if (c2 == c3 && c3 > c1) {
-            return new int[] {2, 3};
-        } else {
-            if (c1 > c2 && c1 > c3) {
-                return new int[] {1};
-            } else if (c2 > c1 && c2 > c3) {
-                return new int[] {2};
-            } else {
-                return new int[] {3};
-            }
         }
         
+        if (oneCnt == twoCnt && oneCnt > threeCnt) {
+            list.add(1);
+            list.add(2);
+        }
+        
+        if (twoCnt == threeCnt && twoCnt > oneCnt) {
+            list.add(2);
+            list.add(3);
+        }
+        
+        if (oneCnt == threeCnt && threeCnt > twoCnt) {
+            list.add(1);
+            list.add(3);
+        }
+        
+        if (oneCnt > twoCnt &&  oneCnt > threeCnt) {
+            list.add(1);
+        }
+        
+        if (twoCnt > oneCnt &&  twoCnt > threeCnt) {
+            list.add(2);
+        }
+        
+        if (threeCnt > twoCnt &&  threeCnt > oneCnt) {
+            list.add(3);
+        }    
+            
+        
+        int[] answer = new int[list.size()];
+        
+        for (int i = 0; i < list.size(); i++) {
+            answer[i] = list.get(i);
+        }
+        
+        return answer;
     }
 }
