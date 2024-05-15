b6bdc5844449c5d9f3bbaf72ed3a4055e90e48ed
Kang Min Kim <lnewgatel@gmail.com>
2024-05-16 01:29:00 +0900

    [level 1] Title: 모의고사, Time: 2.35 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,69 +1,31 @@
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 
 class Solution {
     public int[] solution(int[] answers) {
-        List<Integer> list = new ArrayList();
+        int[] answer = new int[3];
         int[] one = {1, 2, 3, 4, 5};
         int[] two = {2, 1, 2, 3, 2, 4, 2, 5};
         int[] three = {3, 3, 1, 1, 2, 2, 4, 4, 5, 5};
         
-        int oneCnt = 0;
-        int twoCnt = 0;
-        int threeCnt = 0;
-        
         for (int i = 0; i < answers.length; i++) {
-            if (answers[i] == one[i % one.length]) {
-                oneCnt++;
-            } 
-            
-            if (answers[i] == two[i % two.length]) {
-                twoCnt++;
-            }
-            
-            if (answers[i] == three[i % three.length]) {
-                threeCnt++;
-            }
+            if (answers[i] == one[i % one.length]) answer[0]++; 
+            if (answers[i] == two[i % two.length]) answer[1]++; 
+            if (answers[i] == three[i % three.length]) answer[2]++; 
         }
         
-        if (oneCnt == twoCnt && twoCnt == threeCnt) {
-            return new int[] {1, 2, 3};
-        }
-        
-        if (oneCnt == twoCnt && oneCnt > threeCnt) {
-            list.add(1);
-            list.add(2);
-        }
-        
-        if (twoCnt == threeCnt && twoCnt > oneCnt) {
-            list.add(2);
-            list.add(3);
-        }
-        
-        if (oneCnt == threeCnt && threeCnt > twoCnt) {
-            list.add(1);
-            list.add(3);
-        }
-        
-        if (oneCnt > twoCnt &&  oneCnt > threeCnt) {
-            list.add(1);
-        }
-        
-        if (twoCnt > oneCnt &&  twoCnt > threeCnt) {
-            list.add(2);
-        }
-        
-        if (threeCnt > twoCnt &&  threeCnt > oneCnt) {
-            list.add(3);
-        }    
-            
-        
-        int[] answer = new int[list.size()];
+        List<Integer> list = Arrays.asList(answer[0], answer[1], answer[2]);
+        List<Integer> score = new ArrayList();
+        int max = Collections.max(list);
         
         for (int i = 0; i < list.size(); i++) {
-            answer[i] = list.get(i);
+            if (max == answer[i]) {
+                score.add(i + 1);
+            }
         }
         
-        return answer;
+        return score.stream().mapToInt(i -> i).toArray();
     }
 }
