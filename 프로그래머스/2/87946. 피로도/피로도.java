f58a358d3201a63f4e5cb08f05d21b20992e38a8
Kang Min Kim <lnewgatel@gmail.com>
2024-05-16 17:30:57 +0900

    [level 2] Title: 피로도, Time: 0.50 ms, Memory: 75.2 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,22 +1,30 @@
 import java.util.*;
 
 class Solution {
-    private int maxDungeons = 0; // 최대로 탐험할 수 있는 던전 수를 저장하는 변수
-
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length]; // 각 던전의 방문 여부를 저장
-        explore(dungeons, visited, k, 0);
-        return maxDungeons;
+        int answer = -1;
+        boolean[] visited = new boolean[dungeons.length];
+        List<Integer> list = new ArrayList();
+        permutation(dungeons, visited, list, k, 0);
+        Collections.sort(list);
+        
+        return list.get(list.size() - 1);
     }
-
-    private void explore(int[][] dungeons, boolean[] visited, int fatigue, int count) {
-        maxDungeons = Math.max(maxDungeons, count);
+    
+    public void permutation(int[][] dungeons, boolean[] visited, List<Integer> list, int k, int cnt) {
+        if (cnt == visited.length) {
+            list.add(cnt);
+            return;
+        }
+        
         for (int i = 0; i < dungeons.length; i++) {
-            if (!visited[i] && fatigue >= dungeons[i][0]) { // 방문하지 않았고, 탐험할 수 있는 에너지가 충분한 경우
-                visited[i] = true; // 해당 던전을 방문 표시
-                explore(dungeons, visited, fatigue - dungeons[i][1], count + 1); // 다음 던전 탐색
-                visited[i] = false; // 백트래킹: 다른 모든 가능성을 탐색하기 위해 방문 표시를 원상 복구
+            if (k >= dungeons[i][0] && !visited[i]) {
+                visited[i] = true;
+                permutation(dungeons, visited, list, k - dungeons[i][1], cnt + 1);
+                visited[i] = false;
             }
         }
+        
+        list.add(cnt);
     }
 }
