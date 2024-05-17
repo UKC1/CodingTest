a035fd2ad57515294072f56c3bb98ce2df382e63
Kang Min Kim <lnewgatel@gmail.com>
2024-05-17 16:48:54 +0900

    [level 2] Title: 전력망을 둘로 나누기, Time: 3.02 ms, Memory: 79.6 MB -BaekjoonHub

--- 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
+++ 프로그래머스/2/86971. 전력망을 둘로 나누기/전력망을 둘로 나누기.java
@@ -3,59 +3,55 @@ import java.util.*;
 class Solution {
     public int solution(int n, int[][] wires) {
         int answer = Integer.MAX_VALUE;
-        
-        // 그래프를 인접 리스트로 구성
         List<List<Integer>> graph = new ArrayList<>();
+        
         for (int i = 0; i < n; i++) {
             graph.add(new ArrayList<>());
         }
+        
         for (int[] wire : wires) {
             graph.get(wire[0] - 1).add(wire[1] - 1);
             graph.get(wire[1] - 1).add(wire[0] - 1);
         }
         
-        // 각 전선을 제거하면서 탐색
         for (int[] wire : wires) {
-            // 전선 제거
             int node1 = wire[0] - 1;
             int node2 = wire[1] - 1;
-            graph.get(node1).remove((Integer) node2);
-            graph.get(node2).remove((Integer) node1);
             
-            // 두 트리의 노드 개수 계산
-            int nodesInSubtree1 = bfs(graph, node1, n);
-            int nodesInSubtree2 = n - nodesInSubtree1;
+            graph.get(node1).remove(Integer.valueOf(node2));
+            graph.get(node2).remove(Integer.valueOf(node1));
             
-            // 두 트리의 노드 개수 차이 계산
-            answer = Math.min(answer, Math.abs(nodesInSubtree1 - nodesInSubtree2));
+            int subNode1 = bfs(graph, node1, n);
+            int subNode2 = n - subNode1;
             
-            // 전선 복구
+            answer = Math.min(Math.abs(subNode1 - subNode2), answer);
+            
             graph.get(node1).add(node2);
             graph.get(node2).add(node1);
         }
-        
+    
         return answer;
     }
     
-    // BFS를 사용하여 트리의 노드 개수를 계산
-    private int bfs(List<List<Integer>> graph, int start, int n) {
+    public int bfs(List<List<Integer>> graph, int start, int n) {
         boolean[] visited = new boolean[n];
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(start);
+        Queue<Integer> queue = new LinkedList();
+        queue.add(start);
         visited[start] = true;
-        int count = 0;
+        int count = 1;
         
-        while (!queue.isEmpty()) {
+        while(!queue.isEmpty()) {
             int node = queue.poll();
-            count++;
             for (int neighbor : graph.get(node)) {
                 if (!visited[neighbor]) {
+                    count++;
                     visited[neighbor] = true;
-                    queue.offer(neighbor);
+                    queue.add(neighbor);
                 }
-            }
+            }    
         }
         
         return count;
+        
     }
 }
