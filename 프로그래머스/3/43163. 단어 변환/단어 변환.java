6a40f6456653917b577d4bb4813fbacb9a3368cd
Kang Min Kim <lnewgatel@gmail.com>
2024-05-31 15:44:19 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,46 +1,44 @@
 import java.util.*;
-
 class Solution {
     class Node {
-        private String word;
-        private int changedCnt; 
-
-        Node(String word, int changedCnt) {
+        String word;
+        int cnt;
+        public Node(String word, int cnt) {
             this.word = word;
-            this.changedCnt = changedCnt;
+            this.cnt = cnt;
         }
-     }
-   
-    
+    }
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        Queue<Node> queue = new LinkedList();
-        queue.add(new Node(begin, 0));
         boolean[] visited = new boolean[words.length];
+        Queue<Node> queue = new LinkedList();
+        queue.offer(new Node(begin, 0));
         
         while(!queue.isEmpty()) {
-            Node current = queue.poll();
-            if (target.equals(current.word)) {
-                return current.changedCnt;
+            Node node = queue.poll();
+            if (node.word.equals(target)) {
+                return node.cnt;
             }
             
             for (int i = 0; i < words.length; i++) {
-                if(!visited[i] && canChange(current.word, words[i])) {
+                if (!visited[i] && canChange(node.word, words[i])) {
                     visited[i] = true;
-                    queue.add(new Node(words[i], current.changedCnt + 1));
+                    queue.offer(new Node(words[i], node.cnt+1));           
                 }
             }
         }
         return answer;
     }
     
-    public boolean canChange(String begin, String word) {
+    public boolean canChange(String beforeWord, String afterWord) {
         int cnt = 0;
-        for (int i = 0; i < begin.length(); i++) {
-            if (begin.charAt(i) != word.charAt(i)) {
+        for (int i = 0; i < beforeWord.length(); i++) {
+            if (beforeWord.charAt(i) != afterWord.charAt(i)) {
                 cnt++;
             }
         }
+        
         return cnt == 1;
     }
 }
+
