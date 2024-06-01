78501ca3a0e55983616f57cd9a2124a2eb97969f
Kang Min Kim <lnewgatel@gmail.com>
2024-06-01 22:37:09 +0900

    [level 2] Title: 하노이의 탑, Time: 5.86 ms, Memory: 102 MB -BaekjoonHub

--- 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
+++ 프로그래머스/2/12946. 하노이의 탑/하노이의 탑.java
@@ -1,24 +1,23 @@
 import java.util.*;
 class Solution {
     public int[][] solution(int n) {
-        List<int[]> moves = new ArrayList<>();
-        solveHanoi(n, 1, 2, 3, moves);
-        
-        int[][] result = new int[moves.size()][2];
+        List<int[]> moves = new ArrayList();
+        solvedHanoi(n, 1, 2, 3, moves);
+        int[][] answer = new int[moves.size()][2];
         for (int i = 0; i < moves.size(); i++) {
-            result[i] = moves.get(i);
+            answer[i] = moves.get(i);
         }
-        
-        return result;
+        return answer;
     }
     
-     private void solveHanoi(int n, int start, int auxiliary, int end, List<int[]> moves) {
+    public void solvedHanoi(int n, int start, int middle, int end, List<int[]> moves) {
         if (n == 1) {
             moves.add(new int[]{start, end});
+            return;
         } else {
-            solveHanoi(n - 1, start, end, auxiliary, moves);
+            solvedHanoi(n - 1, start, end, middle, moves);
             moves.add(new int[]{start, end});
-            solveHanoi(n - 1, auxiliary, start, end, moves);
+            solvedHanoi(n - 1, middle, start, end, moves);
         }
     }
 }
