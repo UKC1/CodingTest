2f34268264b6f62b444225d303b62e0c57445ed1
Kang Min Kim <lnewgatel@gmail.com>
2024-06-02 17:39:11 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 66.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,30 +1,19 @@
-import java.util.*;
-
 class Solution {
+    int max = 0;
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
         boolean[] visited = new boolean[dungeons.length];
-        List<Integer> list = new ArrayList();
-        permutation(dungeons, visited, list, k, 0);
-        Collections.sort(list);
-        
-        return list.get(list.size() - 1);
+        dfs(dungeons, visited, k, 0);
+        return max;
     }
     
-    public void permutation(int[][] dungeons, boolean[] visited, List<Integer> list, int k, int cnt) {
-        if (cnt == visited.length) {
-            list.add(cnt);
-            return;
-        }
-        
+    public void dfs(int[][] dungeons, boolean[] visited, int k, int cnt) {
+        max = Math.max(cnt, max);
         for (int i = 0; i < dungeons.length; i++) {
-            if (k >= dungeons[i][0] && !visited[i]) {
+            if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                permutation(dungeons, visited, list, k - dungeons[i][1], cnt + 1);
+                dfs(dungeons, visited, k - dungeons[i][1], cnt + 1);
                 visited[i] = false;
             }
         }
-        
-        list.add(cnt);
     }
 }
