3bcb229795228c2dbdfdbfc00d9463413981f113
Kang Min Kim <lnewgatel@gmail.com>
2024-06-06 00:43:04 +0900

    [level 3] Title: 아이템 줍기, Time: 0.25 ms, Memory: 75.9 MB -BaekjoonHub

--- 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
+++ 프로그래머스/3/87694. 아이템 줍기/아이템 줍기.java
@@ -3,6 +3,7 @@ import java.util.*;
 class Solution {
     public int solution(int[][] rectangle, int characterX, int characterY, int itemX, int itemY) {
         int answer = 0;
+        Queue<int[]> queue = new LinkedList();
         boolean[][] visited = new boolean[102][102];
         boolean[][] isBoundary = new boolean[102][102];
         int[] dx = {-1, 0, 1, 0};
@@ -17,7 +18,7 @@ class Solution {
             for (int x = x1; x <= x2; x++) {
                 for (int y = y1; y <= y2; y++) {
                     isBoundary[x][y] = true;
-                }    
+                }   
             }
         }
         
@@ -30,31 +31,32 @@ class Solution {
             for (int x = x1; x <= x2; x++) {
                 for (int y = y1; y <= y2; y++) {
                     isBoundary[x][y] = false;
-                }    
+                }   
             }
         }
         
-        Queue<int[]> queue = new LinkedList();
         queue.offer(new int[] {characterX * 2, characterY * 2, 0});
         
         while(!queue.isEmpty()) {
-            int[] arr = queue.poll();
-            int x = arr[0], y = arr[1], cnt = arr[2];
+            int[] current = queue.poll();
+            int x = current[0], y = current[1], cnt = current[2];
             
             if (x == itemX * 2 && y == itemY * 2) {
                 return cnt / 2;
             }
+            
             for (int i = 0; i < 4; i++) {
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if (nx >= 0 && ny >= 0 && nx < 102 && ny < 102 && isBoundary[nx][ny] && !visited[nx][ny]) {
+                if (nx >= 0 && ny >= 0 && nx < 102 && ny < 102
+                   && isBoundary[nx][ny] && !visited[nx][ny]) {
                     visited[nx][ny] = true;
-                    queue.offer(new int[]{nx, ny, cnt + 1});
-                    
+                    queue.offer(new int[] {nx, ny, cnt + 1});
                 }
             }
+            
+            
         }
-        
         return answer;
     }
 }
