748e6eefa1591b2748278b44659175196155481e
Kang Min Kim <lnewgatel@gmail.com>
2024-06-05 17:45:17 +0900

    [level 2] Title: 게임 맵 최단거리, Time: 4.02 ms, Memory: 52.5 MB -BaekjoonHub

--- 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
+++ 프로그래머스/2/1844. 게임 맵 최단거리/게임 맵 최단거리.java
@@ -2,45 +2,33 @@ import java.util.*;
 
 class Solution {
     public int solution(int[][] maps) {
-        
-        int x = maps.length, y = maps[0].length;
-        boolean[][] visited = new boolean[x][y];
-        Queue<Node> queue = new LinkedList();
-        queue.offer(new Node(0, 0, 1));
+        int n = maps.length, m = maps[0].length;
+        boolean[][] visited = new boolean[n][m];
+        Queue<int[]> queue = new LinkedList();
+        int[] dx = {-1, 0, 1, 0};
+        int[] dy = {0, -1, 0, 1};
+        queue.offer(new int[] {0, 0, 1});
         visited[0][0] = true;
         
-        while (!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (node.currentX + 1 == x && node.currentY + 1 == y) {
-                return node.count;   
+        while(!queue.isEmpty()) {
+            int[] dirArr = queue.poll();
+            int x = dirArr[0], y = dirArr[1], cnt = dirArr[2];
+            
+            if (x == n - 1 && y == m - 1) {
+                return cnt;
             }
-            int[] dx = {-1, 0, 1, 0};
-            int[] dy = {0, -1, 0, 1};
+            
             for (int i = 0; i < 4; i++) {
-                int nx = node.currentX + dx[i];
-                int ny = node.currentY + dy[i];
+                int nx = x + dx[i];
+                int ny = y + dy[i];
                 
-                if (nx >= 0 && ny >= 0 
-                    && nx < x && ny < y
-                   && !visited[nx][ny]
-                   && maps[nx][ny] == 1) {
+                if (nx >= 0 && ny >= 0 && nx < n && ny < m && !visited[nx][ny] && maps[nx][ny] == 1) {
                     visited[nx][ny] = true;
-                    queue.offer(new Node(nx, ny, node.count + 1));
+                    queue.offer(new int[] {nx, ny, cnt + 1});
                 }
             }
         }
+        
         return -1;
-    }
-}
-
-class Node {
-    int currentX;
-    int currentY;
-    int count;
-    
-    public Node(int currentX, int currentY, int count) {
-        this.currentX = currentX;
-        this.currentY = currentY;
-        this.count = count; 
     }
 }
