75f69681b0813d32eb6150a0a853b4f58f029f50
Kang Min Kim <lnewgatel@gmail.com>
2024-06-05 17:52:17 +0900

    [level 3] Title: 단어 변환, Time: 0.27 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/3/43163. 단어 변환/단어 변환.java
+++ 프로그래머스/3/43163. 단어 변환/단어 변환.java
@@ -1,39 +1,42 @@
 import java.util.*;
+
 class Solution {
     class Node {
         String word;
         int cnt;
-        public Node(String word, int cnt) {
+        Node(String word, int cnt) {
             this.word = word;
             this.cnt = cnt;
         }
     }
     public int solution(String begin, String target, String[] words) {
         int answer = 0;
-        boolean[] visited = new boolean[words.length];
+        boolean[] visited = new boolean[words.length];    
         Queue<Node> queue = new LinkedList();
         queue.offer(new Node(begin, 0));
-        
         while(!queue.isEmpty()) {
-            Node node = queue.poll();
-            if (node.word.equals(target)) {
-                return node.cnt;
+            Node current = queue.poll();
+            
+            if (current.word.equals(target)) {
+                return current.cnt;
             }
             
             for (int i = 0; i < words.length; i++) {
-                if (!visited[i] && canChange(node.word, words[i])) {
+                if (!visited[i] && canChange(current.word, words[i])) {
                     visited[i] = true;
-                    queue.offer(new Node(words[i], node.cnt+1));           
+                    queue.offer(new Node(words[i], current.cnt + 1));
+                    
                 }
             }
+            
         }
         return answer;
     }
     
-    public boolean canChange(String beforeWord, String afterWord) {
+    public boolean canChange(String begin, String word) {
         int cnt = 0;
-        for (int i = 0; i < beforeWord.length(); i++) {
-            if (beforeWord.charAt(i) != afterWord.charAt(i)) {
+        for (int i = 0; i < begin.length(); i++) {
+            if (begin.charAt(i) != word.charAt(i)) {
                 cnt++;
             }
         }
@@ -41,4 +44,3 @@ class Solution {
         return cnt == 1;
     }
 }
-
