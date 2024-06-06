3ed4516ef91f1db27b247ff9e8a846a2bcc9637d
Kang Min Kim <lnewgatel@gmail.com>
2024-06-06 18:32:08 +0900

    [level 3] Title: 여행경로, Time: 0.68 ms, Memory: 77.2 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -2,36 +2,31 @@ import java.util.*;
 
 class Solution {
     public String[] solution(String[][] tickets) {
-        // 항공권을 출발 도시 기준으로 정렬
-        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        List<String> path = new ArrayList();
         
-        List<String> answer = new ArrayList<>();
+        path.add("ICN");
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         boolean[] visited = new boolean[tickets.length];
-        answer.add("ICN");  // 초기 출발 도시 추가
-
-        dfs("ICN", tickets, visited, answer, 0);
-        return answer.toArray(new String[0]);
+        dfs("ICN", tickets, visited, path, 0);
+        return path.toArray(new String[0]);
     }
-
-    private boolean dfs(String current, String[][] tickets, boolean[] visited, List<String> path, int count) {
-        if (count == tickets.length) {
-            return true; // 모든 티켓을 사용했으면 true 반환
+    
+    public boolean dfs(String current, String[][] tickets, boolean[] visited, List<String> path, int cnt) {
+        if (cnt == tickets.length) {
+            return true;
         }
-
+        
         for (int i = 0; i < tickets.length; i++) {
-            if (!visited[i] && tickets[i][0].equals(current)) {
+            if (!visited[i] && current.equals(tickets[i][0])) {
                 visited[i] = true;
                 path.add(tickets[i][1]);
-                
-                if (dfs(tickets[i][1], tickets, visited, path, count + 1)) {
-                    return true; // 경로 완성
+                if (dfs(tickets[i][1], tickets, visited, path, cnt + 1)) {
+                    return true;
                 }
-                
-                visited[i] = false; // 백트래킹
+                visited[i] = false;
                 path.remove(path.size() - 1);
             }
         }
-
         return false;
     }
 }
