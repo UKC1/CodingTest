475da102e95f5d1206036c46e88fbe4a6a2cbf0f
Kang Min Kim <lnewgatel@gmail.com>
2024-06-08 23:01:48 +0900

    [level 3] Title: 여행경로, Time: 2.11 ms, Memory: 68.7 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -3,23 +3,21 @@ import java.util.*;
 class Solution {
     public String[] solution(String[][] tickets) {
         List<String> path = new ArrayList();
-        
-        path.add("ICN");
-        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
         boolean[] visited = new boolean[tickets.length];
+        Arrays.sort(tickets, (a, b) -> a[1].compareTo(b[1]));
+        path.add("ICN");
         dfs("ICN", tickets, visited, path, 0);
         return path.toArray(new String[0]);
     }
-    
     public boolean dfs(String current, String[][] tickets, boolean[] visited, List<String> path, int cnt) {
         if (cnt == tickets.length) {
             return true;
         }
         
         for (int i = 0; i < tickets.length; i++) {
-            if (!visited[i] && current.equals(tickets[i][0])) {
-                visited[i] = true;
+            if(!visited[i] && current.equals(tickets[i][0])) {
                 path.add(tickets[i][1]);
+                visited[i] = true;
                 if (dfs(tickets[i][1], tickets, visited, path, cnt + 1)) {
                     return true;
                 }
@@ -27,6 +25,7 @@ class Solution {
                 path.remove(path.size() - 1);
             }
         }
+        
         return false;
     }
 }
