46855085fb9a8a6d59e1996065e66e8137b1c450
Kang Min Kim <lnewgatel@gmail.com>
2024-06-10 20:15:05 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 79.3 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,19 +1,20 @@
 class Solution {
-    int max = 0;
+    
     public int solution(int k, int[][] dungeons) {
-        boolean[] visited = new boolean[dungeons.length];
-        dfs(dungeons, visited, k, 0);
-        return max;
+        boolean[] visited = new boolean[dungeons.length];     
+        
+        return dfs(k, dungeons, visited);
     }
     
-    public void dfs(int[][] dungeons, boolean[] visited, int k, int cnt) {
-        max = Math.max(cnt, max);
+    public int dfs(int k, int[][] dungeons, boolean[] visited) {
+        int maxCount = 0;
         for (int i = 0; i < dungeons.length; i++) {
             if (!visited[i] && dungeons[i][0] <= k) {
                 visited[i] = true;
-                dfs(dungeons, visited, k - dungeons[i][1], cnt + 1);
+                maxCount = Math.max(maxCount, 1 + dfs(k - dungeons[i][1], dungeons, visited));
                 visited[i] = false;
             }
         }
+        return maxCount;
     }
 }
