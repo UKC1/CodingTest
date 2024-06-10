aa1ac6b5a23a2cce68528c68ccac8d51b32a4eb6
Kang Min Kim <lnewgatel@gmail.com>
2024-06-10 19:53:23 +0900

    [level 2] Title: 카펫, Time: 0.07 ms, Memory: 75.8 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,17 +1,19 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow; // 전체 타일 수
+        int total = brown + yellow;
         
-        for (int width = 3; width <= Math.sqrt(total); width++) { // 너비는 최소 3부터 시작
-            if (total % width == 0) { // 너비로 나누어 떨어지는 경우
-                int length = total / width; // 길이 계산
-                
-                // 노란색 타일 면적이 맞는지 확인
-                if ((width - 2) * (length - 2) == yellow) {
-                    return new int[] {Math.max(width, length), Math.min(width, length)}; // 너비와 길이 반환
+        int[] answer = new int[2];
+        for (int i = 2; i <= Math.sqrt(total); i++) {
+            if (total % i == 0) {
+                int wid = i;
+                int len = total / i;
+                if ((wid - 2) * (len - 2) == yellow) {
+                    answer[0] = len;
+                    answer[1] = wid;
                 }
             }
+            
         }
-        return new int[] {}; // 적절한 치수를 찾지 못한 경우
+        return answer;
     }
 }
