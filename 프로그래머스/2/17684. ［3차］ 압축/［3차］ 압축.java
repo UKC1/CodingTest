8c8b157c05d827b6c26181c8c39d084e3334a1f1
Kang Min Kim <lnewgatel@gmail.com>
2024-06-13 19:56:42 +0900

    [level 2] Title: [3차] 압축, Time: 2.35 ms, Memory: 74.5 MB -BaekjoonHub

--- 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
+++ 프로그래머스/2/17684. ［3차］ 압축/［3차］ 압축.java
@@ -1,38 +1,29 @@
 import java.util.*;
+
 class Solution {
     public int[] solution(String msg) {
         Map<String, Integer> map = new HashMap();
         for (int i = 0; i < 26; i++) {
-            map.put(Character.toString((char) (i + 'A')), i + 1);
-        }
-        
-        // 추가된 제일 긴 단어까지 찾음
-        
-        List<Integer> list = new ArrayList();
-        int len = 0;
-        int idx = 1;
-        String w = msg.substring(0, 1);
-        String c;
-        while(len <= msg.length() && idx < msg.length()) {
-            c = msg.substring(idx, idx + 1);
-            if (map.get(w+c) == null) {
-                map.put(w+c, map.size() + 1);
-                list.add(map.get(w));
-                w = c;
-                idx++;
-                len += w.length();
-            } else {
-                w += c;
-                idx++;
-            }
-            // System.out.print(w + " " + c + " / ");
+            map.put(Character.toString((char)i+'A'), i + 1);
             
+        } 
+        List<Integer> output = new ArrayList();
+        int idx = 0;
+        int endIdx = 27;
+        int length = 2;
+        
+        while(idx + length <= msg.length()) {
+            String w = msg.substring(idx, idx + length);
+            if (idx + length <= msg.length() && !map.containsKey(w)) {
+                output.add(map.get(msg.substring(idx, idx + length - 1)));
+                map.put(w, endIdx++);
+                idx += w.length() - 1;
+                length = 2;
+            } else {
+                length++;
+            }
         }
-        list.add(map.get(w));
-        int[] answer = new int[list.size()];
-        for (int i = 0 ; i < list.size(); i++) {
-            answer[i] = list.get(i);
-        }
-        return answer;
+        output.add(map.get(msg.substring(idx, idx + length - 1)));
+        return output.stream().mapToInt(i->i).toArray();
     }
 }
