1d9f6d9b8be0835ba404f7144ebe546668681537
Kang Min Kim <lnewgatel@gmail.com>
2024-06-13 21:29:10 +0900

    [level 2] Title: [3차] n진수 게임, Time: 3.84 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
+++ 프로그래머스/2/17687. ［3차］ n진수 게임/［3차］ n진수 게임.java
@@ -1,20 +1,20 @@
+import java.util.*;
 class Solution {
     public String solution(int n, int t, int m, int p) {
         StringBuilder sb = new StringBuilder();
         int num = 0;
-        while (sb.length() <= t * m) {
-            sb.append(Integer.toString(num, n));
-            num++;
+        while(sb.length() < t * m) {
+            sb.append(Integer.toString(num++, n));
         }
         
+        String str = sb.toString().toUpperCase();
         StringBuilder answer = new StringBuilder();
-        for (int i = p - 1; i < sb.length(); i += m) {
-            if (answer.length() >= t) {
+        for(int i = p - 1; i < str.length(); i += m) {
+            if (answer.length() == t) {
                 break;
             }
-            answer.append(sb.toString().toUpperCase().charAt(i));
+            answer.append(str.charAt(i));
         }
-       
         return answer.toString();
     }
 }
