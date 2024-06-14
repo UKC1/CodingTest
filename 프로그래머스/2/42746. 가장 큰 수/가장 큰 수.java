582ebb23c07989d42d972401d9536d5c9be279c3
Kang Min Kim <lnewgatel@gmail.com>
2024-06-14 10:29:02 +0900

    [level 2] Title: 가장 큰 수, Time: 2.33 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,28 +1,32 @@
-import java.util.Arrays;
-import java.util.Comparator;
+import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        String[] str = new String[numbers.length];
-        for (int i = 0; i < str.length; i++) {
-            str[i] = Integer.toString(numbers[i]);
+        // 숫자 배열을 문자열 배열로 변환
+        String[] numStrs = new String[numbers.length];
+        for (int i = 0; i < numbers.length; i++) {
+            numStrs[i] = String.valueOf(numbers[i]);
         }
-        
-        Arrays.sort(str, new Comparator<String>() {
+
+        // 문자열 배열을 정렬
+        Arrays.sort(numStrs, new Comparator<String>() {
             @Override
-            public int compare(String o1, String o2) {
-                return (o2 + o1).compareTo(o1 + o2);
+            public int compare(String a, String b) {
+                return (b + a).compareTo(a + b);  // 내림차순 정렬
             }
         });
 
-        if (str[0].equals("0")) {
+        // 정렬된 문자열 배열을 결합하여 결과 생성
+        StringBuilder sb = new StringBuilder();
+        for (String numStr : numStrs) {
+            sb.append(numStr);
+        }
+
+        // 결과가 "0"으로 시작하는 경우 처리 (모든 숫자가 0인 경우)
+        if (sb.charAt(0) == '0') {
             return "0";
         }
 
-        StringBuilder answer = new StringBuilder();
-        for (String s : str) {
-            answer.append(s);
-        }
-        return answer.toString();
+        return sb.toString();
     }
 }
