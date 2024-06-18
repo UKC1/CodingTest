9ce33961f1e894d19a86ad7665e14676a3266c15
Kang Min Kim <lnewgatel@gmail.com>
2024-06-18 23:08:16 +0900

    [level 2] Title: 소수 찾기, Time: 16.25 ms, Memory: 77.9 MB -BaekjoonHub

--- 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
+++ 프로그래머스/2/42839. 소수 찾기/소수 찾기.java
@@ -1,44 +1,45 @@
 import java.util.*;
+
 class Solution {
     public int solution(String numbers) {
         int answer = 0;
         Set<Integer> set = new HashSet();
         boolean[] visited = new boolean[numbers.length()];
-        dfs("", numbers.toCharArray(), set, visited, 0);
+        dfs("", numbers.toCharArray(), visited, set);
         for (Integer num : set) {
-            if (isPrime(num)) {
-                answer++;
-            }
+            if (isPrime(num)) answer++;
         }
         return answer;
     }
     
-    public void dfs(String current, char[] numbers, Set<Integer> set, boolean[] visited, int start){
-        
-        if (!current.isEmpty()){
-            set.add(Integer.parseInt(current));  
+    public void dfs(String current, char[] numArr, boolean[] visited, Set<Integer> set) {
+        if (!current.isEmpty()) {
+            set.add(Integer.parseInt(current));
         }
-                
-        for (int i = 0; i < numbers.length; i++) {
+        
+        for (int i = 0; i < numArr.length; i++) {
             if (!visited[i]) {
                 visited[i] = true;
-                dfs(current + numbers[i], numbers, set, visited, i);       
+                dfs(current + numArr[i], numArr, visited, set);
                 visited[i] = false;
-            }       
-               
+            }
         }
     }
     
     public boolean isPrime(int n) {
-        if (n < 2) {
+        if (n <= 1) {
             return false;
-        }
+        } 
         if (n == 2 || n == 3) {
             return true;
         }
         
-        for (int i = 2; i <= Math.sqrt(n); i++) {
-            if (n % i == 0) {
+        if (n % 2 == 0 || n % 3 == 0) {
+            return false;
+        }
+        
+        for (int i = 5; i <= Math.sqrt(n); i += 6) {
+            if (n % i == 0 || n % (i + 2) == 0) {
                 return false;
             }
         }
