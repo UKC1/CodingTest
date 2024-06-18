e6ab694ca51b228484d1a622a12c87447d354214
Kang Min Kim <lnewgatel@gmail.com>
2024-06-18 18:49:07 +0900

    [level 2] Title: 방문 길이, Time: 14.03 ms, Memory: 79.1 MB -BaekjoonHub

--- 프로그래머스/2/49994. 방문 길이/방문 길이.java
+++ 프로그래머스/2/49994. 방문 길이/방문 길이.java
@@ -4,37 +4,29 @@ class Solution {
     public int solution(String dirs) {
         Set<String> visited = new HashSet<>();
         int x = 0, y = 0;
-        int[] dx = {0, 0, -1, 1}; // L, R, D, U
-        int[] dy = {-1, 1, 0, 0}; // L, R, D, U
-        int[][] dirMap = {{'L', 0}, {'R', 1}, {'D', 2}, {'U', 3}};
-        int answer = 0;
-
+        
+        Map<Character, int[]> directions = new HashMap<>();
+        directions.put('L', new int[]{-1, 0});
+        directions.put('R', new int[]{1, 0});
+        directions.put('D', new int[]{0, -1});
+        directions.put('U', new int[]{0, 1});
+        
         for (char dir : dirs.toCharArray()) {
-            int direction = -1;
-            for (int i = 0; i < dirMap.length; i++) {
-                if (dirMap[i][0] == dir) {
-                    direction = dirMap[i][1];
-                    break;
-                }
-            }
-
-            int nx = x + dx[direction];
-            int ny = y + dy[direction];
+            int[] move = directions.get(dir);
+            int nx = x + move[0];
+            int ny = y + move[1];
 
             if (nx >= -5 && nx <= 5 && ny >= -5 && ny <= 5) {
                 String path1 = x + "," + y + "," + nx + "," + ny;
                 String path2 = nx + "," + ny + "," + x + "," + y;
 
-                if (!visited.contains(path1) && !visited.contains(path2)) {
-                    visited.add(path1);
+                if (visited.add(path1)) {  // if path1 is newly added, path2 will be implicitly added
                     visited.add(path2);
-                    answer++;
                 }
                 x = nx;
                 y = ny;
             }
         }
-
-        return answer;
+        return visited.size() / 2;  // since each path is stored twice
     }
 }
