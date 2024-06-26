2f46631dbd1b0275053a929a453da3bd40067791
Kang Min Kim <lnewgatel@gmail.com>
2024-06-26 10:26:29 +0900

    [level 2] Title: 땅따먹기, Time: 19.92 ms, Memory: 86.4 MB -BaekjoonHub

--- 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
+++ 프로그래머스/2/12913. 땅따먹기/땅따먹기.java
@@ -1,17 +1,18 @@
 class Solution {
     int solution(int[][] land) {
+        int answer = 0;
         int rows = land.length;
         int[][] dp = new int[rows][4];
-        
         for (int i = 0; i < 4; i++) {
             dp[0][i] = land[0][i];
         }
         
         for (int i = 1; i < rows; i++) {
             for (int j = 0; j < 4; j++) {
-                dp[i][j] = land[i][j] + Math.max(dp[i-1][(j+1)%4], Math.max(dp[i-1][(j+2)%4], dp[i-1][(j+3)%4])); 
+                dp[i][j] = land[i][j] + Math.max(dp[i - 1][(j + 1) % 4], Math.max(dp[i - 1][(j + 2) % 4], dp[i - 1][(j + 3) % 4]));
             }
-        } 
-        return Math.max(dp[rows-1][0], Math.max(dp[rows-1][1], Math.max(dp[rows-1][2], dp[rows-1][3])));
+        }
+        
+        return Math.max(dp[rows - 1][0], Math.max(dp[rows - 1][1], Math.max(dp[rows - 1][2], dp[rows - 1][3])));
     }
 }
