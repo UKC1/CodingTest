1071fdd2829afb400abfca06246f26c85672f11d
Kang Min Kim <lnewgatel@gmail.com>
2024-07-24 14:23:24 +0900

    [Bronze III] Title: 행렬 덧셈, Time: 368 ms, Memory: 19316 KB -BaekjoonHub

---
+++ 백준/Bronze/2738. 행렬 덧셈/행렬 덧셈.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] arr = new int[N][M];
+        for (int num = 0; num < 2; num++) {
+            for (int i = 0; i < N; i++) {
+                st = new StringTokenizer(br.readLine());
+                for (int j = 0; j < M; j++) {
+                    arr[i][j] += Integer.parseInt(st.nextToken());
+                }
+            }
+        }
+
+        for(int[] matrix : arr) {
+            for (int num : matrix) {
+                System.out.print(num + " ");
+            }
+            System.out.println();
+        }
+    }
+}
