4094b5bc38d9b3ee29756ce5f42f6fc98ce083e4
Kang Min Kim <lnewgatel@gmail.com>
2024-07-24 09:59:38 +0900

    [Bronze V] Title: 문자열, Time: 144 ms, Memory: 15956 KB -BaekjoonHub

---
+++ 백준/Bronze/9086. 문자열/문자열.java
@@ -0,0 +1,16 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < N; i++) {
+            String str = br.readLine().trim();
+            System.out.println(str.charAt(0) + "" + str.charAt(str.length() - 1));
+        }
+    }
+}
