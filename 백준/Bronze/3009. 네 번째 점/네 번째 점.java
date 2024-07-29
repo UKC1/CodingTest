6f36705ec2d07d2346bed8ea89c3934ca76757ef
Kang Min Kim <lnewgatel@gmail.com>
2024-07-29 14:36:33 +0900

    [Bronze III] Title: 네 번째 점, Time: 144 ms, Memory: 15712 KB -BaekjoonHub

---
+++ 백준/Bronze/3009. 네 번째 점/네 번째 점.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int[] xArr = new int[1001];
+        int[] yArr = new int[1001];
+        for (int i = 0; i < 3; i++) {
+            st = new StringTokenizer(br.readLine());
+            int x = Integer.parseInt(st.nextToken());
+            int y = Integer.parseInt(st.nextToken());
+            xArr[x]++;
+            yArr[y]++;
+        }
+
+        for (int i = 0; i < xArr.length; i++) {
+            if (xArr[i] == 1) {
+                System.out.print(i + " ");
+            }
+        }
+
+        for (int i = 0; i < yArr.length; i++) {
+            if (yArr[i] == 1) {
+                System.out.print(i + " ");
+            }
+        }
+
+    }
+}
