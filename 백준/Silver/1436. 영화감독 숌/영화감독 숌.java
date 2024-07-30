17ce55d9233a03d9071f359e2f48251100773ddb
Kang Min Kim <lnewgatel@gmail.com>
2024-07-30 18:00:00 +0900

    [Silver V] Title: 영화감독 숌, Time: 284 ms, Memory: 84208 KB -BaekjoonHub

---
+++ 백준/Silver/1436. 영화감독 숌/영화감독 숌.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int cnt = 0;
+        int num = 666;
+        while (cnt < N) {
+            if (Integer.toString(num).contains("666")) {
+                cnt++;
+            }
+            num++;
+        }
+        System.out.println(num - 1);
+    }
+}
