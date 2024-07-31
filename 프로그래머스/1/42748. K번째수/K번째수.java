27359e6592da2bb1a80de4154f1e264e2b568b58
Kang Min Kim <lnewgatel@gmail.com>
2024-07-31 21:20:14 +0900

    [level 1] Title: K번째수, Time: 1.21 ms, Memory: 76.8 MB -BaekjoonHub

--- 프로그래머스/1/42748. K번째수/K번째수.java
+++ 프로그래머스/1/42748. K번째수/K번째수.java
@@ -1,15 +1,21 @@
-import java.util.*;
+import java.util.Arrays;
 class Solution {
     public int[] solution(int[] array, int[][] commands) {
         int[] answer = new int[commands.length];
+        int cnt = 0;
         for (int i = 0; i < commands.length; i++) {
-            int[] arr = new int[commands[i][1] - commands[i][0] + 1];
+            int start = commands[i][0] - 1;
+            int end = commands[i][1] - 1;
+            int findIdx = commands[i][2] - 1;
+            
+            int[] arr = new int[end - start + 1];
             int idx = 0;
-            for (int j = commands[i][0] - 1; j < commands[i][1]; j++) {
+            for (int j = start; j <= end; j++) {
                 arr[idx++] = array[j];
             }
+            
             Arrays.sort(arr);
-            answer[i] = arr[commands[i][2] - 1];
+            answer[cnt++] = arr[findIdx];
             
         }
         return answer;
