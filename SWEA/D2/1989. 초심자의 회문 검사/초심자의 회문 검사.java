410edd72f89040cb57d96d5477564e1e780a585a
Kang Min Kim <lnewgatel@gmail.com>
2024-07-31 17:23:08 +0900

    [D2] Title: 초심자의 회문 검사, Time: 143 ms, Memory: 17,432 KB -BaekjoonHub

---
+++ SWEA/D2/1989. 초심자의 회문 검사/초심자의 회문 검사.java
@@ -0,0 +1,23 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int T = Integer.parseInt(st.nextToken());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            String str = br.readLine();
+            boolean isPalindrome = true;
+            for (int i = 0; i < str.length() / 2; i++) {
+                if (str.charAt(i) != str.charAt(str.length() - 1 - i)) {
+                    isPalindrome = false;
+                    break;
+                }
+            }
+            System.out.println("#" + test_case + " " + (isPalindrome ? 1 : 0));
+        }
+    }
+}
