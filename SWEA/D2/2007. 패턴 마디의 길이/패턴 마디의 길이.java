41b5fa4743ed9f36fbb35a5a9569e8dcf6a6a05b
Kang Min Kim <lnewgatel@gmail.com>
2024-08-02 00:32:42 +0900

    [D2] Title: 패턴 마디의 길이, Time: 113 ms, Memory: 19,684 KB -BaekjoonHub

--- SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
+++ SWEA/D2/2007. 패턴 마디의 길이/패턴 마디의 길이.java
@@ -4,23 +4,24 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        int T = Integer.parseInt(st.nextToken());
-        for (int test_case = 1; test_case <= T; test_case++) {
-            String str = br.readLine();
-            StringBuilder sb = new StringBuilder();
-            int minLen = str.length();
-            int repeatedLen = 0;
-            for (int i = 0; i < 10; i++) {
-                sb.append(str.charAt(i));
-                if (str.replace(sb.toString(), "").length() < minLen) {
-                    minLen = str.replace(sb.toString(), "").length();
-                    repeatedLen = sb.length();
-                }
-            }
-            System.out.println("#" + test_case + " " + repeatedLen);
-        }
-    }
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		for (int i = 1; i <= N; i++) {
+			String str = br.readLine();
+			int minLen = 30;
+			int newLen = 0;
+			for (int j = 0; j < 10; j++) {
+				String newStr = str.substring(0, j + 1);
+				int strLen = str.replace(newStr, "").length();
+				if (minLen > strLen) {
+					minLen = strLen;
+					newLen = newStr.length();
+				}
+			}
+			System.out.println("#" + i + " " + newLen);
+		}
+	}
 }
