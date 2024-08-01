2f0e3a9d7f63ff6407755bfaa90b62062bad9b5f
Kang Min Kim <lnewgatel@gmail.com>
2024-08-01 17:13:47 +0900

    [level 2] Title: 피로도, Time: 3.48 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/2/87946. 피로도/피로도.java
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -1,23 +1,20 @@
-import java.util.*;
-
 class Solution {
     public int solution(int k, int[][] dungeons) {
-        int answer = -1;
-        boolean[] visited = new boolean[dungeons.length];
-        answer = Math.max(answer, dfs(k, dungeons, visited, 0));
-        System.out.println(answer);
+        int answer = dfs(k, 0, new boolean[dungeons.length], dungeons);
         return answer;
     }
     
-    public int dfs(int k, int[][] dungeons, boolean[] visited, int cnt) {
-        int maxCnt = cnt;
-        for (int i = 0; i < dungeons.length; i++) {
-            if (k >= dungeons[i][0] && !visited[i]) {
+    public int dfs(int k, int maxCount, boolean[] visited, int[][] dungeons) {
+        int cnt = maxCount;
+        for (int i = 0; i < visited.length; i++) {
+            int needFatigue = dungeons[i][0];
+            int consumeFatigue = dungeons[i][1];
+            if (!visited[i] && k >= needFatigue) {
                 visited[i] = true;
-                maxCnt = Math.max(maxCnt, dfs(k - dungeons[i][1], dungeons, visited, cnt + 1));
+                cnt = Math.max(cnt, dfs(k - consumeFatigue, maxCount + 1, visited, dungeons));
                 visited[i] = false;
             }
         }
-        return maxCnt;
+        return cnt;
     }
 }
