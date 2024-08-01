b83486feed724e72582166cd65df1e30bd8c7341
Kang Min Kim <lnewgatel@gmail.com>
2024-08-02 00:39:37 +0900

    [D2] Title: 숫자를 정렬하자, Time: 139 ms, Memory: 19,168 KB -BaekjoonHub

---
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -0,0 +1,30 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			int[] arr = new int[N];
+			st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			System.out.print("#" + test_case);
+			for (int num : arr) {
+				System.out.print(" " + num);
+			}
+			System.out.println();
+		}
+
+
+	}
+}
