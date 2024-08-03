0202fa41f46c77d4296ed64f051f0fb3e3ab51a3
Kang Min Kim <lnewgatel@gmail.com>
2024-08-03 18:46:58 +0900

    [D2] Title: 간단한 압축 풀기, Time: 120 ms, Memory: 19,728 KB -BaekjoonHub

--- SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.java
+++ SWEA/D2/1946. 간단한 압축 풀기/간단한 압축 풀기.java
@@ -5,38 +5,33 @@ import java.util.StringTokenizer;
 
 public class Solution {
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		for (int test_case = 1; test_case <= T; test_case++) {
-			int N = Integer.parseInt(br.readLine());
-			char[] charStr = new char[N];
-			int[] count = new int[N];
-			int totalCount = 0;
-			for (int i = 0; i < N; i++) {
-				st = new StringTokenizer(br.readLine());
-				charStr[i] = st.nextToken().charAt(0);
-				count[i] = Integer.parseInt(st.nextToken());
-				totalCount += count[i];
-			}
-			int cnt = 0;
-			int idx = 0;
-			System.out.println("#" + test_case);
-			while (cnt < totalCount) {
-				for (int i = 0; i < 10; i++) {
-					if (count[idx] > 0) {
-						System.out.print(charStr[idx]);
-						count[idx] --;
-						cnt++;
-					} else {
-						idx++;
-						if (idx == N) break;
-						i--;
-					}
-				}
-				System.out.println();
-			}
-		}
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+
+        for (int test_case = 1; test_case <= T; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            StringBuilder output = new StringBuilder();
+            System.out.println("#" + test_case);
+
+            int lineLength = 0;
+            for (int i = 0; i < N; i++) {
+                StringTokenizer st = new StringTokenizer(br.readLine());
+                char character = st.nextToken().charAt(0);
+                int count = Integer.parseInt(st.nextToken());
+
+                while (count > 0) {
+                    if (lineLength == 10) {
+                        output.append('\n');
+                        lineLength = 0;
+                    }
+                    output.append(character);
+                    lineLength++;
+                    count--;
+                }
+            }
+            output.append('\n');
+            System.out.print(output.toString());
+        }
+    }
 }
