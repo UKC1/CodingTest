5b137010db16ce3703f365c06cea14a1c73abf66
Kang Min Kim <lnewgatel@gmail.com>
2024-08-04 16:11:14 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 155 ms, Memory: 19,180 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -12,14 +12,10 @@ public class Solution {
 		int T = Integer.parseInt(st.nextToken());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			char[] strToChar = br.readLine().toCharArray();
-			int[] arr = new int[strToChar.length];
-			int updateCnt = 0;
-			for (int i = 0; i < strToChar.length; i++) {
-				if (strToChar[i] - '0' != arr[i]) {
+			int updateCnt = strToChar[0] == '1' ? 1 : 0;
+			for (int i = 0; i < strToChar.length - 1; i++) {
+				if (strToChar[i] != strToChar[i + 1]) {
 					updateCnt++;
-					for (int j = i; j < arr.length; j++) {
-						arr[j] = strToChar[i] - '0';
-					}
 				}
 			}
 			System.out.println("#" + test_case + " " + updateCnt);
