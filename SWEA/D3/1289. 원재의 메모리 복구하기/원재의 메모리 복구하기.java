cf8914a5782b12bd42df5d7e2bb34a9095129af5
Kang Min Kim <lnewgatel@gmail.com>
2024-08-04 16:13:52 +0900

    [D3] Title: 원재의 메모리 복구하기, Time: 108 ms, Memory: 18,636 KB -BaekjoonHub

--- SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
+++ SWEA/D3/1289. 원재의 메모리 복구하기/원재의 메모리 복구하기.java
@@ -1,15 +1,12 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
-import java.util.StringTokenizer;
 
 public class Solution {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
+		int T = Integer.parseInt(br.readLine());
 		for (int test_case = 1; test_case <= T; test_case++) {
 			char[] strToChar = br.readLine().toCharArray();
 			int updateCnt = strToChar[0] == '1' ? 1 : 0;
