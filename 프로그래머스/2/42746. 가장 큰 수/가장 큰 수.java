f0ff739458362d2af5b87a7af493ef11a3ea0559
Kang Min Kim <lnewgatel@gmail.com>
2024-08-05 15:39:28 +0900

    [level 2] Title: 가장 큰 수, Time: 166.76 ms, Memory: 151 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -1,32 +1,18 @@
 import java.util.*;
-
 class Solution {
     public String solution(int[] numbers) {
-        // 숫자 배열을 문자열 배열로 변환
-        String[] numStrs = new String[numbers.length];
+        String[] numToStr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            numStrs[i] = String.valueOf(numbers[i]);
+            numToStr[i] = Integer.toString(numbers[i]);
         }
-
-        // 문자열 배열을 정렬
-        Arrays.sort(numStrs, new Comparator<String>() {
-            @Override
-            public int compare(String a, String b) {
-                return (b + a).compareTo(a + b);  // 내림차순 정렬
-            }
-        });
-
-        // 정렬된 문자열 배열을 결합하여 결과 생성
+        Arrays.sort(numToStr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+        // System.out.println(Arrays.toString(numToStr));
         StringBuilder sb = new StringBuilder();
-        for (String numStr : numStrs) {
-            sb.append(numStr);
+        for (int i = 0; i < numToStr.length; i++) {
+            sb.append(numToStr[i]);
         }
-
-        // 결과가 "0"으로 시작하는 경우 처리 (모든 숫자가 0인 경우)
-        if (sb.charAt(0) == '0') {
-            return "0";
-        }
-
+        
+        if (sb.charAt(0) == '0') return "0";
         return sb.toString();
     }
 }
