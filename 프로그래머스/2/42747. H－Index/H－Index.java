551aa8e3d5531abe26f505414ceab86573dcf8b1
Kang Min Kim <lnewgatel@gmail.com>
2024-08-05 16:58:00 +0900

    [level 2] Title: H-Index, Time: 1.02 ms, Memory: 76.1 MB -BaekjoonHub

--- 프로그래머스/2/42747. H－Index/H－Index.java
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -1,22 +1,30 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] citations) {
+        int n = citations.length;
         Arrays.sort(citations);
-        int idx = 0;
-        int count = 0;
-        while(idx <= citations.length - count) {
-            count = 0;
-            for (int i = 0; i < citations.length; i++) {
-                if (idx >= citations[i]) {
-                    count++;
+        int h = 0;
+        int hIdx = 0;
+        for (int i = 0; i < citations.length; i++) {
+            if (citations[i] <= n - i && i + 1 <= citations[i]) {
+                h = citations[i];
+                hIdx = i;
+            } 
+        }
+        if (h == 0) {
+            for (int newH = h + 1; newH < citations[hIdx + 1]; newH++) {
+                if (newH <= n - hIdx  && hIdx + 1 <= newH) {
+                    h = newH;
                 }
-            
-             }
-            idx++;
+            }
+        } else {
+            for (int newH = h + 1; newH < citations[hIdx + 1]; newH++) {
+                if (newH < n - hIdx  && hIdx + 1 <= newH) {
+                    h = newH;
+                }
+            }    
         }
         
-        return idx - 1;
-        
+        return h;
     }
 }
