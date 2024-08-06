663dae4014f5fd074899920bfa7b55e7f8849578
Kang Min Kim <lnewgatel@gmail.com>
2024-08-06 15:32:33 +0900

    [D3] Title: 농작물 수확하기, Time: 112 ms, Memory: 18,648 KB -BaekjoonHub

--- SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
+++ SWEA/D3/2805. 농작물 수확하기/농작물 수확하기.java
@@ -6,21 +6,24 @@ public class Solution {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int T = Integer.parseInt(br.readLine());
+
         for (int test_case = 1; test_case <= T; test_case++) {
             int N = Integer.parseInt(br.readLine());
-            char[][] arr = new char[N][N];
+            char[][] farm = new char[N][N];
             for (int i = 0; i < N; i++) {
-                arr[i] = br.readLine().toCharArray();
+                farm[i] = br.readLine().toCharArray();
             }
 
             int benefit = 0;
+            int mid = N / 2;
             for (int i = 0; i < N; i++) {
-                for (int j = 0; j < N; j++) {
-                    if (Math.abs(i - N / 2) + Math.abs(j - N / 2) <= N / 2) {
-                        benefit += arr[i][j] - '0';
-                    }
+                int start = Math.abs(mid - i);
+                int end = N - start;
+                for (int j = start; j < end; j++) {
+                    benefit += farm[i][j] - '0';
                 }
             }
+
             System.out.println("#" + test_case + " " + benefit);
         }
     }
