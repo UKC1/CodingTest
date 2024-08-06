3c7a0eee03060d9f2f10357f4f55914749476254
Kang Min Kim <lnewgatel@gmail.com>
2024-08-06 14:32:05 +0900

    [D3] Title: [S/W 문제해결 기본] 8일차 - 암호문3, Time: 166 ms, Memory: 26,872 KB -BaekjoonHub

---
+++ SWEA/D3/1230. ［S／W 문제해결 기본］ 8일차 － 암호문3/［S／W 문제해결 기본］ 8일차 － 암호문3.java
@@ -0,0 +1,54 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        for (int test_case = 1; test_case <= 10; test_case++) {
+            int N = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+            List<Integer> arr = new ArrayList<>();
+            for (int i = 0; i < N; i++) {
+                arr.add(Integer.parseInt(st.nextToken()));
+            }
+
+            int M = Integer.parseInt(br.readLine());
+            st = new StringTokenizer(br.readLine());
+
+            while (st.hasMoreTokens()) {
+                String command = st.nextToken();
+
+                if (command.equals("I")) {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+                    for (int i = 0; i < y; i++) {
+                        arr.add(x + i, Integer.parseInt(st.nextToken()));
+                    }
+                } else if (command.equals("D")) {
+                    int x = Integer.parseInt(st.nextToken());
+                    int y = Integer.parseInt(st.nextToken());
+                    for (int i = 0; i < y; i++) {
+                        arr.remove(x);
+                    }
+                } else if (command.equals("A")) {
+                    int y = Integer.parseInt(st.nextToken());
+                    for (int i = 0; i < y; i++) {
+                        arr.add(Integer.parseInt(st.nextToken()));
+                    }
+                }
+            }
+
+            System.out.print("#" + test_case);
+            for (int i = 0; i < 10; i++) {
+                System.out.print(" " + arr.get(i));
+            }
+            System.out.println(); // 마지막 출력 후 줄바꿈
+        }
+    }
+}
