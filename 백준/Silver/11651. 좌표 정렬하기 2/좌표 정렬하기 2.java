701e0e83f31e06aac11dcdba62af1f9b800a0f82
Kang Min Kim <lnewgatel@gmail.com>
2024-08-07 12:57:08 +0900

    [Silver V] Title: 좌표 정렬하기 2, Time: 1468 ms, Memory: 57328 KB -BaekjoonHub

---
+++ 백준/Silver/11651. 좌표 정렬하기 2/좌표 정렬하기 2.java
@@ -0,0 +1,45 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.StringTokenizer;
+
+public class Main {
+	static class Point {
+		int x;
+		int y;
+		Point(int x, int y) {
+			this.x = x;
+			this.y = y;
+		}
+
+		public int getX() {
+			return x;
+		}
+
+		public int getY() {
+			return y;
+		}
+	}
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int N = Integer.parseInt(st.nextToken());
+		List<Point> list = new ArrayList<>();
+		for (int i = 0; i < N; i++) {
+			st = new StringTokenizer(br.readLine());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			list.add(new Point(x, y));
+		}
+		list.sort(Comparator.comparingInt(Point::getY)
+		.thenComparingInt(Point::getX));
+
+		for (int i = 0; i < list.size(); i++) {
+			System.out.println(list.get(i).getX() + " " + list.get(i).getY());
+		}
+	}
+}
