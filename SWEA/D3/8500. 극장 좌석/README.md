59b5133a3583da706b00fb975f9227a471f2651b
Kang Min Kim <lnewgatel@gmail.com>
2024-08-07 10:53:56 +0900

    [D3] Title: 극장 좌석, Time: 468 ms, Memory: 91,216 KB -BaekjoonHub

---
+++ SWEA/D3/8500. 극장 좌석/README.md
@@ -0,0 +1,15 @@
+# [D3] 극장 좌석 - 8500 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWz5yIfq74QDFARQ) 
+
+### 성능 요약
+
+메모리: 91,216 KB, 시간: 468 ms, 코드길이: 869 Bytes
+
+### 제출 일자
+
+2024-08-07 10:53
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/8500. 극장 좌석/극장 좌석.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			int N = Integer.parseInt(br.readLine());
+			st = new StringTokenizer(br.readLine());
+			int[] arr = new int[N];
+			for (int i = 0; i < N; i++) {
+				arr[i] = Integer.parseInt(st.nextToken());
+			}
+			Arrays.sort(arr);
+			int sum = arr[arr.length - 1] * 2 + N;
+			for (int i = arr.length - 2; i >= 0; i--) {
+				sum += arr[i];
+			}
+			System.out.println("#" + test_case + " " + sum);
+		}
+	}
+}
