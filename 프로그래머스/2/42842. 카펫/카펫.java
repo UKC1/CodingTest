27372c91355f82e48d7972bbcbe35a6cec92e415
Kang Min Kim <lnewgatel@gmail.com>
2024-08-08 22:09:03 +0900

    [level 2] Title: 카펫, Time: 0.05 ms, Memory: 77.5 MB -BaekjoonHub

--- 프로그래머스/2/42842. 카펫/카펫.java
+++ 프로그래머스/2/42842. 카펫/카펫.java
@@ -1,18 +1,17 @@
 class Solution {
     public int[] solution(int brown, int yellow) {
-        int total = brown + yellow;
-        
+        int totalWidth = brown + yellow;
         int[] answer = new int[2];
-        for (int i = 2; i <= Math.sqrt(total); i++) {
-            if (total % i == 0) {
-                int wid = i;
-                int len = total / i;
-                if ((wid - 2) * (len - 2) == yellow) {
-                    answer[0] = len;
-                    answer[1] = wid;
-                }
+        for(int i = 1; i <= totalWidth; i++) {
+            if (totalWidth % i == 0) {
+                int width = totalWidth / i;
+                int height = i;
+                if ((width - 2) * (height - 2) == yellow) {
+                    answer[0] = width;
+                    answer[1] = height;
+                    break;
+                } 
             }
-            
         }
         return answer;
     }
