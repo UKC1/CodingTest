9a1677c5c4ae77ea6a04ae709e8e6dbe121281a5
Kang Min Kim <lnewgatel@gmail.com>
2024-08-08 21:15:36 +0900

    [level 1] Title: 모의고사, Time: 1.00 ms, Memory: 72.7 MB -BaekjoonHub

--- 프로그래머스/1/42840. 모의고사/모의고사.java
+++ 프로그래머스/1/42840. 모의고사/모의고사.java
@@ -1,36 +1,31 @@
 import java.util.*;
 class Solution {
     public int[] solution(int[] answers) {
-        int[] count = new int[3];
+        List<Integer> list = new ArrayList<>();
         int[] first = {1,2,3,4,5};
         int[] second = {2,1,2,3,2,4,2,5};
         int[] third = {3,3,1,1,2,2,4,4,5,5};
-        
-        for (int i = 0 ; i < answers.length; i++) {
-            if (answers[i] == first[i % first.length]) count[0]++;
-            if (answers[i] == second[i % second.length]) count[1]++;
-            if (answers[i] == third[i % third.length]) count[2]++;
-            
+        int[] cntArr = new int[4];
+        for (int i = 0; i < answers.length; i++) {
+            if (answers[i] == first[i % 5] ) cntArr[1]++;
+            if (answers[i] == second[i % 8]) cntArr[2]++;
+            if (answers[i] == third[i % 10]) cntArr[3]++;
         }
-        int maxNum = 0;
-        for (int i = 0; i < count.length; i++) {
-            if (maxNum < count[i]) {
-                maxNum = count[i];
+        
+        int maxCnt = 0;
+        for (int i = 1; i < cntArr.length; i++) {
+            if (maxCnt < cntArr[i]) maxCnt = cntArr[i];
+        }
+        
+        for (int i = 1; i < cntArr.length; i++) {
+            if (maxCnt == cntArr[i]) {
+                list.add(i);
             }
         }
-        
-        List<Integer> list = new ArrayList();
-        for (int i = 0 ; i < count.length; i++) {
-            if (maxNum == count[i]) {
-                list.add(i + 1);
-            }
-        }
-        
         int[] answer = new int[list.size()];
-        for (int i = 0; i < list.size(); i++) {
+        for (int i = 0; i < answer.length; i++) {
             answer[i] = list.get(i);
         }
-        
         return answer;
     }
 }
