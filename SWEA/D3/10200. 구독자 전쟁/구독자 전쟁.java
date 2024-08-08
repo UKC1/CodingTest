100c89498614b88c152e1b18d9756d1733889832
Kang Min Kim <lnewgatel@gmail.com>
2024-08-08 23:41:05 +0900

    [D3] Title: 구독자 전쟁, Time: 138 ms, Memory: 21,520 KB -BaekjoonHub

---
+++ SWEA/D3/10200. 구독자 전쟁/구독자 전쟁.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int T = Integer.parseInt(st.nextToken());
+		for (int test_case = 1; test_case <= T; test_case++) {
+			st = new StringTokenizer(br.readLine());
+			int N = Integer.parseInt(st.nextToken());
+			int A = Integer.parseInt(st.nextToken());
+			int B = Integer.parseInt(st.nextToken());
+			int minNum = A + B - N < 0 ?  0 : A + B - N;
+			int maxNum = A < B ? A : B;
+			System.out.println("#" + test_case + " " + maxNum + " " + minNum);
+		}
+	}
+}
