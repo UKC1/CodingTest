b1148c4b62901a9f2845000d84a1449e7d99e4f6
Kang Min Kim <lnewgatel@gmail.com>
2024-08-09 16:17:57 +0900

    [Silver IV] Title: 다음 소수, Time: 184 ms, Memory: 14380 KB -BaekjoonHub

---
+++ 백준/Silver/4134. 다음 소수/다음 소수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        long N = Long.parseLong(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (long i = 0; i < N; i++) {
+            long num = Long.parseLong(br.readLine());
+            while(!isPrime(num)) {
+                num++;
+            }
+            sb.append(num).append('\n');
+        }
+        System.out.print(sb);
+    }
+
+    static boolean isPrime(long num) {
+        if (num < 2) return false;
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (long i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+}
