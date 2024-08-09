d823cddc2421a951b58dfee5651832bfd81dd43a
Kang Min Kim <lnewgatel@gmail.com>
2024-08-09 10:40:31 +0900

    [Silver II] Title: 좌표 압축, Time: 1848 ms, Memory: 269084 KB -BaekjoonHub

--- 백준/Silver/18870. 좌표 압축/좌표 압축.java
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.java
@@ -1,29 +1,40 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.HashMap;
 import java.util.Map;
 import java.util.StringTokenizer;
-import java.util.TreeMap;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
         int[] arr = new int[N];
+        int[] sortedArr = new int[N];
+        
         StringTokenizer st = new StringTokenizer(br.readLine());
-        Map<Integer, Integer> map = new TreeMap<>();
         for (int i = 0; i < N; i++) {
-            int num = Integer.parseInt(st.nextToken());
-            arr[i] = num;
-            map.put(num, i);
+            arr[i] = Integer.parseInt(st.nextToken());
+            sortedArr[i] = arr[i];
         }
+
+        // 정렬
+        Arrays.sort(sortedArr);
+
+        // 좌표 압축을 위한 Map 생성
+        Map<Integer, Integer> map = new HashMap<>();
         int cnt = 0;
-        for(Integer num : map.keySet()) {
-            map.put(num, cnt++);
+        for (int num : sortedArr) {
+            if (!map.containsKey(num)) {
+                map.put(num, cnt++);
+            }
         }
+
+        // 결과 출력
         StringBuilder sb = new StringBuilder();
         for (int i = 0; i < N; i++) {
-           sb.append(map.get(arr[i])).append(" ");
+            sb.append(map.get(arr[i])).append(" ");
         }
         System.out.println(sb);
     }
