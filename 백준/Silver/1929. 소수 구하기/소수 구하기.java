cca5571fd04e42d958b97cc405c58c8d82abe2e9
Kang Min Kim <lnewgatel@gmail.com>
2024-08-09 16:21:46 +0900

    [Silver III] Title: 소수 구하기, Time: 316 ms, Memory: 19320 KB -BaekjoonHub

---
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int M = Integer.parseInt(st.nextToken());
+        int N = Integer.parseInt(st.nextToken());
+
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = M; i <= N; i++) {
+            if (isPrime(i)) {
+                sb.append(i).append('\n');
+            }
+        }
+        System.out.print(sb);
+    }
+
+    static boolean isPrime(int num) {
+        if (num < 2) return false;
+        if (num == 2 || num == 3) return true;
+        if (num % 2 == 0 || num % 3 == 0) return false;
+        for (int i = 5; i * i <= num; i += 6) {
+            if (num % i == 0 || num % (i + 2) == 0) return false;
+        }
+        return true;
+    }
+}
