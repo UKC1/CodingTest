0a6570f497af828b0ad930e35b4740a045ee0bd9
Kang Min Kim <lnewgatel@gmail.com>
2024-08-09 09:06:28 +0900

    [D3] Title: 규영이와 인영이의 카드게임, Time: 2,926 ms, Memory: 20,416 KB -BaekjoonHub

--- SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
+++ SWEA/D3/6808. 규영이와 인영이의 카드게임/규영이와 인영이의 카드게임.java
@@ -1,8 +1,6 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Solution {
@@ -11,6 +9,7 @@ public class Solution {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int T = Integer.parseInt(st.nextToken());
+
         for (int test_case = 1; test_case <= T; test_case++) {
             st = new StringTokenizer(br.readLine());
             boolean[] cards = new boolean[19];
@@ -29,32 +28,32 @@ public class Solution {
                 }
             }
 
-            List<int[]> list = new ArrayList<>();
-            dfs(9, 9, new boolean[9], iny, new int[9], 0, list);
-            int totalKyuWin = 0;
-            for (int[] inyes : list ) {
-                int inyScore = 0;
-                int kyuScore = 0;
-                for (int i = 0; i < kyu.length; i++) {
-                    if (kyu[i] > inyes[i]) {
-                        kyuScore += kyu[i] + inyes[i];
-                    } else if (kyu[i] < inyes[i]) {
-                        inyScore += kyu[i] + inyes[i];
-                    }
-                }
-                if (kyuScore > inyScore) {
-                    totalKyuWin++;
-                }
-            }
-            System.out.println("#" + test_case + " " + totalKyuWin + " " + (list.size() - totalKyuWin));
+            int[] result = new int[2]; // [0]: 규영 승리 횟수, [1]: 인영 승리 횟수
+            dfs(9, new boolean[9], iny, new int[9], 0, kyu, result);
+
+            System.out.println("#" + test_case + " " + result[0] + " " + result[1]);
         }
     }
 
-    static void dfs(int N, int M, boolean[] visited,int[] iny, int[] arr, int idx, List<int[]> list) {
-        if (idx == M) {
-            int[] newArr = new int[M];
-            System.arraycopy(arr, 0, newArr, 0, M);
-            list.add(newArr);
+    static void dfs(int N, boolean[] visited, int[] iny, int[] arr, int idx, int[] kyu, int[] result) {
+        if (idx == N) {
+            int kyuScore = 0;
+            int inyScore = 0;
+
+            for (int i = 0; i < N; i++) {
+                if (kyu[i] > arr[i]) {
+                    kyuScore += kyu[i] + arr[i];
+                } else if (kyu[i] < arr[i]) {
+                    inyScore += kyu[i] + arr[i];
+                }
+            }
+
+            if (kyuScore > inyScore) {
+                result[0]++;
+            } else if (inyScore > kyuScore) {
+                result[1]++;
+            }
+
             return;
         }
 
@@ -62,7 +61,7 @@ public class Solution {
             if (!visited[i]) {
                 visited[i] = true;
                 arr[idx] = iny[i];
-                dfs(N, M, visited, iny, arr, idx + 1, list);
+                dfs(N, visited, iny, arr, idx + 1, kyu, result);
                 visited[i] = false;
             }
         }
