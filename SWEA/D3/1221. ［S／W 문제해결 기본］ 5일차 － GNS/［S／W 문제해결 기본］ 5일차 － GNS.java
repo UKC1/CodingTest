ba65c674ce7592c15edb76ee8c19b6fccd6655b2
Kang Min Kim <lnewgatel@gmail.com>
2024-08-09 23:37:30 +0900

    [D3] Title: [S/W 문제해결 기본] 5일차 - GNS, Time: 191 ms, Memory: 36,036 KB -BaekjoonHub

--- SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
+++ SWEA/D3/1221. ［S／W 문제해결 기본］ 5일차 － GNS/［S／W 문제해결 기본］ 5일차 － GNS.java
@@ -1,65 +1,43 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Solution {
-	static class Number {
-		String Name;
-		int value;
-		Number(String Name, int value) {
-			this.Name = Name;
-			this.value = value;
-		}
 
-		public String getName() {
-			return Name;
-		}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int T = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
-		public int getValue() {
-			return value;
-		}
-	}
-
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int T = Integer.parseInt(st.nextToken());
-		StringBuilder sb = new StringBuilder();
-		for (int test_case = 1; test_case <= T; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			String str = st.nextToken();
-			int N = Integer.parseInt(st.nextToken());
-			st = new StringTokenizer(br.readLine());
-			Map<String, Integer> map = new HashMap<>();
-			map.put("ZRO", 0);
-			map.put("ONE", 1);
-			map.put("TWO", 2);
-			map.put("THR", 3);
-			map.put("FOR", 4);
-			map.put("FIV", 5);
-			map.put("SIX", 6);
-			map.put("SVN", 7);
-			map.put("EGT", 8);
-			map.put("NIN", 9);
-			List<Number> list = new ArrayList<>();
-			for (int i = 0; i < N; i++) {
-				String name = st.nextToken();
-				int value = map.get(name);
-				list.add(new Number(name, value));
-			}
-			list.sort(Comparator.comparingInt(Number::getValue));
-			sb.append("#").append(test_case).append("\n");
-			for (Number number : list) {
-				sb.append(number.getName()).append(" ");
-			}
-			sb.append("\n");
-		}
-		System.out.print(sb);
-	}
+        String[] nums = {"ZRO", "ONE", "TWO", "THR", "FOR", "FIV", "SIX", "SVN", "EGT", "NIN"};
+        
+        for (int test_case = 1; test_case <= T; test_case++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            String str = st.nextToken();  // 케이스 번호 가져오기 (무시)
+            int N = Integer.parseInt(st.nextToken());  // 숫자 갯수
+            
+            int[] count = new int[10];
+            
+            st = new StringTokenizer(br.readLine());
+            for (int i = 0; i < N; i++) {
+                String name = st.nextToken();
+                for (int j = 0; j < nums.length; j++) {
+                    if (name.equals(nums[j])) {
+                        count[j]++;
+                        break;
+                    }
+                }
+            }
+            
+            sb.append("#").append(test_case).append("\n");
+            for (int i = 0; i < 10; i++) {
+                for (int j = 0; j < count[i]; j++) {
+                    sb.append(nums[i]).append(" ");
+                }
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
 }
