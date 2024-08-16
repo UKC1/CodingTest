0e163fd960eb6d6be422f1c8c759ce40c5313d23
Kang Min Kim <lnewgatel@gmail.com>
2024-08-16 20:13:26 +0900

    [level 2] Title: 모음 사전, Time: 0.26 ms, Memory: 72.2 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,31 +1,21 @@
 import java.util.*;
 
 class Solution {
-    static String[] letters = {"A", "E", "I", "O", "U"};
-    static int answer = 0;
-    static int count = 0;
-    
     public int solution(String word) {
-        dfs("", word);
-        return answer;
-    }
-    
-    private void dfs(String current, String target) {
-        // 단어가 목표 단어와 일치할 때의 순서를 기록
-        if (current.equals(target)) {
-            answer = count;
-            return;
+        Map<Character, Integer> map = new HashMap<>();
+        map.put('A', 1);
+        map.put('E', 2);
+        map.put('I', 3);
+        map.put('O', 4);
+        map.put('U', 5);
+        int sum = 0;
+        int[] values = {781, 156, 31, 6, 1};
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            int value = (map.get(c) - 1) * values[i];
+            sum += value;
         }
-        
-        // 단어의 길이가 5를 넘지 않도록 제한
-        if (current.length() >= 5) {
-            return;
-        }
-        
-        // 각 글자를 추가하면서 새로운 단어를 생성
-        for (int i = 0; i < letters.length; i++) {
-            count++;  // 생성된 단어의 순서를 증가시킴
-            dfs(current + letters[i], target);  // 다음 단계의 단어 생성
-        }
+        sum += word.length();
+        return sum;
     }
 }
