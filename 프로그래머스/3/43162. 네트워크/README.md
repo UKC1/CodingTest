2b2c6f05ab1e817541504ae40efbeb4e054693d9
Kang Min Kim <lnewgatel@gmail.com>
2024-08-17 12:14:25 +0900

    [level 3] Title: 네트워크, Time: 0.29 ms, Memory: 80.2 MB -BaekjoonHub

--- 프로그래머스/3/43162. 네트워크/README.md
+++ 프로그래머스/3/43162. 네트워크/README.md
@@ -1,10 +1,10 @@
 # [level 3] 네트워크 - 43162 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43162) 
 
 ### 성능 요약
 
-메모리: 75.4 MB, 시간: 0.11 ms
+메모리: 80.2 MB, 시간: 0.29 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 06월 05일 17:03:42
+2024년 08월 17일 12:14:22
 
 ### 문제 설명
 
--- 프로그래머스/3/43162. 네트워크/네트워크.java
+++ 프로그래머스/3/43162. 네트워크/네트워크.java
@@ -2,21 +2,21 @@ class Solution {
     public int solution(int n, int[][] computers) {
         int answer = 0;
         boolean[] visited = new boolean[n];
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < computers.length; i++) {
             if (!visited[i]) {
                 answer++;
-                dfs(computers, visited, i);        
+                dfs(computers, visited, i);
             }
         }
         return answer;
     }
     
-    public void dfs(int[][] computers, boolean[] visited, int start) {
+    void dfs(int[][] computers, boolean[] visited, int start) {
         visited[start] = true;
         for (int i = 0; i < computers.length; i++) {
-            if (!visited[i] && computers[start][i] == 1) {
+            if (computers[start][i] == 1 && !visited[i]) {
                 dfs(computers, visited, i);
             }
         }
-    }
+    } 
 }
