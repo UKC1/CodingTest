28a2e69dca02c5fd77d1a9d80ca7eb300e45c7fd
Kang Min Kim <lnewgatel@gmail.com>
2024-08-17 13:37:11 +0900

    [Bronze II] Title: 피보나치 수 5, Time: 96 ms, Memory: 14220 KB -BaekjoonHub

--- 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
+++ 백준/Bronze/10870. 피보나치 수 5/피보나치 수 5.java
@@ -1,26 +1,23 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		int N = Integer.parseInt(st.nextToken());
-		System.out.println(fibonacci(N));
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        System.out.println(fibonacci(N));
+    }
 
-	static int fibonacci(int N) {
-		if (N < 2) return N;
-		int a = 0;
-		int b = 1;
-		int c = 0;
-		for (int i = 1; i < N; i++) {
-			c = a + b;
-			a = b;
-			b = c;
-		}
-		return c;
-	}
+    static int fibonacci(int N) {
+        if (N < 2) return N;
+        int prev = 0;
+        int curr = 1;
+        for (int i = 2; i <= N; i++) {
+            int next = prev + curr;
+            prev = curr;
+            curr = next;
+        }
+        return curr;
+    }
 }
