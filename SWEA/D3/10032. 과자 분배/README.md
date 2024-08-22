535e8a308e9fcefa498efc3f744f482f2e4e0f48
Kang Min Kim <lnewgatel@gmail.com>
2024-08-22 11:03:50 +0900

    [D3] Title: 과자 분배, Time: 144 ms, Memory: 30,964 KB -BaekjoonHub

---
+++ SWEA/D3/10032. 과자 분배/README.md
@@ -0,0 +1,15 @@
+# [D3] 과자 분배 - 10032 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AXJZ6_6KCLcDFAU3) 
+
+### 성능 요약
+
+메모리: 30,964 KB, 시간: 144 ms, 코드길이: 792 Bytes
+
+### 제출 일자
+
+2024-08-22 11:03
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/10032. 과자 분배/과자 분배.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 1; test_case <= T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int K = Integer.parseInt(st.nextToken());
+            sb.append("#").append(test_case).append(" ").append(N % K == 0 ? 0 : 1).append("\n");
+        }
+        System.out.print(sb);
+    }
+}
