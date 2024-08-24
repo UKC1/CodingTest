29fb9f4023e0e1295f1e9ba63c344bde32609bf1
Kang Min Kim <lnewgatel@gmail.com>
2024-08-24 19:12:19 +0900

    [level 3] Title: 이중우선순위큐, Time: 105.77 ms, Memory: 130 MB -BaekjoonHub

---
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -0,0 +1,29 @@
+import java.util.*;
+class Solution {
+    public int[] solution(String[] operations) {
+        PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>(); 
+        for (String operation : operations) {
+            String command = operation.split(" ")[0];
+            int num = Integer.parseInt(operation.split(" ")[1]);
+            if (command.equals("I")) {
+                maxPQ.offer(num);
+                minPQ.offer(num);
+            } else {
+                if (num == 1) {
+                    if (maxPQ.isEmpty()) continue;
+                    int maxNum = maxPQ.poll();
+                    minPQ.remove(Integer.valueOf(maxNum));
+                } else if (num == -1) {
+                    if (minPQ.isEmpty()) continue;
+                    int minNum = minPQ.poll();
+                    maxPQ.remove(Integer.valueOf(minNum));
+                }
+            }
+        }
+        int[] answer = new int[2];
+        if (!maxPQ.isEmpty()) answer[0] = maxPQ.poll();
+        if (!minPQ.isEmpty()) answer[1] = minPQ.poll();
+        return answer;
+    }
+}
