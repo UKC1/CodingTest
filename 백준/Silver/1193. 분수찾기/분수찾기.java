b2bc0fc76b900f64d3041f0fd58d13d105cae12d
Kang Min Kim <lnewgatel@gmail.com>
2024-08-25 19:40:34 +0900

    [Silver V] Title: 분수찾기, Time: 136 ms, Memory: 16052 KB -BaekjoonHub

--- 백준/Silver/1193. 분수찾기/분수찾기.java
+++ 백준/Silver/1193. 분수찾기/분수찾기.java
@@ -1,29 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
 
-		int N = Integer.parseInt(st.nextToken());
-		int cnt = 0;
-		loop : for (int i = 1; i <= N; i++) {
-			for (int j = 1; j <= i + 1 ; j++) {
-				if (j < i + 1) {
-					cnt++;
-				}
-				if (cnt == N) {
-					if ((i + 1) % 2 != 0) {
-						System.out.print(j + "/" + (i + 1 - j));
-					} else {
-						System.out.print((i + 1 - j) + "/" + j);
-					}
-					break loop;
-				}
-			}
-		}
-	}
+        int cnt = 0;
+        loop: for (int i = 1; i <= N; i++) {
+            for (int j = 1; j < i; j++) {
+                cnt++;
+                if (cnt == N) {
+                    if (i % 2 == 0) {
+                        System.out.print((i - j) + "/" + j + " ");
+                    } else {
+                        System.out.print(j + "/" + (i - j) + " ");
+                    }
+                    break loop;
+                }
+            }
+        }
+
+        if (N == 1) System.out.print("1/1");
+        else if (N == 2) System.out.print("1/2");
+    }
 }
