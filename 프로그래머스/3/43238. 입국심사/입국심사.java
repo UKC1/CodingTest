c9c771fb4d79e68355ef637a3c9f3da0d43e5558
Kang Min Kim <lnewgatel@gmail.com>
2024-08-26 16:13:02 +0900

    [level 3] Title: 입국심사, Time: 104.15 ms, Memory: 90 MB -BaekjoonHub

---
+++ 프로그래머스/3/43238. 입국심사/입국심사.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+
+class Solution {
+    public long solution(int n, int[] times) {
+        Arrays.sort(times);
+        
+        long low = 1;
+        long high = (long) times[times.length - 1] * n;
+        long answer = high;
+
+        while (low <= high) {
+            long mid = (low + high) / 2;
+            long peopleProcessed = 0;
+
+            for (int time : times) {
+                peopleProcessed += mid / time;
+                if (peopleProcessed >= n) break;  // 이미 필요한 사람 수를 채웠으면 더 이상 계산할 필요 없음
+            }
+
+            if (peopleProcessed >= n) {
+                answer = mid;  // 현재 mid 시간이 가능한 경우, 더 짧은 시간 시도
+                high = mid - 1;
+            } else {
+                low = mid + 1;  // mid 시간이 부족한 경우, 더 긴 시간 시도
+            }
+        }
+
+        return answer;
+    }
+}
