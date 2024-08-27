97cc3fee8d3dc12a3ae758a2762c62f1c59c8708
Kang Min Kim <lnewgatel@gmail.com>
2024-08-27 11:45:06 +0900

    [D3] Title: 부분 수열의 합, Time: 842 ms, Memory: 19,596 KB -BaekjoonHub

--- SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
+++ SWEA/D3/2817. 부분 수열의 합/부분 수열의 합.java
@@ -4,12 +4,12 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Solution {
-    static int cnt;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
         StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
+
         for (int test_case = 1; test_case <= T; test_case++) {
             st = new StringTokenizer(br.readLine());
             int N = Integer.parseInt(st.nextToken());
@@ -20,25 +20,26 @@ public class Solution {
                 numbers[i] = Integer.parseInt(st.nextToken());
             }
 
-            cnt = 0;
-            for (int i = 1; i <= N; i++) {
-                combination(numbers, i, 0, K, 0, 0);
+            int cnt = 0;
+
+            // 1 << N 은 2^N을 의미함. 0부터 2^N - 1까지 모든 경우의 수를 탐색
+            for (int i = 1; i < (1 << N); i++) {
+                int sum = 0;
+                // i를 비트마스크로 사용
+                for (int j = 0; j < N; j++) {
+                    // i의 j번째 비트가 1이면 numbers[j]를 포함
+                    if ((i & (1 << j)) != 0) {
+                        sum += numbers[j];
+                    }
+                }
+                if (sum == K) {
+                    cnt++;
+                }
             }
+
             sb.append("#").append(test_case).append(" ").append(cnt).append("\n");
         }
+
         System.out.print(sb);
-    }
-
-    static void combination(int[] numbers, int R, int sum, int target, int idx, int start) {
-        if (idx == R) {
-            if (sum == target) {
-                cnt++;
-            }
-            return;
-        }
-
-        for (int i = start; i < numbers.length; i++) {
-            combination(numbers, R, sum + numbers[i], target, idx + 1, i + 1);
-        }
     }
 }
