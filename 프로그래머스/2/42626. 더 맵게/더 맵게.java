32e4f70945ddf14b40ed23f4a6fdd6de1e09b844
Kang Min Kim <lnewgatel@gmail.com>
2024-08-27 17:53:23 +0900

    [level 2] Title: 더 맵게, Time: 1585.17 ms, Memory: 122 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.java
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.java
@@ -1,26 +1,22 @@
 import java.util.*;
-
 class Solution {
     public int solution(int[] scoville, int K) {
-        int answer = 0;
-        PriorityQueue<Integer> pq = new PriorityQueue();
-        for (int sco : scoville) {
-            pq.offer(sco);
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+        for (int scov : scoville) {
+            pq.offer(scov);
         }
         
-        while(pq.size() > 1 && pq.peek() < K)  {
-            int firstMin = pq.poll();
-            int secondMin = pq.poll();
-            pq.offer(firstMin + secondMin * 2);
-            answer++;
+        int cnt = 0;
+        while(pq.size() > 1 && pq.peek() < K) {
+            int newScov = pq.poll() + pq.poll() * 2;
+            pq.offer(newScov);
+            cnt++;
         }
         
         if (pq.peek() < K) {
             return -1;
         }
         
-        
-        
-        return answer;
+        return cnt;
     }
 }
