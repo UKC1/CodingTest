10e4d88f38b2ee5ce76123b93a63e964ae4ceb22
Kang Min Kim <lnewgatel@gmail.com>
2024-08-28 10:11:00 +0900

    [Silver II] Title: 최대 힙, Time: 288 ms, Memory: 26044 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Comparator;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
+        StringBuilder sb = new StringBuilder();
+        int N = Integer.parseInt(br.readLine());
+        for (int i = 0; i < N; i++) {
+            int num = Integer.parseInt(br.readLine());
+            if (num == 0) {
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
+            } else {
+                pq.offer(num);
+            }
+        }
+        System.out.print(sb);
+    }
+}
