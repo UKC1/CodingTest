0bf27278593f6e561faa13e2fcc231d1d59142d3
Kang Min Kim <lnewgatel@gmail.com>
2024-08-30 16:49:47 +0900

    [level 4] Title: 징검다리, Time: 35.38 ms, Memory: 85.8 MB -BaekjoonHub

--- 프로그래머스/4/43236. 징검다리/징검다리.java
+++ 프로그래머스/4/43236. 징검다리/징검다리.java
@@ -1,42 +1,35 @@
-import java.util.*;
-
+import java.util.Arrays;
 class Solution {
     public int solution(int distance, int[] rocks, int n) {
+        int answer = 0;
         Arrays.sort(rocks);
         
-        int left = 1; // 최소 거리
-        int right = distance; // 최대 거리
-        int answer = 0;
-        
+        int left = 0;
+        int right = distance;
         while (left <= right) {
             int mid = left + (right - left) / 2;
-            int removeCount = 0;
-            int prev = 0; // 시작점
-
+            int removeStone = 0;
+            int prev = 0;
+            
             for (int rock : rocks) {
                 if (rock - prev < mid) {
-                    // 바위를 제거
-                    removeCount++;
+                    removeStone++;
                 } else {
-                    prev = rock; // 바위를 남겨둠
+                    prev = rock;
                 }
             }
             
-            // 마지막 남은 거리 처리
             if (distance - prev < mid) {
-                removeCount++;
+                removeStone++;
             }
             
-            // 제거한 바위의 수가 n보다 많으면, 최소 거리를 줄임
-            if (removeCount > n) {
-                right = mid - 1;
-            } else {
-                // 제거한 바위의 수가 n 이하이면, 답을 갱신하고 최소 거리를 늘림
+            if (removeStone <= n) {
                 answer = mid;
                 left = mid + 1;
+            } else {
+                right = mid - 1;
             }
         }
-        
         return answer;
     }
 }
