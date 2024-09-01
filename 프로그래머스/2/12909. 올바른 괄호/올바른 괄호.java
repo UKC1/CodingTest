b8832b1df4ecdf6b9a20d217f5adba176039d7e6
Kang Min Kim <lnewgatel@gmail.com>
2024-09-01 19:57:31 +0900

    [level 2] Title: 올바른 괄호, Time: 17.28 ms, Memory: 53.9 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,19 @@
+import java.util.*;
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        for (char c : s.toCharArray()) {
+            if (c == '(') {
+                stack.push('(');
+            } else if (c == ')') {
+                if (stack.isEmpty()) {
+                    return false;
+                } else {
+                    stack.pop();
+                }
+            }
+        }
+        return stack.isEmpty();
+    }
+}
