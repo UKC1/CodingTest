f46842672a5c19f63acb942fe94e9507fce420b8
Kang Min Kim <lnewgatel@gmail.com>
2024-09-01 19:09:01 +0900

    [level 2] Title: 기능개발, Time: 0.18 ms, Memory: 69.4 MB -BaekjoonHub

--- 프로그래머스/2/42586. 기능개발/기능개발.java
+++ 프로그래머스/2/42586. 기능개발/기능개발.java
@@ -1,33 +1,33 @@
-import java.util.ArrayList;
-import java.util.LinkedList;
-import java.util.Queue;
-
+import java.util.*;
 class Solution {
     public int[] solution(int[] progresses, int[] speeds) {
-        Queue<Integer> remainDays = new LinkedList<>();
-        ArrayList<Integer> releaseCounts = new ArrayList<>();
-
-        // 각 기능별로 완료되기까지 남은 일수 계산
-        for (int i = 0; i < progresses.length; i++) {
-            int remainProgress = 100 - progresses[i];
-            int day = remainProgress / speeds[i] + (remainProgress % speeds[i] > 0 ? 1 : 0);
-            remainDays.offer(day);
-        }
-
-        while (!remainDays.isEmpty()) {
-            int releaseDay = remainDays.poll();
-            int count = 1;
-
-            // 현재 기능보다 먼저 완성되는 기능들을 함께 배포
-            while (!remainDays.isEmpty() && remainDays.peek() <= releaseDay) {
-                count++;
-                remainDays.poll();
+        // 순서가 보장이 되어야 함!
+        // 앞에 있는 애가 나가야 다음에!
+        Queue<Integer> queue = new ArrayDeque<>();
+        int totalCnt = 0;
+        int start = 0;
+        
+        while (totalCnt < progresses.length) {
+            for (int i = 0; i < progresses.length; i++) {
+                progresses[i] += speeds[i];
+            }    
+            // 맨 처음이 배포 가능하면?
+            if (progresses[start] >= 100) {
+                // 배포 가능한 애들 다 찾아!
+                int cnt = 0;
+                while(start < progresses.length && progresses[start] >= 100) {
+                    cnt++;
+                    start++;
+                }
+                totalCnt += cnt;
+                queue.offer(cnt);
             }
-
-            releaseCounts.add(count);
         }
-
-        // 결과를 배열로 변환
-        return releaseCounts.stream().mapToInt(i -> i).toArray();
+        int[] answer = new int[queue.size()];
+        for (int i = 0; i < answer.length; i++) {
+            answer[i] = queue.poll();
+        }
+        
+        return answer;
     }
 }
