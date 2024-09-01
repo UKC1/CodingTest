71155dc36fdc5487e379315c196faff82886fba0
Kang Min Kim <lnewgatel@gmail.com>
2024-09-01 17:06:45 +0900

    [level 3] Title: 이중우선순위큐, Time: 110.56 ms, Memory: 121 MB -BaekjoonHub

--- 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
+++ 프로그래머스/3/42628. 이중우선순위큐/이중우선순위큐.java
@@ -1,27 +1,28 @@
 import java.util.*;
 class Solution {
     public int[] solution(String[] operations) {
+        int[] answer = new int[2];
         PriorityQueue<Integer> maxPQ = new PriorityQueue<>(Comparator.reverseOrder());
-        PriorityQueue<Integer> minPQ = new PriorityQueue<>(); 
-        for (String operation : operations) {
-            String command = operation.split(" ")[0];
-            int num = Integer.parseInt(operation.split(" ")[1]);
-            if (command.equals("I")) {
+        PriorityQueue<Integer> minPQ = new PriorityQueue<>();
+        for (String operate : operations) {
+            char command = operate.split(" ")[0].charAt(0);
+            int num = Integer.parseInt(operate.split(" ")[1]);
+            
+            if (command == 'I') {
                 maxPQ.offer(num);
                 minPQ.offer(num);
             } else {
                 if (num == 1) {
-                    if (maxPQ.isEmpty()) continue;
-                    int maxNum = maxPQ.poll();
-                    minPQ.remove(maxNum);
+                    if (!maxPQ.isEmpty()) {
+                        minPQ.remove(maxPQ.poll());
+                    }
                 } else if (num == -1) {
-                    if (minPQ.isEmpty()) continue;
-                    int minNum = minPQ.poll();
-                    maxPQ.remove(minNum);
+                    if (!minPQ.isEmpty()) {
+                        maxPQ.remove(minPQ.poll());
+                    }
                 }
             }
         }
-        int[] answer = new int[2];
         if (!maxPQ.isEmpty()) answer[0] = maxPQ.poll();
         if (!minPQ.isEmpty()) answer[1] = minPQ.poll();
         return answer;
