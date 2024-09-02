c4e6d86becaded3ced60e60e5173e7d2d3e6e464
Kang Min Kim <lnewgatel@gmail.com>
2024-09-02 12:39:17 +0900

    [level 2] Title: 프로세스, Time: 1.78 ms, Memory: 76.7 MB -BaekjoonHub

--- 프로그래머스/2/42587. 프로세스/프로세스.java
+++ 프로그래머스/2/42587. 프로세스/프로세스.java
@@ -1,40 +1,30 @@
-import java.util.Deque;
-import java.util.LinkedList;
-
+import java.util.*;
 class Solution {
     public int solution(int[] priorities, int location) {
         int answer = 0;
-        Deque<Integer> deque = new LinkedList<>();
+        Queue<Integer> priorityQ = new ArrayDeque<>();
+        Queue<Integer> idxQ = new ArrayDeque<>();
+        List<Integer> list = new ArrayList<>();
         for (int i = 0; i < priorities.length; i++) {
-            deque.offer(priorities[i]);
+            priorityQ.offer(priorities[i]);
+            list.add(priorities[i]);
+            idxQ.offer(i);
         }
-
-        while (!deque.isEmpty()) {
-            int process = deque.poll();
-            boolean isFirst = true;
-            for (Integer i : deque) {
-                if (process < i) {
-                    isFirst = false;
-                    break;
+        list.sort(Comparator.reverseOrder());
+        while(!priorityQ.isEmpty()) {
+            int num = priorityQ.poll();
+            int idx = idxQ.poll();
+            if (num == list.get(0)) {
+                if (location == idx) {
+                    return priorities.length - priorityQ.size(); 
                 }
-            }
-
-            if (isFirst) {
-                if (location == 0) {
-                    return priorities.length - deque.size();
-                }
-                location--;
+                list.remove(0);
             } else {
-                deque.addLast(process);
-                if (location == 0) {
-                    location = deque.size() - 1;
-                } else {
-                    location--;
-                }
+                priorityQ.offer(num);
+                idxQ.offer(idx);
             }
-            
         }
-
+        
         return answer;
     }
 }
