246333cb57ecd52f19ba7e19046e2b19b0bd3960
Kang Min Kim <lnewgatel@gmail.com>
2024-10-15 13:29:53 +0900

    [Silver I] Title: 정수 삼각형, Time: 240 ms, Memory: 25648 KB -BaekjoonHub

--- 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.java
@@ -1,15 +1,16 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        
         int N = Integer.parseInt(br.readLine());
         int[][] triangle = new int[N][];
+
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
             triangle[i] = new int[i + 1];
@@ -18,24 +19,12 @@ public class Main {
             }
         }
 
-        for (int i = 1; i < N; i++) {
+        for (int i = N - 2; i >= 0; i--) {
             for (int j = 0; j <= i; j++) {
-                if (j == 0) {
-                    triangle[i][0] += triangle[i - 1][0];
-                } else if (j == i) {
-                    triangle[i][j] += triangle[i - 1][j - 1];
-                } else {
-                    triangle[i][j] += Math.max(triangle[i - 1][j - 1], triangle[i - 1][j]);
-                }
+                triangle[i][j] += Math.max(triangle[i + 1][j], triangle[i + 1][j + 1]);
             }
         }
 
-        int maxSum = 0;
-        for (int i = 0; i < N; i++) {
-            if (maxSum < triangle[N - 1][i]) {
-                maxSum = triangle[N - 1][i];
-            }
-        }
-        System.out.print(maxSum);
+        System.out.print(triangle[0][0]);
     }
 }
