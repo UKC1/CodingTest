2fad4cbb395a988d143c2c5415a6682556f90b16
Kang Min Kim <lnewgatel@gmail.com>
2024-10-19 22:56:24 +0900

    [Silver I] Title: 인간-컴퓨터 상호작용, Time: 644 ms, Memory: 94780 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
+++ 백준/Silver/16139. 인간－컴퓨터 상호작용/인간－컴퓨터 상호작용.java
@@ -6,24 +6,46 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+        // 입력 문자열
         String str = br.readLine();
-        StringBuilder sb = new StringBuilder();
-        StringTokenizer st;
+        int len = str.length();
+        
+        // 누적 합 배열 선언: 26개의 알파벳 각각에 대해 [0...len] 구간 저장
+        int[][] prefixSum = new int[26][len + 1];
+
+        // 각 알파벳별로 누적 합 계산
+        for (int i = 0; i < len; i++) {
+            int charIndex = str.charAt(i) - 'a';  // 알파벳 인덱스 계산 ('a' -> 0, 'b' -> 1, ...)
+            
+            // 현재 위치까지의 누적 합을 복사
+            for (int j = 0; j < 26; j++) {
+                prefixSum[j][i + 1] = prefixSum[j][i];
+            }
+            
+            // 현재 문자의 누적 합 증가
+            prefixSum[charIndex][i + 1]++;
+        }
+        
+        // 쿼리 수 입력
         int N = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        // 각 쿼리 처리
         for (int i = 0; i < N; i++) {
-            st = new StringTokenizer(br.readLine());
+            StringTokenizer st = new StringTokenizer(br.readLine());
             char c = st.nextToken().charAt(0);
             int start = Integer.parseInt(st.nextToken());
             int end = Integer.parseInt(st.nextToken());
-
-            int cnt = 0;
-            for (int j = start; j <= end; j++) {
-                if (str.charAt(j) == c) {
-                    cnt++;
-                }
-            }
-            sb.append(cnt).append("\n");
+            
+            int charIndex = c - 'a';  // 쿼리에서 찾는 문자의 인덱스 계산
+            
+            // 해당 문자의 구간 [start, end]에서 등장 횟수는 prefixSum 배열을 사용해 계산
+            int result = prefixSum[charIndex][end + 1] - prefixSum[charIndex][start];
+            sb.append(result).append("\n");
         }
+        
+        // 결과 출력
         System.out.print(sb);
     }
 }
