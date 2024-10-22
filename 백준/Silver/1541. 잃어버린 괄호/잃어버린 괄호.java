da1bf4c93faca7ebbb5c6c070660019d53cbef5d
Kang Min Kim <lnewgatel@gmail.com>
2024-10-22 17:06:16 +0900

    [Silver II] Title: 잃어버린 괄호, Time: 104 ms, Memory: 14200 KB -BaekjoonHub

--- 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
+++ 백준/Silver/1541. 잃어버린 괄호/잃어버린 괄호.java
@@ -3,24 +3,28 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String[] minus = br.readLine().split("-");
-		int minSum = 0;
-		for (int i = 0; i < minus.length; i++) {
-			int minusSum = 0;
-			if (minus[i].contains("+")) {
-				String[] plus = minus[i].replace("+", " ").split(" ");
-				for (int j = 0; j < plus.length; j++) {
-					minusSum += Integer.parseInt(plus[j]);
-				}
-			} else {
-				minusSum += Integer.parseInt(minus[i]);
-			}
-			
-			if (i == 0) minSum += minusSum;
-			else minSum -= minusSum;
-		}
-		System.out.print(minSum);
-	}
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String[] minus = br.readLine().split("-");
+
+        int minSum = 0;
+
+        // 첫 번째 그룹은 더하고, 이후 그룹은 빼기
+        for (int i = 0; i < minus.length; i++) {
+            int sum = 0;
+            String[] plus = minus[i].split("\\+");  // "+"로 분리하여 합 계산
+            for (String num : plus) {
+                sum += Integer.parseInt(num);
+            }
+
+            // 첫 번째 그룹은 더하고, 나머지는 빼기
+            if (i == 0) {
+                minSum += sum;
+            } else {
+                minSum -= sum;
+            }
+        }
+
+        System.out.println(minSum);
+    }
 }
