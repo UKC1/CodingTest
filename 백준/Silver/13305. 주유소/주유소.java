36fb35e19cf075b4ec1dfb83034b11db1fc8107d
Kang Min Kim <lnewgatel@gmail.com>
2024-10-23 22:09:51 +0900

    [Silver III] Title: 주유소, Time: 296 ms, Memory: 30908 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,53 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int[] dist = new int[N - 1];
+        int[] cost = new int[N];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N - 1; i++) {
+            dist[i] = Integer.parseInt(st.nextToken());
+        }
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < N; i++) {
+            cost[i] = Integer.parseInt(st.nextToken());
+        }
+
+        long minCost = 0;
+        int currentPos = 0;
+        while(currentPos < N - 1) {
+
+            int currentCost = cost[currentPos];
+            int nextPos = -1;
+            for (int i = currentPos + 1; i < N; i++) {
+                if (currentCost > cost[i]) {
+                    nextPos = i;
+                    break;
+                }
+            }
+
+            if (nextPos == -1) {
+                int distSum = 0;
+                for (int i = currentPos; i < N - 1; i++) {
+                    distSum += dist[i];
+                }
+                minCost += distSum * currentCost;
+                break;
+            }
+
+            int distSum = 0;
+            for (int i = currentPos; i < nextPos; i++) {
+                distSum += dist[i];
+            }
+            minCost += distSum * currentCost;
+            currentPos = nextPos;
+        }
+        System.out.print(minCost);
+    }
+}
