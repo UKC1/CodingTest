ca2f98de938208a92d6f19f27a5f6682d813c5d8
Kang Min Kim <lnewgatel@gmail.com>
2024-11-08 15:50:44 +0900

    [Silver I] Title: 쉬운 최단거리, Time: 544 ms, Memory: 57228 KB -BaekjoonHub

--- 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
+++ 백준/Silver/14940. 쉬운 최단거리/쉬운 최단거리.java
@@ -6,57 +6,57 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, -1, 0, 1};
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		StringBuilder sb = new StringBuilder();
-		int N = Integer.parseInt(st.nextToken());
-		int M = Integer.parseInt(st.nextToken());
-		int[][] maps = new int[N][M];
-		int[][] dists = new int[N][M];
-		boolean[][] visited = new boolean[N][M];
-		Queue<int[]> queue = new LinkedList<>();
-		for (int i = 0; i < N; i++) {
-			st = new StringTokenizer(br.readLine());
-			for (int j = 0; j < M; j++) {
-				maps[i][j] = Integer.parseInt(st.nextToken());
-				if (maps[i][j] == 2) {
-					queue.offer(new int[] {i, j, 1});
-					visited[i][j] = true;
-				}
-			}
-		}
-				
-		
-		while(!queue.isEmpty()) {
-			int[] current = queue.poll();
-			int x = current[0];
-			int y = current[1];
-			int cnt = current[2];
-			
-			for (int i = 0; i < 4; i++) {
-				int nx = x + dx[i];
-				int ny = y + dy[i];
-				if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && maps[nx][ny] == 1) {
-					dists[nx][ny] = cnt;
-					visited[nx][ny] = true;
-					queue.offer(new int[] {nx, ny, cnt + 1});
-				}
-			}
-		}
-		
-		for (int i = 0; i < N; i++) {
-			for (int j = 0; j < M; j++) {
-				if (!visited[i][j] && maps[i][j] == 1) {
-					sb.append(-1).append(' ');
-				} else {
-					sb.append(dists[i][j]).append(' ');
-				}
-			}
-			sb.append('\n');
-		}
-		System.out.print(sb);
-	}
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        int[][] maps = new int[N][M];
+        int[][] dists = new int[N][M];
+        boolean[][] visited = new boolean[N][M];
+        Queue<int[]> queue = new LinkedList<>();
+
+        for (int i = 0; i < N; i++) {
+            st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < M; j++) {
+                maps[i][j] = Integer.parseInt(st.nextToken());
+                if (maps[i][j] == 2) {
+                    queue.offer(new int[]{i, j, 0});
+                    visited[i][j] = true;
+                } else if (maps[i][j] == 1) {
+                    dists[i][j] = -1; // 이동 가능한 위치를 -1로 초기화
+                }
+            }
+        }
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+            int x = current[0];
+            int y = current[1];
+            int cnt = current[2];
+
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i];
+                int ny = y + dy[i];
+                if (nx >= 0 && nx < N && ny >= 0 && ny < M && !visited[nx][ny] && maps[nx][ny] == 1) {
+                    dists[nx][ny] = cnt + 1;
+                    visited[nx][ny] = true;
+                    queue.offer(new int[]{nx, ny, cnt + 1});
+                }
+            }
+        }
+
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                sb.append(dists[i][j]).append(' ');
+            }
+            sb.append('\n');
+        }
+        System.out.print(sb);
+    }
 }
