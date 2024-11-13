680c9a8bcbc9ee0b2e22ab0e36f1cde2235ba8e2
Kang Min Kim <lnewgatel@gmail.com>
2024-11-13 13:45:34 +0900

    [Silver IV] Title: 상근이의 여행, Time: 192 ms, Memory: 24164 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.java
@@ -1,55 +1,28 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayDeque;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st;
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		for (int test_case = 0; test_case < T; test_case++) {
-			st = new StringTokenizer(br.readLine());
-			int N = Integer.parseInt(st.nextToken());
-			int M = Integer.parseInt(st.nextToken());
-			List<List<Integer>> graph = new ArrayList<>();
-			for (int i = 0; i <= N; i++) {
-				graph.add(new ArrayList<>());
-			}
-			
-			for (int i = 0; i < M; i++) {
-				st = new StringTokenizer(br.readLine());
-				int u = Integer.parseInt(st.nextToken());
-				int v = Integer.parseInt(st.nextToken());
-				graph.get(u).add(v);
-				graph.get(v).add(u);
-			}
-			
-			boolean[] visited = new boolean[N + 1];
-			int cnt = 0;
-			Queue<Integer> queue = new ArrayDeque<>();
-			queue.offer(1);
-			visited[0] = true;
-			visited[1] = true;
-			while(!queue.isEmpty()) {
-				int curr = queue.poll();
-				for (Integer next : graph.get(curr)) {
-					if (!visited[next]) {
-						visited[next] = true;
-						queue.offer(next);
-						cnt++;
-					}
-				}
-			}
-			
-			sb.append(cnt).append('\n');
-		}
-		System.out.print(sb);
-	}
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
+        for (int test_case = 0; test_case < T; test_case++) {
+            st = new StringTokenizer(br.readLine());
+            int N = Integer.parseInt(st.nextToken());
+            int M = Integer.parseInt(st.nextToken());
+
+            // N개의 국가를 모두 연결하는 데 필요한 최소 비행 횟수는 N - 1
+            sb.append(N - 1).append('\n');
+
+            // M개의 간선 정보를 읽기만 하고 처리하지 않음
+            for (int i = 0; i < M; i++) {
+                br.readLine(); // 간선 정보는 무시
+            }
+        }
+        System.out.print(sb);
+    }
 }
