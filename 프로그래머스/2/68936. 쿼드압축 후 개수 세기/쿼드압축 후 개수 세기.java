7be520c0e1263e29097e33522af067b7ac41ad4c
Kang Min Kim <lnewgatel@gmail.com>
2024-12-01 18:38:56 +0900

    [level 2] Title: 쿼드압축 후 개수 세기, Time: 11.36 ms, Memory: 138 MB -BaekjoonHub

--- 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
+++ 프로그래머스/2/68936. 쿼드압축 후 개수 세기/쿼드압축 후 개수 세기.java
@@ -1,36 +1,33 @@
 class Solution {
     public int[] solution(int[][] arr) {
-        int[] answer = new int[2]; // 0의 개수와 1의 개수
+        int[] answer = new int[2];
         quadTree(arr, 0, 0, arr.length, answer);
         return answer;
     }
-
-    // 쿼드 트리 탐색 함수
-    private void quadTree(int[][] arr, int x, int y, int size, int[] answer) {
-        // 현재 영역이 모두 같은 숫자인지 확인
+    
+    public void quadTree(int[][] arr, int x, int y, int size, int[] answer) {
         if (isUniform(arr, x, y, size)) {
-            if (arr[x][y] == 0) {
-                answer[0]++; // 0의 개수 증가
+            int num = arr[x][y];
+            if (num == 0) {
+                answer[0]++;
             } else {
-                answer[1]++; // 1의 개수 증가
+                answer[1]++;
             }
             return;
         }
-
-        // 영역을 4분할하여 재귀적으로 탐색
-        int newSize = size / 2;
-        quadTree(arr, x, y, newSize, answer); // 1사분면
-        quadTree(arr, x, y + newSize, newSize, answer); // 2사분면
-        quadTree(arr, x + newSize, y, newSize, answer); // 3사분면
-        quadTree(arr, x + newSize, y + newSize, newSize, answer); // 4사분면
+        
+        size /= 2;
+        quadTree(arr, x, y, size, answer); 
+        quadTree(arr, x, y + size, size, answer); 
+        quadTree(arr, x + size, y + size, size, answer); 
+        quadTree(arr, x + size, y, size, answer); 
     }
-
-    // 현재 영역이 모두 같은 숫자인지 확인하는 함수
-    private boolean isUniform(int[][] arr, int x, int y, int size) {
-        int firstNum = arr[x][y];
+    
+    public boolean isUniform(int[][] arr, int x, int y, int size) {
+        int first = arr[x][y];
         for (int i = x; i < x + size; i++) {
             for (int j = y; j < y + size; j++) {
-                if (arr[i][j] != firstNum) {
+                if (arr[i][j] != first) {
                     return false;
                 }
             }
