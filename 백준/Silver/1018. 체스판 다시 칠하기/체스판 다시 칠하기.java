3445929af381755c8d705b8f03ad459e3bca9503
Kang Min Kim <lnewgatel@gmail.com>
2024-12-05 15:36:14 +0900

    [Silver IV] Title: 체스판 다시 칠하기, Time: 112 ms, Memory: 14396 KB -BaekjoonHub

--- 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
+++ 백준/Silver/1018. 체스판 다시 칠하기/체스판 다시 칠하기.java
@@ -2,40 +2,62 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
+import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int M;
+    static char[][] boards;
+    static int minCnt = 64;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        String str = br.readLine();
-        int N = Integer.parseInt(str.split(" ")[0]);
-        int M = Integer.parseInt(str.split(" ")[1]);
-        int firstNum = 1;
-
-        int minCnt = Integer.MAX_VALUE;
-        char[][] arr = new char[N][M];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        char[][] board = new char[N][M];
         for (int i = 0; i < N; i++) {
-            str = br.readLine();
-            for (int j = 0; j < M; j++) {
-                arr[i][j] = str.charAt(j);
+            board[i] = br.readLine().toCharArray();
+        }
+
+        char[] blackWhite = new char[8];
+        char[] whiteBlack = new char[8];
+        for (int i = 0; i < 8; i++) {
+            if (i % 2 == 0) {
+                blackWhite[i] = 'B';
+                whiteBlack[i] = 'W';
+            } else {
+                blackWhite[i] = 'W';
+                whiteBlack[i] = 'B';
             }
         }
 
-        for (int i = 0; i <= N - 8; i++) {
-            for (int j = 0; j <= M - 8; j++) {
+
+        for (int row = 0; row <= N - 8; row++) {
+            for (int col = 0; col <= M - 8; col++) {
                 int bCnt = 0;
-                firstNum = 1;
-                for (int k = i; k < i + 8; k++) {
-                    for (int l = j; l < j + 8; l++) {
-                        if (arr[k][l] == 'B') {
-                            if (firstNum == -1) bCnt++;
-                        } else if (arr[k][l] == 'W'){
-                            if (firstNum == 1) bCnt++;
+                int wCnt = 0;
+                for (int subRow = row; subRow < row + 8; subRow++) {
+                    for (int subCol = col; subCol < col + 8 ; subCol++) {
+                        if (subRow % 2 == 0) {
+                            if (board[subRow][subCol] != blackWhite[subCol - col]) {
+                                bCnt++;
+                            }
+
+                            if (board[subRow][subCol] != whiteBlack[subCol - col]) {
+                                wCnt++;
+                            }
+                        } else {
+                            if (board[subRow][subCol] != whiteBlack[subCol - col]) {
+                                bCnt++;
+                            }
+
+                            if (board[subRow][subCol] != blackWhite[subCol - col]) {
+                                wCnt++;
+                            }
                         }
-                        firstNum *= -1;
                     }
-                    firstNum *= -1;
                 }
-                minCnt = Math.min(minCnt, Math.min(bCnt, Math.abs(64 - bCnt)));
+                minCnt = Math.min(minCnt, Math.min(bCnt, wCnt));
             }
         }
         System.out.println(minCnt);
