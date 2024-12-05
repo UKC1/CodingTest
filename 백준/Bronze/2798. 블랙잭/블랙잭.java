277f7558a577ab63cea62f600ad96203e063ba93
Kang Min Kim <lnewgatel@gmail.com>
2024-12-05 13:34:58 +0900

    [Bronze II] Title: 블랙잭, Time: 108 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.java
+++ 백준/Bronze/2798. 블랙잭/블랙잭.java
@@ -4,29 +4,35 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
+    static int N;
+    static int M;
+    static int[] cards;
+    static int maxSum;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int N = Integer.parseInt(st.nextToken());
-        int M = Integer.parseInt(st.nextToken());
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        cards = new int[N];
+        maxSum = 0;
         st = new StringTokenizer(br.readLine());
-
-        int[] arr = new int[N];
         for (int i = 0; i < N; i++) {
-            arr[i] = Integer.parseInt(st.nextToken());
+            cards[i] = Integer.parseInt(st.nextToken());
         }
+        combination(0, 0, 0);
+        System.out.print(maxSum);
+    }
 
-        int maxSum = 0;
-        for (int i = 0; i < N; i++) {
-            for (int j = i + 1; j < N; j++) {
-                for (int k = j + 1; k < N; k++) {
-                    int threeSum = arr[i] + arr[j] + arr[k];
-                    if(maxSum < threeSum && threeSum <= M) {
-                        maxSum = threeSum;
-                    }
-                }
+    static void combination(int idx, int start, int sum) {
+        if (idx == 3) {
+            if (sum <= M && maxSum < sum) {
+                maxSum = sum;
             }
+            return;
         }
-        System.out.println(maxSum);
+
+        for (int i = start; i < N; i++) {
+            combination(idx + 1, i + 1, sum + cards[i]);
+        }
     }
 }
