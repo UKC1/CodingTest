6e164164882f057d7d1374f7a464b8bb008fd3b7
Kang Min Kim <lnewgatel@gmail.com>
2024-12-08 21:04:23 +0900

    [Silver III] Title: 퇴사, Time: 100 ms, Memory: 14280 KB -BaekjoonHub

--- 백준/Silver/14501. 퇴사/퇴사.java
+++ 백준/Silver/14501. 퇴사/퇴사.java
@@ -4,34 +4,34 @@ import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int maxProfit;
     static int N;
+    static int[][] rooms;
+    static int maxProfit;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
-        N = Integer.parseInt(br.readLine());
-        int[][] works = new int[N][2];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        N = Integer.parseInt(st.nextToken());
+        rooms = new int[N][2];
         maxProfit = 0;
         for (int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            works[i][0] = Integer.parseInt(st.nextToken());
-            works[i][1] = Integer.parseInt(st.nextToken());
+            rooms[i][0] = Integer.parseInt(st.nextToken());
+            rooms[i][1] = Integer.parseInt(st.nextToken());
         }
 
-        dfs(works, 0, 0);
+        dfs(0, 0);
         System.out.print(maxProfit);
     }
 
-    static void dfs(int[][] works, int time, int sum) {
-        if (time >= N) {
-            if (sum > maxProfit) maxProfit = sum;
+    static void dfs(int day, int sum) {
+        if (day >= N) {
+            maxProfit = Math.max(maxProfit, sum);
             return;
         }
 
-        dfs(works, time + 1, sum);
-
-        if (time + works[time][0] <= N) {
-            dfs(works, time + works[time][0], sum + works[time][1]);
+        if (day + rooms[day][0] <= N) {
+            dfs(day + rooms[day][0], sum + rooms[day][1]);
         }
+        dfs(day + 1, sum);
     }
 }
