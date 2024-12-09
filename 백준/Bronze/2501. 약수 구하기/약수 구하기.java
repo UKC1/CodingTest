ae0c1d7dff86047dd89abb24e0a6a953b7bcb93a
Kang Min Kim <lnewgatel@gmail.com>
2024-12-10 00:48:49 +0900

    [Bronze III] Title: 약수 구하기, Time: 100 ms, Memory: 14364 KB -BaekjoonHub

--- 백준/Bronze/2501. 약수 구하기/약수 구하기.java
+++ 백준/Bronze/2501. 약수 구하기/약수 구하기.java
@@ -1,25 +1,27 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		String str = br.readLine();
-		int N = Integer.parseInt(str.split(" ")[0]);
-		int K = Integer.parseInt(str.split(" ")[1]);
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int K = Integer.parseInt(st.nextToken());
 
-		for (int i = 1; i <= N; i++) {
-			if (N % i == 0) {
-				if (--K == 0) {
-					System.out.println(i);
-					break;
-				}
-			}
-		}
+        for (int i = 1; i <= N; i++) {
+            if (N % i == 0) {
+                K--;
+                if (K == 0) {
+                    System.out.println(i);
+                    break;
+                }
+            }
+        }
 
-		if (K != 0) {
-			System.out.println(0);
-		}
-	}
+        if (K != 0) {
+            System.out.println(0);
+        }
+    }
 }
