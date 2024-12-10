cf5d029a0ec05a28dd76e24f53893658a8249d3c
Kang Min Kim <lnewgatel@gmail.com>
2024-12-10 13:44:54 +0900

    [Silver III] Title: N번째 큰 수, Time: 796 ms, Memory: 276132 KB -BaekjoonHub

--- 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
+++ 백준/Silver/2075. N번째 큰 수/N번째 큰 수.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.Comparator;
+import java.util.PriorityQueue;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -10,15 +11,18 @@ public class Main {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
-        int[] numbers = new int[N * N];
-        int idx = 0;
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Comparator.reverseOrder());
         for (int i = 0 ; i < N ; i++) {
             st = new StringTokenizer(br.readLine());
             for (int j = 0 ; j < N ; j++) {
-                numbers[idx++] = Integer.parseInt(st.nextToken());
+                pq.offer(Integer.parseInt(st.nextToken()));
             }
         }
-        Arrays.sort(numbers);
-        System.out.print(numbers[N * N - N]);
+        int cnt = 0;
+        while (cnt < N - 1) {
+            pq.poll();
+            cnt++;
+        }
+        System.out.print(pq.poll());
     }
 }
