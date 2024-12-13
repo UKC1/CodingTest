30ac14d80d0cb6e539de11d1723de94b01a70c0a
Kang Min Kim <lnewgatel@gmail.com>
2024-12-13 15:23:44 +0900

    [Gold I] Title: 2048 (Easy), Time: 340 ms, Memory: 27584 KB -BaekjoonHub

--- 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
+++ 백준/Gold/12100. 2048 （Easy）/2048 （Easy）.java
@@ -31,7 +31,6 @@ public class Main {
                 maxSum = Math.max(maxSum, maps[i][j]);
             }
         }
-
         return maxSum;
     }
     static void dfs(int cnt, int[][] maps) {
@@ -43,9 +42,130 @@ public class Main {
         // 1. 좌 밀어
         int[][] leftMap = mapCopy(maps);
         // 좌로 미는 로직
+        pushLeft(leftMap);
+        // 다음 단계
+        dfs(cnt + 1, leftMap);
+
+        // 2. 우 밀어
+        int[][] rightMap = mapCopy(maps);
+        // 우로 미는 로직
+        pushRight(rightMap);
+        // 다음 단계
+        dfs(cnt + 1, rightMap);
+
+        // 3. 상 밀어
+        int[][] upMap = mapCopy(maps);
+        // 상로 미는 로직
+        pushUp(upMap);
+        // 다음 단계
+        dfs(cnt + 1, upMap);
+
+        // 4. 하 밀어
+        int[][] downMap = mapCopy(maps);
+        // 하로 미는 로직
+        pushDown(downMap);
+        // 다음 단계
+        dfs(cnt + 1, downMap);
+    }
+
+    private static void pushDown(int[][] downMap) {
+        Stack<Integer> stack = new Stack<>();
+        for (int col = 0; col < N; col++) {
+            for (int row = N - 1; row >= 0; row--) {
+                int num = downMap[row][col];
+                if (num != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(num);
+                    } else {
+                        if (stack.peek() == num) {
+                            int mergedNum = stack.pop() * 2;
+                            stack.push(mergedNum);
+                            stack.push(0);
+                        } else {
+                            stack.push(num);
+                        }
+                    }
+                }
+                downMap[row][col] = 0;
+            }
+
+            int idx = N - 1;
+            for (Integer stackNum : stack) {
+                if (stackNum != 0) {
+                    downMap[idx--][col] = stackNum;
+                }
+            }
+            stack.clear();
+        }
+    }
+
+    private static void pushUp(int[][] upMap) {
+        Stack<Integer> stack = new Stack<>();
+        for (int col = 0; col < N; col++) {
+            for (int row = 0; row < N; row++) {
+                int num = upMap[row][col];
+                if (num != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(num);
+                    } else {
+                        if (stack.peek() == num) {
+                            int mergedNum = stack.pop() * 2;
+                            stack.push(mergedNum);
+                            stack.push(0);
+                        } else {
+                            stack.push(num);
+                        }
+                    }
+                }
+                upMap[row][col] = 0;
+            }
+
+            int idx = 0;
+            for (Integer stackNum : stack) {
+                if (stackNum != 0) {
+                    upMap[idx++][col] = stackNum;
+                }
+            }
+            stack.clear();
+        }
+    }
+
+    private static void pushRight(int[][] rightMap) {
+        Stack<Integer> stack = new Stack<>();
+        for (int row = 0; row < N; row++) {
+            for (int col = N - 1; col >= 0; col--) {
+                int num = rightMap[row][col];
+                if (num != 0) {
+                    if (stack.isEmpty()) {
+                        stack.push(num);
+                    } else {
+                        if (stack.peek() == num) {
+                            int mergedNum = stack.pop() * 2;
+                            stack.push(mergedNum);
+                            stack.push(0);
+                        } else {
+                            stack.push(num);
+                        }
+                    }
+                }
+                rightMap[row][col] = 0;
+            }
+
+            int idx = N - 1;
+            for (Integer stackNum : stack) {
+                if (stackNum != 0) {
+                    rightMap[row][idx--] = stackNum;
+                }
+            }
+            stack.clear();
+        }
+    }
+
+    private static void pushLeft(int[][] leftMap) {
+        // 좌로 미는 로직
+        Stack<Integer> stack = new Stack<>();
         for (int row = 0; row < N; row++) {
             // 같은 숫자 찾기
-            Stack<Integer> stack = new Stack<>();
             for (int col = 0; col < N; col++) {
                 int num = leftMap[row][col];
                 if (num != 0) {
@@ -73,103 +193,8 @@ public class Main {
                     leftMap[row][colIdx++] = stackNum;
                 }
             }
+            stack.clear();
         }
-        dfs(cnt + 1, leftMap);
-        // 2. 우 밀어
-        int[][] rightMap = mapCopy(maps);
-        // 우로 미는 로직
-        for (int row = 0; row < N; row++) {
-            Stack<Integer> stack = new Stack<>();
-            for (int col = N - 1; col >= 0; col--) {
-                int num = rightMap[row][col];
-                if (num != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(num);
-                    } else {
-                        if (stack.peek() == num) {
-                            int mergedNum = stack.pop() * 2;
-                            stack.push(mergedNum);
-                            stack.push(0);
-                        } else {
-                            stack.push(num);
-                        }
-                    }
-                }
-                rightMap[row][col] = 0;
-            }
-
-            int idx = N - 1;
-            for (Integer stackNum : stack) {
-                if (stackNum != 0) {
-                    rightMap[row][idx--] = stackNum;
-                }
-            }
-
-
-        }
-        dfs(cnt + 1, rightMap);
-        // 3. 상 밀어
-        int[][] upMap = mapCopy(maps);
-        // 상로 미는 로직
-        for (int col = 0; col < N; col++) {
-            Stack<Integer> stack = new Stack<>();
-            for (int row = 0; row < N; row++) {
-                int num = upMap[row][col];
-                if (num != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(num);
-                    } else {
-                        if (stack.peek() == num) {
-                            int mergedNum = stack.pop() * 2;
-                            stack.push(mergedNum);
-                            stack.push(0);
-                        } else {
-                            stack.push(num);
-                        }
-                    }
-                }
-                upMap[row][col] = 0;
-            }
-
-            int idx = 0;
-            for (Integer stackNum : stack) {
-                if (stackNum != 0) {
-                    upMap[idx++][col] = stackNum;
-                }
-            }
-        }
-        dfs(cnt + 1, upMap);
-        // 4. 하 밀어
-        int[][] downMap = mapCopy(maps);
-        // 하로 미는 로직
-        for (int col = 0; col < N; col++) {
-            Stack<Integer> stack = new Stack<>();
-            for (int row = N - 1; row >= 0; row--) {
-                int num = downMap[row][col];
-                if (num != 0) {
-                    if (stack.isEmpty()) {
-                        stack.push(num);
-                    } else {
-                        if (stack.peek() == num) {
-                            int mergedNum = stack.pop() * 2;
-                            stack.push(mergedNum);
-                            stack.push(0);
-                        } else {
-                            stack.push(num);
-                        }
-                    }
-                }
-                downMap[row][col] = 0;
-            }
-
-            int idx = N - 1;
-            for (Integer stackNum : stack) {
-                if (stackNum != 0) {
-                    downMap[idx--][col] = stackNum;
-                }
-            }
-        }
-        dfs(cnt + 1, downMap);
     }
 
     static int[][] mapCopy(int[][] maps) {
