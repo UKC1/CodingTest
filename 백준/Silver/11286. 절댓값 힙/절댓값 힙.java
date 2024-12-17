c60f2047a543511f18b9ca6d44a5781eb7d472b9
Kang Min Kim <lnewgatel@gmail.com>
2024-12-17 20:28:50 +0900

    [Silver I] Title: 절댓값 힙, Time: 292 ms, Memory: 25100 KB -BaekjoonHub

--- 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
+++ 백준/Silver/11286. 절댓값 힙/절댓값 힙.java
@@ -7,31 +7,27 @@ import java.util.StringTokenizer;
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
         StringBuilder sb = new StringBuilder();
-        
-        int N = Integer.parseInt(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
         PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> {
             int absA = Math.abs(a);
             int absB = Math.abs(b);
             if (absA == absB) {
-                return Integer.compare(a, b); // 절댓값이 같으면 음수 우선
+                return Integer.compare(a, b);
+            } else {
+                return Integer.compare(absA, absB);
             }
-            return Integer.compare(absA, absB); // 절댓값 기준 오름차순
+
         });
-        
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
             if (num == 0) {
-                if (pq.isEmpty()) {
-                    sb.append(0).append("\n");
-                } else {
-                    sb.append(pq.poll()).append("\n");
-                }
+                sb.append(pq.isEmpty() ? 0 : pq.poll()).append("\n");
             } else {
                 pq.offer(num);
             }
         }
-        
         System.out.print(sb);
     }
 }
