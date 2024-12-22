7392f85d73891b1df18baba94972a66badccf9e6
Kang Min Kim <lnewgatel@gmail.com>
2024-12-22 14:25:20 +0900

    [Silver II] Title: 로또, Time: 104 ms, Memory: 14108 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.java
+++ 백준/Silver/6603. 로또/로또.java
@@ -3,9 +3,11 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
+    static StringBuilder sb;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str;
+        sb = new StringBuilder();
         while (!(str = br.readLine()).equals("0")) {
             String[] numbers = str.split(" ");
             int[] cards = new int[numbers.length - 1];
@@ -13,16 +15,17 @@ public class Main {
                 cards[i] = Integer.parseInt(numbers[i + 1]);
             }
             combination(cards, new int[6], 0, 0);
-            System.out.println();
+            sb.append('\n');
         }
+        System.out.print(sb);
     }
 
     static void combination(int[] cards, int[] arr, int index, int start) {
         if (index == arr.length) {
             for (int i = 0; i < arr.length; i++) {
-                System.out.print(arr[i] + " ");
+                sb.append(arr[i]).append(' ');
             }
-            System.out.println();
+            sb.append('\n');
             return;
         }
 
