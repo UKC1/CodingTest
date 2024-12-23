bb9aa59b035ff97e566d612670a67ed94979fccb
Kang Min Kim <lnewgatel@gmail.com>
2024-12-23 23:13:20 +0900

    [Silver I] Title: 부등호, Time: 212 ms, Memory: 23172 KB -BaekjoonHub

--- 백준/Silver/2529. 부등호/부등호.java
+++ 백준/Silver/2529. 부등호/부등호.java
@@ -6,72 +6,51 @@ import java.util.StringTokenizer;
 public class Main {
     static int K;
     static String[] operators;
-    static char[] arr;
     static boolean[] visited;
-    static long minNum;
-    static long maxNum;
-    static String minNumStr;
-    static String maxNumStr;
-    static StringBuilder sb;
+    static String minNum = "";
+    static String maxNum = "";
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        K = Integer.parseInt(st.nextToken());
+        K = Integer.parseInt(br.readLine());
         operators = br.readLine().split(" ");
-        arr = new char[K + 1];
-        visited = new boolean[10];
-        minNum = Long.MAX_VALUE;
-        maxNum = Long.MIN_VALUE;
-        minNumStr = "";
-        maxNumStr = "";
-        dfs(0);
-        System.out.println(maxNumStr);
-        System.out.println(minNumStr);
+
+        visited = new boolean[10]; // 숫자 방문 여부
+
+        dfs("", 0); // 순열 생성 시작
+
+        System.out.println(maxNum);
+        System.out.println(minNum);
     }
-    static boolean isValid() {
-        for (int i = 0; i < operators.length; i++) {
-            if (operators[i].equals(">")) {
-                if (arr[i + 1] >= arr[i]) {
-                    return false;
-                }
-            } else if (operators[i].equals("<")) {
-                if (arr[i + 1] <= arr[i]) {
-                    return false;
-                }
+
+    static void dfs(String num, int depth) {
+        if (depth == K + 1) { // K+1 자리 숫자를 완성
+            if (minNum.isEmpty() || num.compareTo(minNum) < 0) {
+                minNum = num;
             }
-        }
-        return true;
-    }
-
-    static void dfs(int idx) {
-        if (idx == K + 1) {
-            if (isValid()) {
-                sb = new StringBuilder();
-                for (int i = 0; i < arr.length; i++) {
-                    sb.append(arr[i]);
-                }
-                long num = Long.parseLong(sb.toString());
-                if (num < minNum) {
-                    minNum = num;
-                    minNumStr = sb.toString();
-                }
-
-                if (num > maxNum) {
-                    maxNum = num;
-                    maxNumStr = sb.toString();
-                }
+            if (maxNum.isEmpty() || num.compareTo(maxNum) > 0) {
+                maxNum = num;
             }
             return;
         }
 
-        for (int i = 0; i < 10; i++) {
+        for (int i = 0; i <= 9; i++) {
             if (!visited[i]) {
-                visited[i] = true;
-                arr[idx] = Character.forDigit(i, 10);
-                dfs(idx + 1);
-                visited[i] = false;
+                // 이전 숫자와 부등호 조건 확인
+                if (depth == 0 || isValid(num.charAt(depth - 1) - '0', i, operators[depth - 1])) {
+                    visited[i] = true;
+                    dfs(num + i, depth + 1);
+                    visited[i] = false;
+                }
             }
         }
+    }
 
+    static boolean isValid(int prev, int curr, String operator) {
+        if (operator.equals(">")) {
+            return prev > curr;
+        } else {
+            return prev < curr;
+        }
     }
 }
