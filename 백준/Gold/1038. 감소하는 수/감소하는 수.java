e967345ba2bb6994f87d75031168e1e9ec2b22b5
Kang Min Kim <lnewgatel@gmail.com>
2024-12-29 17:12:54 +0900

    [Gold V] Title: 감소하는 수, Time: 116 ms, Memory: 14376 KB -BaekjoonHub

--- 백준/Gold/1038. 감소하는 수/감소하는 수.java
+++ 백준/Gold/1038. 감소하는 수/감소하는 수.java
@@ -1,40 +1,48 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Main {
-    static int N;
-    static int cnt;
-    static boolean flag;
+    static int N; // 입력으로 주어지는 N번째 감소하는 수
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         N = Integer.parseInt(br.readLine());
-        // N개 짜리 배열
-        cnt = 0;
-        flag = false;
-        // a1 > a2 > ... > aN
+
         if (N >= 1023) {
-            System.out.println(-1);
-        } else {
-            for (int i = 1; i <= 10; i++) {
-                dfs("", 9, 0, i);
-                if (flag) break;
-            }
-        }
-    }
-    static void dfs(String numStr, int end, int idx, int depth) {
-        if (idx == depth) {
-            if (cnt == N) {
-                System.out.println(numStr);
-                flag = true;
-            }
-            cnt++;
+            System.out.println(-1); // 감소하는 수가 1022개를 초과하면 존재하지 않음
             return;
         }
-        if (flag) return;
 
-        for (int i = 0; i <= end; i++) {
-            dfs(numStr + i,i - 1, idx + 1, depth);
+        Queue<Long> queue = new LinkedList<>();
+
+        // 0부터 9까지 초기 감소하는 수를 큐에 삽입
+        for (long i = 0; i <= 9; i++) {
+            queue.offer(i);
         }
+
+        int count = -1; // 감소하는 수의 순서
+        long result = -1; // N번째 감소하는 수
+
+        while (!queue.isEmpty()) {
+            long current = queue.poll();
+            count++;
+
+            // N번째 감소하는 수를 찾으면 종료
+            if (count == N) {
+                result = current;
+                break;
+            }
+
+            // 마지막 자릿수보다 작은 숫자를 추가하여 새로운 감소하는 수 생성
+            long lastDigit = current % 10;
+            for (long nextDigit = 0; nextDigit < lastDigit; nextDigit++) {
+                queue.offer(current * 10 + nextDigit);
+            }
+        }
+
+        System.out.println(result);
     }
 }
