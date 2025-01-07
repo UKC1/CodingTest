3e2d32cad033948bea45c25f237b271889932410
Kang Min Kim <lnewgatel@gmail.com>
2025-01-07 14:00:43 +0900

    [level 2] Title: 마법의 엘리베이터, Time: 0.47 ms, Memory: 76.2 MB -BaekjoonHub

--- 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
+++ 프로그래머스/2/148653. 마법의 엘리베이터/마법의 엘리베이터.java
@@ -1,26 +1,40 @@
 import java.util.*;
+
 class Solution {
     int minCnt = Integer.MAX_VALUE;
+    Map<Integer, Integer> memo = new HashMap<>();
+
     public int solution(int storey) {
         dfs(storey, 0);
         return minCnt;
     }
     
     void dfs(int currentNum, int cnt) {
-        if (currentNum < 10) {
-            int lastCnt = Math.min(currentNum, 10 - currentNum + 1);
-            minCnt = Math.min(minCnt, cnt + lastCnt);
+        // 가지치기: 현재 경로가 이미 최소값보다 크면 중단
+        if (cnt >= minCnt) {
             return;
         }
-        // System.out.println(currentNum + " : " + cnt);
-        
+
+        // 종료 조건: 한 자리 숫자일 때
+        if (currentNum < 10) {
+            minCnt = Math.min(minCnt, cnt + Math.min(currentNum, 10 - currentNum + 1));
+            return;
+        }
+
+        // 메모이제이션: 이미 계산된 최소값이 있다면 탐색 중단
+        if (memo.containsKey(currentNum) && memo.get(currentNum) <= cnt) {
+            return;
+        }
+        memo.put(currentNum, cnt);
+
+        // 현재 숫자의 1의 자리 처리
         int r = currentNum % 10;
         int nextNum = currentNum / 10;
-        if (r == 0) {
-            dfs(nextNum, cnt);
-        } else {
-            dfs(nextNum, cnt + r);
-            dfs(nextNum + 1, cnt + 10 - r);
-        }
+
+        // 1. 현재 자리에서 그대로 빼는 경우
+        dfs(nextNum, cnt + r);
+
+        // 2. 현재 자리에서 올림하여 다음 자리 처리
+        dfs(nextNum + 1, cnt + (10 - r));
     }
 }
