b12982ccd37d602014ef59eaf3025168cb5c1dfe
Kang Min Kim <lnewgatel@gmail.com>
2025-01-14 22:59:31 +0900

    [Gold IV] Title: 지각하기 싫어, Time: 564 ms, Memory: 77104 KB -BaekjoonHub

--- 백준/Gold/29811. 지각하기 싫어/지각하기 싫어.java
+++ 백준/Gold/29811. 지각하기 싫어/지각하기 싫어.java
@@ -1,27 +1,9 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.StringTokenizer;
 
 public class Main {
-    static class Route {
-        int x;
-        int cnt;
-
-        Route(int x, int cnt) {
-            this.x = x;
-            this.cnt = cnt;
-        }
-
-        public int getCnt() {
-            return cnt;
-        }
-
-        public int getX() {
-            return x;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
@@ -30,25 +12,33 @@ public class Main {
         int N = Integer.parseInt(st.nextToken());
         int M = Integer.parseInt(st.nextToken());
 
-        PriorityQueue<Route> pq1 = new PriorityQueue<>(Comparator.comparingInt(Route::getCnt).thenComparingInt(Route::getX));
-        PriorityQueue<Route> pq2 = new PriorityQueue<>(Comparator.comparingInt(Route::getCnt).thenComparingInt(Route::getX));
-        Map<Integer, Integer> map1 = new HashMap<>();
-        Map<Integer, Integer> map2 = new HashMap<>();
+        int[] group1 = new int[N + 1];
+        int[] group2 = new int[M + 1];
 
+        int minIndex1 = 1, minValue1 = Integer.MAX_VALUE;
+        int minIndex2 = 1, minValue2 = Integer.MAX_VALUE;
+
+        // Group 1 초기화
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= N; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            pq1.offer(new Route(i, value));
-            map1.put(i, value);
+            group1[i] = Integer.parseInt(st.nextToken());
+            if (group1[i] < minValue1 || (group1[i] == minValue1 && i < minIndex1)) {
+                minValue1 = group1[i];
+                minIndex1 = i;
+            }
         }
 
+        // Group 2 초기화
         st = new StringTokenizer(br.readLine());
         for (int i = 1; i <= M; i++) {
-            int value = Integer.parseInt(st.nextToken());
-            pq2.offer(new Route(i + N, value));
-            map2.put(i + N, value);
+            group2[i] = Integer.parseInt(st.nextToken());
+            if (group2[i] < minValue2 || (group2[i] == minValue2 && i + N < minIndex2)) {
+                minValue2 = group2[i];
+                minIndex2 = i + N;
+            }
         }
 
+        // 명령 처리
         int K = Integer.parseInt(br.readLine());
         for (int i = 0; i < K; i++) {
             st = new StringTokenizer(br.readLine());
@@ -58,25 +48,35 @@ public class Main {
                 int x = Integer.parseInt(st.nextToken());
                 int y = Integer.parseInt(st.nextToken());
 
-                if (x > N) {
-                    // Update group 2
-                    map2.put(x, y);
-                    pq2.offer(new Route(x, y));
-                } else {
-                    // Update group 1
-                    map1.put(x, y);
-                    pq1.offer(new Route(x, y));
-                }
-            } else {
-                // Find smallest valid values
-                while (!pq1.isEmpty() && map1.get(pq1.peek().x) != pq1.peek().cnt) {
-                    pq1.poll(); // Remove outdated value
-                }
-                while (!pq2.isEmpty() && map2.get(pq2.peek().x) != pq2.peek().cnt) {
-                    pq2.poll(); // Remove outdated value
-                }
+                if (x > N) { // Group 2 Update
+                    group2[x - N] = y;
 
-                sb.append(pq1.peek().x).append(' ').append(pq2.peek().x).append('\n');
+                    if (x == minIndex2 || y < minValue2 || (y == minValue2 && x < minIndex2)) {
+                        // 최소값 갱신 필요
+                        minValue2 = Integer.MAX_VALUE;
+                        for (int j = 1; j <= M; j++) {
+                            if (group2[j] < minValue2 || (group2[j] == minValue2 && j + N < minIndex2)) {
+                                minValue2 = group2[j];
+                                minIndex2 = j + N;
+                            }
+                        }
+                    }
+                } else { // Group 1 Update
+                    group1[x] = y;
+
+                    if (x == minIndex1 || y < minValue1 || (y == minValue1 && x < minIndex1)) {
+                        // 최소값 갱신 필요
+                        minValue1 = Integer.MAX_VALUE;
+                        for (int j = 1; j <= N; j++) {
+                            if (group1[j] < minValue1 || (group1[j] == minValue1 && j < minIndex1)) {
+                                minValue1 = group1[j];
+                                minIndex1 = j;
+                            }
+                        }
+                    }
+                }
+            } else { // 명령어 L
+                sb.append(minIndex1).append(' ').append(minIndex2).append('\n');
             }
         }
 
