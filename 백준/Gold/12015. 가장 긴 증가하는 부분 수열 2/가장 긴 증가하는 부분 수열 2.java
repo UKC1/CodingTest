788099eae3fd2df6d465762ae6a566d4ee485b42
Kang Min Kim <lnewgatel@gmail.com>
2025-02-03 23:13:55 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 2, Time: 600 ms, Memory: 121140 KB -BaekjoonHub

--- 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
+++ 백준/Gold/12015. 가장 긴 증가하는 부분 수열 2/가장 긴 증가하는 부분 수열 2.java
@@ -18,18 +18,30 @@ public class Main {
         for (int i = 1; i < N; i++) {
             int num = arr[i];
             if (num > list.get(list.size() - 1)) {
-                // 가장 큰 값보다 크면 뒤에 추가
-                list.add(num);
+                list.add(num); // 가장 큰 값이면 추가
             } else {
-                // 이진 탐색을 통해 적절한 위치 찾기
-                int pos = Collections.binarySearch(list, num);
-                if (pos < 0) {
-                    pos = -(pos + 1); // 삽입 위치
-                }
-                list.set(pos, num); // 해당 위치 값 교체
+                int pos = lowerBound(list, num); // 이진 탐색 직접 구현
+                list.set(pos, num); // 적절한 위치의 값 교체
             }
         }
 
         System.out.println(list.size()); // LIS 길이 출력
+    }
+
+    // lowerBound: 이진 탐색으로 num이 들어갈 위치 찾기
+    static int lowerBound(ArrayList<Integer> list, int key) {
+        int left = 0, right = list.size() - 1;
+        
+        while (left < right) {
+            int mid = (left + right) / 2;
+            
+            if (list.get(mid) >= key) {
+                right = mid; // 왼쪽으로 탐색
+            } else {
+                left = mid + 1; // 오른쪽으로 탐색
+            }
+        }
+        
+        return right; // key가 들어갈 위치 반환
     }
 }
