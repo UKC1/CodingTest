db0b46a5b4659964ca54965cd53fd2ca39bfb19a
Kang Min Kim <lnewgatel@gmail.com>
2025-03-05 14:19:04 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 900 ms, Memory: 91144 KB -BaekjoonHub

--- 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -10,6 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
 
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
@@ -41,8 +42,9 @@ public class Main {
 
         // 방문 순서 출력
         for (int i = 1; i <= N; i++) {
-            System.out.println(order[i]);
+            sb.append(order[i]).append("\n");
         }
+        System.out.print(sb);
     }
 
     static void dfs(int vertex, boolean[] visited) {
