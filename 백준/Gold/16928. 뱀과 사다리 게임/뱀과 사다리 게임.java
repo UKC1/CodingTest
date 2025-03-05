a1e06f23cf89c58283234ad4cd7b3a3688aa38bb
Kang Min Kim <lnewgatel@gmail.com>
2025-03-06 00:03:11 +0900

    [Gold V] Title: 뱀과 사다리 게임, Time: 104 ms, Memory: 14304 KB -BaekjoonHub

--- 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
+++ 백준/Gold/16928. 뱀과 사다리 게임/뱀과 사다리 게임.java
@@ -1,17 +1,18 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.util.*;
 
 public class Main {
     static int N, M;
     static Map<Integer, Integer> jump;
+
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        N = Integer.parseInt(st.nextToken());
-        M = Integer.parseInt(st.nextToken());
+
+        N = Integer.parseInt(st.nextToken()); // 사다리 개수
+        M = Integer.parseInt(st.nextToken()); // 뱀 개수
         jump = new HashMap<>();
+
         for (int i = 0; i < N + M; i++) {
             st = new StringTokenizer(br.readLine());
             int start = Integer.parseInt(st.nextToken());
@@ -19,41 +20,42 @@ public class Main {
             jump.put(start, end);
         }
 
-        int cnt = bfs();
-        System.out.print(cnt);
+        System.out.println(bfs());
     }
 
     static int bfs() {
-        int start = 1;
-        int end = 100;
         Queue<int[]> queue = new LinkedList<>();
-        queue.offer(new int[]{start, 0});
+        queue.offer(new int[]{1, 0});
         boolean[] visited = new boolean[101];
-        visited[start] = true;
+        visited[1] = true;
+
         while (!queue.isEmpty()) {
             int[] cur = queue.poll();
             int curr = cur[0];
             int cnt = cur[1];
-            if (curr == end) {
+
+            if (curr == 100) {
                 return cnt;
             }
-            // 주사위를 굴려서 이동
-            // 이동한 좌표에 사다리나 뱀이 있으면 점프해야 됨
+
+            // 주사위 굴리기
             for (int dice = 1; dice <= 6; dice++) {
                 int next = curr + dice;
-                if (next <= end && !visited[next]) {
+                if (next > 100) continue; // 100을 넘어가면 무시
+                if (visited[next]) continue; // 이미 방문한 곳이면 무시
+                
+                // 뱀이나 사다리가 있는 경우 이동
+                if (jump.containsKey(next)) {
+                    next = jump.get(next);
+                }
+
+                // 방문 체크 & 큐에 추가
+                if (!visited[next]) {
                     visited[next] = true;
-                    if (jump.containsKey(next)) {
-                        queue.offer(new int[]{jump.get(next), cnt + 1});
-                    } else {
-                        queue.offer(new int[]{next, cnt + 1});
-                    }
+                    queue.offer(new int[]{next, cnt + 1});
                 }
             }
         }
         return 0;
-
     }
 }
-
-
