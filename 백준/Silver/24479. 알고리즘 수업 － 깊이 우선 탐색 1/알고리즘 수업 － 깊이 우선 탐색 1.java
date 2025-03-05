1d17e188a70a9b4065f62dc681c0a8fbdbc067f0
Kang Min Kim <lnewgatel@gmail.com>
2025-03-05 14:12:41 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 1512 ms, Memory: 104288 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,58 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    static int N, M, R;
+    static List<List<Integer>> graph;
+    static int[] order;
+    static int cnt = 1; // 방문 순서 카운트
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        N = Integer.parseInt(st.nextToken());
+        M = Integer.parseInt(st.nextToken());
+        R = Integer.parseInt(st.nextToken());
+
+        graph = new ArrayList<>();
+        order = new int[N + 1]; // 방문 순서를 저장하는 배열
+
+        for (int i = 0; i <= N; i++) {
+            graph.add(new ArrayList<>());
+        }
+
+        // 간선 입력 받기
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        }
+
+        // 방문할 노드를 오름차순으로 정렬
+        for (int i = 1; i <= N; i++) {
+            Collections.sort(graph.get(i));
+        }
+
+        boolean[] visited = new boolean[N + 1];
+        dfs(R, visited);
+
+        // 방문 순서 출력
+        for (int i = 1; i <= N; i++) {
+            System.out.println(order[i]);
+        }
+    }
+
+    static void dfs(int vertex, boolean[] visited) {
+        visited[vertex] = true;
+        order[vertex] = cnt++; // 방문 순서 저장
+
+        for (int next : graph.get(vertex)) {
+            if (!visited[next]) {
+                dfs(next, visited);
+            }
+        }
+    }
+}
