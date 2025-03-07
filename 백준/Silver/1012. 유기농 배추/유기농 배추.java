cb6069e090e1a3080122ac89b62b5948a11003b7
Kang Min Kim <lnewgatel@gmail.com>
2025-03-07 16:44:56 +0900

    [Silver II] Title: 유기농 배추, Time: 144 ms, Memory: 15980 KB -BaekjoonHub

--- 백준/Silver/1012. 유기농 배추/유기농 배추.java
+++ 백준/Silver/1012. 유기농 배추/유기농 배추.java
@@ -1,57 +1,53 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.StringTokenizer;
 
 public class Main {
-    static int[] dx = {-1, 1, 0, 0};
-    static int[] dy = {0, 0, -1, 1};
+    static int[] dx = {-1, 0, 1, 0};
+    static int[] dy = {0, -1, 0, 1};
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st;
+        StringBuilder sb = new StringBuilder();
         int T = Integer.parseInt(br.readLine());
         for (int i = 0; i < T; i++) {
-            List<Integer> list = new ArrayList<Integer>();
             st = new StringTokenizer(br.readLine());
             int M = Integer.parseInt(st.nextToken());
             int N = Integer.parseInt(st.nextToken());
             int K = Integer.parseInt(st.nextToken());
-            int[][] arr = new int[M][N];
-            boolean[][] visited = new boolean[M][N];
+
+            int[][] maps = new int[M][N];
             for (int j = 0; j < K; j++) {
                 st = new StringTokenizer(br.readLine());
-                int x = Integer.parseInt(st.nextToken());
-                int y = Integer.parseInt(st.nextToken());
-                arr[x][y] = 1;
+                int row = Integer.parseInt(st.nextToken());
+                int col = Integer.parseInt(st.nextToken());
+                maps[row][col] = 1;
             }
 
-            for (int x = 0; x < arr.length; x++) {
-                for (int y = 0; y < arr[i].length; y++) {
-                    if (arr[x][y] == 1 && !visited[x][y]) {
-                        int cnt = dfs(arr, visited, x, y);
-                        list.add(cnt);
+            boolean[][] visited = new boolean[M][N];
+            int cnt = 0;
+            for (int row = 0; row < M; row++) {
+                for (int col = 0; col < N; col++) {
+                    if (maps[row][col] == 1 && !visited[row][col]) {
+                        dfs(row, col, visited, maps);
+                        cnt++;
                     }
                 }
             }
-            System.out.println(list.size());
+            sb.append(cnt).append("\n");
         }
+        System.out.print(sb);
     }
 
-    static int dfs(int[][] arr, boolean[][] visited, int x, int y) {
+    static void dfs(int x, int y, boolean[][] visited, int[][] maps) {
         visited[x][y] = true;
-        int cnt = 1;
-        int n = arr.length;
-        int m = arr[0].length;
         for (int i = 0; i < 4; i++) {
             int nx = x + dx[i];
             int ny = y + dy[i];
-
-            if (nx >= 0 && nx < n && ny >= 0 && ny < m && arr[nx][ny] == 1 && !visited[nx][ny]) {
-                cnt = dfs(arr, visited, nx, ny);
+            if (nx >= 0 && nx < maps.length && ny >= 0 && ny < maps[0].length && !visited[nx][ny] && maps[nx][ny] == 1) {
+                dfs(nx, ny, visited, maps);
             }
         }
-        return cnt;
     }
 }
