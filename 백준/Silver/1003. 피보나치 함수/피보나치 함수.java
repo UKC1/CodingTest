bfbd8fe76689fa7915578edba74da87e15b1fcdf
Kang Min Kim <lnewgatel@gmail.com>
2025-03-20 19:44:21 +0900

    [Silver III] Title: 피보나치 함수, Time: 100 ms, Memory: 14032 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.java
@@ -1,48 +1,26 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 public class Main {
-	static int zeroCnt;
-	static int oneCnt;
-	static int[][] dp;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringBuilder sb = new StringBuilder();
-		int T = Integer.parseInt(br.readLine());
-		dp = new int[41][2];
-		dp[0][0] = 1;
-		for (int i = 1; i <= 40; i++) {
-			zeroCnt = 0;
-			oneCnt = 0;
-			fibonacci(i);
-			dp[i][0] = zeroCnt;
-			dp[i][1] = oneCnt;
-		}
+        StringBuilder sb = new StringBuilder();
+        int T = Integer.parseInt(br.readLine());
 
-		for (int i = 0; i < T; i++) {
-			int num = Integer.parseInt(br.readLine());
-			sb.append(dp[num][0]).append(" ").append(dp[num][1]).append("\n");
-		}
-		System.out.print(sb);
+        int[][] dp = new int[41][2];
+        dp[0][0] = 1; dp[0][1] = 0;  // fibonacci(0)의 결과
+        dp[1][0] = 0; dp[1][1] = 1;  // fibonacci(1)의 결과
+
+        // DP 배열 미리 계산 (O(40))
+        for (int i = 2; i <= 40; i++) {
+            dp[i][0] = dp[i - 1][0] + dp[i - 2][0];
+            dp[i][1] = dp[i - 1][1] + dp[i - 2][1];
+        }
+
+        for (int i = 0; i < T; i++) {
+            int num = Integer.parseInt(br.readLine());
+            sb.append(dp[num][0]).append(" ").append(dp[num][1]).append("\n");
+        }
+
+        System.out.print(sb);
     }
-
-	static int fibonacci(int n) {
-		if (n == 0) {
-			zeroCnt++;
-			return 0;
-		} else if (n == 1) {
-			oneCnt++;
-			return 1;
-		} else {
-
-			if (dp[n - 2][0] != 0 || dp[n - 2][1] != 0) {
-				zeroCnt = dp[n - 2][0] + dp[n - 1][0];
-				oneCnt = dp[n - 2][1] + dp[n - 1][1];
-				return 0;
-			}
-			return fibonacci(n-1) + fibonacci(n-2);
-		}
-	}
 }
-
