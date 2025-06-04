3c8afb420444b229bca8d6c57b4d2edbe340728f
Kang Min Kim <lnewgatel@gmail.com>
2025-06-04 15:31:49 +0900

    [level 1] Title: 카드 뭉치, Time: 0.03 ms, Memory: 89.6 MB -BaekjoonHub

--- 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
+++ 프로그래머스/1/159994. 카드 뭉치/카드 뭉치.java
@@ -1,17 +1,28 @@
 class Solution {
     public String solution(String[] cards1, String[] cards2, String[] goal) {
         String answer = "Yes";
-        int idx1 = 0;
-        int idx2 = 0;
-        
-        for (int i = 0; i < goal.length; i++) {
-            if (idx1 < cards1.length && goal[i].equals(cards1[idx1])) {
-                idx1++;
-            } else if (idx2 < cards2.length && goal[i].equals(cards2[idx2])) {
-                idx2++;
-            } else {
+        int idx = 0;
+        int leftIdx = 0;
+        int rightIdx = 0;
+        while(idx < goal.length) {
+            String target = goal[idx];
+            boolean isUsed = false;
+            if (leftIdx < cards1.length && cards1[leftIdx].equals(target)) {
+                isUsed = true;
+                leftIdx++;
+            }
+            
+            if (rightIdx <cards2.length && cards2[rightIdx].equals(target)) {
+                isUsed = true;
+                rightIdx++;
+            }
+            
+            if (!isUsed) {
                 return "No";
             }
+            idx++;
+            
+            
         }
         return answer;
     }
