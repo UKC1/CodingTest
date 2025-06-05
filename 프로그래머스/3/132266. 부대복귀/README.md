08ce32ade3e899d2579afda345c16bac18ba36c7
Kang Min Kim <lnewgatel@gmail.com>
2025-06-05 12:17:29 +0900

    [level 3] Title: 부대복귀, Time: 194.20 ms, Memory: 188 MB -BaekjoonHub

--- 프로그래머스/3/132266. 부대복귀/README.md
+++ 프로그래머스/3/132266. 부대복귀/README.md
@@ -1,10 +1,10 @@
 # [level 3] 부대복귀 - 132266 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/132266) 
 
 ### 성능 요약
 
-메모리: 191 MB, 시간: 178.55 ms
+메모리: 188 MB, 시간: 194.20 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 06월 05일 00:28:05
+2025년 06월 05일 12:17:27
 
 ### 문제 설명
 
--- 프로그래머스/3/132266. 부대복귀/부대복귀.java
+++ 프로그래머스/3/132266. 부대복귀/부대복귀.java
@@ -1,36 +1,48 @@
 import java.util.*;
-
 class Solution {
+    final int INF = Integer.MAX_VALUE;
     public int[] solution(int n, int[][] roads, int[] sources, int destination) {
-        List<List<Integer>> graph = new ArrayList<>();
-        for (int i = 0; i <= n; i++) graph.add(new ArrayList<>());
+        int[] answer = new int[sources.length];
+        List<List<Integer>> edges = new ArrayList<>();
+        for (int i = 0; i <= n; i++) {
+            edges.add(new ArrayList<>());
+        }
         
         for (int[] road : roads) {
-            graph.get(road[0]).add(road[1]);
-            graph.get(road[1]).add(road[0]);
+            edges.get(road[0]).add(road[1]);
+            edges.get(road[1]).add(road[0]);
         }
-
+        
+        int[] distFromD = dijkstra(n, destination, edges);
+        
+        for (int i = 0; i < sources.length; i++) {
+            answer[i] = distFromD[sources[i]] == INF ? -1 : distFromD[sources[i]];
+        }
+        return answer;
+    }
+    
+    private int[] dijkstra(int n, int start, List<List<Integer>> edges) {
+        PriorityQueue<int[]> pq = new PriorityQueue<>(Comparator.comparingInt(arr -> arr[1]));
+        pq.offer(new int[] {start, 0});
         int[] dist = new int[n + 1];
-        Arrays.fill(dist, -1);
-        Queue<Integer> queue = new LinkedList<>();
-        queue.offer(destination);
-        dist[destination] = 0;
-
-        while (!queue.isEmpty()) {
-            int curr = queue.poll();
-            for (int next : graph.get(curr)) {
-                if (dist[next] == -1) {
-                    dist[next] = dist[curr] + 1;
-                    queue.offer(next);
+        Arrays.fill(dist, INF);
+        dist[start] = 0;
+        
+        while(!pq.isEmpty()) {
+            int[] curr = pq.poll();
+            int curVertex = curr[0];
+            int curCnt = curr[1];
+            
+            if (dist[curVertex] < curCnt) continue;
+            
+            for (Integer nextVertex : edges.get(curVertex)) {
+                int nextCnt = dist[curVertex] + 1;
+                if (dist[nextVertex] > nextCnt) {
+                    dist[nextVertex] = nextCnt;
+                    pq.offer(new int[] {nextVertex, nextCnt});
                 }
             }
         }
-
-        int[] answer = new int[sources.length];
-        for (int i = 0; i < sources.length; i++) {
-            answer[i] = dist[sources[i]];
-        }
-
-        return answer;
+        return dist;
     }
 }
