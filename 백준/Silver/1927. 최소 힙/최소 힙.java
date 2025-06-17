e5e9612909bad3c147f18a14aa3fb992a4c9ea06
Kang Min Kim <lnewgatel@gmail.com>
2025-06-17 20:07:16 +0900

    [Silver II] Title: 최소 힙, Time: 268 ms, Memory: 26720 KB -BaekjoonHub

--- 백준/Silver/1927. 최소 힙/최소 힙.java
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -2,15 +2,13 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.PriorityQueue;
-import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st;
+        int N = Integer.parseInt(br.readLine());
         PriorityQueue<Integer> pq = new PriorityQueue<>();
         StringBuilder sb = new StringBuilder();
-        int N = Integer.parseInt(br.readLine());
         for (int i = 0; i < N; i++) {
             int num = Integer.parseInt(br.readLine());
             if (num == 0) {
@@ -20,5 +18,6 @@ public class Main {
             }
         }
         System.out.print(sb);
+
     }
 }
