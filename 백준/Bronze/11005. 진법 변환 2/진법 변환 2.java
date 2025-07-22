019796a81d1426816c36d9c8ad2e571d80b46da5
Kang Min Kim <lnewgatel@gmail.com>
2025-07-22 21:38:04 +0900

    [Bronze I] Title: 진법 변환 2, Time: 108 ms, Memory: 14288 KB -BaekjoonHub

--- 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
+++ 백준/Bronze/11005. 진법 변환 2/진법 변환 2.java
@@ -7,9 +7,8 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int number = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
-        System.out.println(Integer.toString(number, n).toUpperCase());
-
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+        System.out.println(Integer.toString(N, M).toUpperCase());
     }
 }
