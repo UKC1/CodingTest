73eb2741078ae9500e9995d78f9fa8f4fa62362c
Kang Min Kim <lnewgatel@gmail.com>
2025-09-23 21:58:30 +0900

    [Silver II] Title: 헌내기는 친구가 필요해, Time: 188 ms, Memory: 134180 KB -BaekjoonHub

--- 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
+++ 백준/Silver/21736. 헌내기는 친구가 필요해/헌내기는 친구가 필요해.py
@@ -1,39 +1,33 @@
 import sys
 from collections import deque
 
-# 입력 받기
-N, M = map(int, sys.stdin.readline().split())
-maps = [list(sys.stdin.readline().strip()) for _ in range(N)]
+input = sys.stdin.readline
+N, M = map(int, input().split())
+maps = [list(input().strip()) for _ in range(N)]
 
-# 시작 위치 찾기
-start_x = start_y = 0
+# 시작 위치 탐색
 for i in range(N):
     for j in range(M):
         if maps[i][j] == 'I':
-            start_x, start_y = i, j
-            break
+            sx, sy = i, j
 
-# BFS 준비
-dx = [-1, 0, 1, 0]
-dy = [0, -1, 0, 1]
+dx = (-1, 0, 1, 0)
+dy = (0, -1, 0, 1)
 visited = [[False] * M for _ in range(N)]
-q = deque()
-q.append((start_x, start_y))
-visited[start_x][start_y] = True
+visited[sx][sy] = True
 
-# 탐색
-person_cnt = 0
+q = deque([(sx, sy)])
+cnt = 0
+
 while q:
     x, y = q.popleft()
-    for i in range(4):
-        nx = x + dx[i]
-        ny = y + dy[i]
+    for d in range(4):
+        nx, ny = x + dx[d], y + dy[d]
         if 0 <= nx < N and 0 <= ny < M and not visited[nx][ny]:
-            if maps[nx][ny] != 'X':  # 벽이 아니면 이동
+            if maps[nx][ny] != 'X':
                 visited[nx][ny] = True
                 if maps[nx][ny] == 'P':
-                    person_cnt += 1
+                    cnt += 1
                 q.append((nx, ny))
 
-# 출력
-print(person_cnt if person_cnt > 0 else "TT")
+print(cnt if cnt else "TT")
