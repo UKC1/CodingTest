695eb4b5d2d10629870eeb22c5751bf507cb085b
Kang Min Kim <lnewgatel@gmail.com>
2025-11-11 21:53:12 +0900

    [Silver I] Title: 숨바꼭질, Time: 244 ms, Memory: 43460 KB -BaekjoonHub

--- 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
+++ 백준/Silver/1697. 숨바꼭질/숨바꼭질.py
@@ -1,35 +1,24 @@
 import sys
-from typing import List
 from collections import deque
+from typing import List
 
-N, K = map(int, sys.stdin.buffer.readline().split())
+N, K = map(int, sys.stdin.readline().split())
 
-visited: List[bool] = [False] * 200001
+MAX = 200000
+visited: List[bool] = [False] * (MAX + 1)
+
 queue = deque()
 queue.append((N, 0))
 visited[N] = True
+
 while queue:
     x, time = queue.popleft()
-
+    
     if x == K:
         print(time)
         break
     
-    nx = x + 1
-    
-    if nx >= 0 and nx < len(visited) and not visited[nx]:
-        visited[nx] = True
-        queue.append((nx, time + 1)) 
-    
-    nx = x - 1
-    
-    if nx >= 0 and nx < len(visited) and not visited[nx]:
-        visited[nx] = True
-        queue.append((nx, time + 1)) 
-    
-    nx = x * 2
-    
-    if nx >= 0 and nx < len(visited) and not visited[nx]:
-        visited[nx] = True
-        queue.append((nx, time + 1)) 
-    
+    for nx in (x - 1, x + 1, x * 2):
+        if 0 <= nx <= MAX and not visited[nx]:
+            visited[nx] = True
+            queue.append((nx, time + 1))
